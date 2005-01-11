d292de82d299390a9eefaa61e5fd59ee49a3ae3a
Tor Andersson <tor@ghostscript.com>
2005-01-11 20:20:33 +0100

    shuffle macpdf files

--- apps/macpdf.c
+++ apps/macpdf.c
@@ -1,10 +1,30 @@
-/* This module implements a Carbon HIView object for a pattern plate
-   editor. */
+#include <Carbon/Carbon.h>
 
-#include "macpdf.h"
+#include <fitz.h>
+#include <mupdf.h>
 
 #define kViewClassID CFSTR("com.artofcode.mupdf.View")
 #define kViewPrivate 'MU_v'
+
+typedef struct viewctx
+{
+    WindowRef window;
+    HIViewRef view;
+    char *doctitle;
+	
+    float zoom;
+    int rotate;
+    int pageno;
+
+    pdf_page *page;
+    fz_obj *pageobj;
+
+    pdf_xref *xref;
+    pdf_pagetree *pages;
+    pdf_outline *outline;
+    fz_renderer *rast;
+    fz_pixmap *image;
+} viewctx;
 
 static OSStatus
 view_construct(EventRef inEvent)
@@ -522,4 +542,72 @@ Lskipload:
 	//xblit();
  CantGetPrivate:
 	return err;
+}
+
+static void
+init_window(viewctx *ctx)
+{
+    WindowRef window = ctx->window;
+    HIViewRef viewPane;
+    static const HIViewID viewPaneID = { 'Poof', 666 };
+    OSStatus err;
+
+    err = HIViewFindByID(HIViewGetRoot(window), viewPaneID, &viewPane);
+    printf("err from findbyid: %d\n", err);
+}
+
+int
+openpdf(WindowRef window, const char *filename)
+{
+    HIViewRef viewPane;
+    static const HIViewID viewPaneID = { 'Poof', 666 };
+    OSStatus err;
+
+    err = HIViewFindByID(HIViewGetRoot(window), viewPaneID, &viewPane);
+    require_noerr(err, cleanup);
+
+    err = view_openpdf(viewPane, filename);
+    require_noerr(err, cleanup);
+
+    err = view_showpage(viewPane);
+
+ cleanup:
+    return err;
+}
+
+int main(int argc, char *argv[])
+{
+    IBNibRef nibRef;
+    OSStatus err;
+    WindowRef window;
+
+    fz_cpudetect();
+    fz_accelerate();
+
+    err = view_register();
+    require_noerr(err, CantRegisterView);
+
+    err = CreateNibReference(CFSTR("main"), &nibRef);
+    printf("err = %d\n", err);
+    require_noerr(err, CantGetNibRef);
+
+    err = SetMenuBarFromNib(nibRef, CFSTR("MenuBar"));
+    require_noerr(err, CantSetMenuBar);
+ 
+    err = CreateWindowFromNib(nibRef, CFSTR("MainWindow"), &window);
+    require_noerr(err, CantCreateWindow);
+
+    openpdf(window, "/Users/tor/src/pdf/tiger.pdf");
+
+    DisposeNibReference(nibRef);
+
+    ShowWindow(window);
+    RunApplicationEventLoop();
+
+ CantGetNibRef:
+ CantSetMenuBar:
+ CantCreateWindow:
+ CantRegisterView:
+
+    return err;
 }
