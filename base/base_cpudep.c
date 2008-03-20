5352905adfc06e9e019f9acec2c973f0ef60d419
Sebastian Rasmussen <sebras@hotmail.com>
2008-03-20 11:20:38 +0100

    Whitespace fixes.

--- base/base_cpudep.c
+++ base/base_cpudep.c
@@ -256,8 +256,8 @@ void fz_cpudetect(void)
 
 	fz_cpuflags = flags;
 
-#if defined(ARCH_X86) || defined(ARCH_X86_64) 
-        __asm__ __volatile__ ("emms\n\t"); 
+#if defined(ARCH_X86) || defined(ARCH_X86_64)
+        __asm__ __volatile__ ("emms\n\t");
 #endif
 
 	dumpflags();
@@ -323,3 +323,4 @@ void fz_cpudetect(void)
 #endif
 
 #endif
+
