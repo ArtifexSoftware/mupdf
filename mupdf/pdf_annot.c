33a1106acd0d1d3b3d8634ae8087ebfe72d010cf
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2008-04-17 08:07:09 +0200

    fix crashes due to dropping NULL links (fixes 8fdd4a5c50de8dcc2315492c65a2755c9485f6ca.pdf)

--- mupdf/pdf_annot.c
+++ mupdf/pdf_annot.c
@@ -165,7 +165,8 @@ pdf_loadannots(pdf_comment **cp, pdf_link **lp, pdf_xref *xref, fz_obj *annots)
 		error = pdf_resolve(&obj, xref);
 		if (error)
 		{
-			pdf_droplink(link);
+			if (link)
+				pdf_droplink(link);
 			return fz_rethrow(error, "cannot resolve annotation");
 		}
 
@@ -178,7 +179,8 @@ pdf_loadannots(pdf_comment **cp, pdf_link **lp, pdf_xref *xref, fz_obj *annots)
 			fz_dropobj(obj);
 			if (error)
 			{
-				pdf_droplink(link);
+				if (link)
+					pdf_droplink(link);
 				return fz_rethrow(error, "cannot load annotation link");
 			}
 
