01315c374dc4ac6e0be91b946f4803269eee802d
Tor Andersson <tor@ghostscript.com>
2008-11-11 22:17:37 +0100

    Cap extreme font sizes.

--- mupdf/pdf_interpret.c
+++ mupdf/pdf_interpret.c
@@ -794,6 +794,16 @@ Lsetcolor:
 				return fz_throw("cannot find font in store");
 
 			gstate->size = fz_toreal(csi->stack[1]);
+			if (gstate->size <= 0.0)
+			{
+				gstate->size = 1.0;
+				fz_warn("font size negative, capping to %g", gstate->size);
+			}
+			if (gstate->size > 1000.0)
+			{
+				gstate->size = 1000.0;
+				fz_warn("font size too large, capping to %g", gstate->size);
+			}
 		}
 
 		else if (!strcmp(buf, "Tr"))
