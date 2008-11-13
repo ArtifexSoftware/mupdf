4ed6bbbfc86ca4027b3da8a268390722e18fa396
Sebastian Rasmussen <sebras@hotmail.com>
2008-11-13 01:09:38 +0100

    Fixed reference counting for shadings found in resource object store.

--- mupdf/pdf_shade.c
+++ mupdf/pdf_shade.c
@@ -265,7 +265,10 @@ pdf_loadshade(fz_shade **shadep, pdf_xref *xref, fz_obj *dict, fz_obj *ref)
 	fz_obj *shd;
 
 	if ((*shadep = pdf_finditem(xref->store, PDF_KSHADE, ref)))
+	{
+		fz_keepshade(*shadep);
 		return fz_okay;
+	}
 
 	/*
 	 * Type 2 pattern dictionary
