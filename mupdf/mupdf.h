44eb22213b1b5573e645a3b464df076a6f667420
Sebastian Rasmussen <sebras@hotmail.com>
2009-06-23 00:15:32 +0200

    Handle JBIG2 stream global segment dictionary entry.

--- mupdf/mupdf.h
+++ mupdf/mupdf.h
@@ -148,7 +148,7 @@ fz_error pdf_loadindirect(fz_obj **objp, pdf_xref *, fz_obj *ref);
 fz_error pdf_resolve(fz_obj **reforobj, pdf_xref *);
 
 int pdf_isstream(pdf_xref *xref, int oid, int gen);
-fz_error pdf_buildinlinefilter(fz_filter **filterp, fz_obj *stmobj);
+fz_error pdf_buildinlinefilter(fz_filter **filterp, pdf_xref *xref, fz_obj *stmobj);
 fz_error pdf_loadrawstream(fz_buffer **bufp, pdf_xref *xref, int oid, int gen);
 fz_error pdf_loadstream(fz_buffer **bufp, pdf_xref *xref, int oid, int gen);
 fz_error pdf_openrawstream(fz_stream **stmp, pdf_xref *, int oid, int gen);
