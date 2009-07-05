1f20476bfb0d4270fa54829e97623dac29f030c9
Sebastian Rasmussen <sebras@hotmail.com>
2009-07-05 21:35:12 +0200

    When reading a complete stream, fill the buffer as much as possible instead of reading just a single byte at a time.

--- fitz/stm_misc.c
+++ fitz/stm_misc.c
@@ -41,6 +41,32 @@ fz_readline(fz_stream *stm, char *mem, int n)
 	return fz_okay;
 }
 
+static inline int fz_fillbuf(fz_stream *stm, fz_buffer *buf)
+{
+	int remaining = buf->ep - buf->wp;
+	int available = stm->buffer->wp - stm->buffer->rp;
+
+	if (available == 0)
+	{
+		int c = fz_readbytex(stm);
+		if (c == EOF)
+			return EOF;
+
+		*buf->wp++ = c;
+
+		remaining = buf->ep - buf->wp;
+		available = stm->buffer->wp - stm->buffer->rp;
+	}
+
+	memmove(buf->wp, stm->buffer->rp, MIN(remaining, available));
+	buf->wp += MIN(remaining, available);
+	stm->buffer->rp += MIN(remaining, available);
+
+	if (buf->rp == buf->wp && stm->buffer->eof)
+		return EOF;
+	return 0;
+}
+
 /*
  * Utility function to consume all the contents of an input stream into
  * a freshly allocated buffer.
@@ -51,7 +77,6 @@ fz_readall(fz_buffer **bufp, fz_stream *stm, int sizehint)
 {
 	fz_error error;
 	fz_buffer *buf;
-	int c;
 
 	if (sizehint == 0)
 	    sizehint = 4 * 1024;
@@ -60,7 +85,7 @@ fz_readall(fz_buffer **bufp, fz_stream *stm, int sizehint)
 	if (error)
 	    return fz_rethrow(error, "cannot create scratch buffer");
 
-	for (c = fz_readbyte(stm); c != EOF; c = fz_readbyte(stm))
+	while (fz_fillbuf(stm, buf) != EOF)
 	{
 		if (buf->wp == buf->ep)
 		{
@@ -71,8 +96,6 @@ fz_readall(fz_buffer **bufp, fz_stream *stm, int sizehint)
 			return fz_rethrow(error, "cannot resize scratch buffer");
 		    }
 		}
-
-		*buf->wp++ = c;
 	}
 
 	*bufp = buf;
