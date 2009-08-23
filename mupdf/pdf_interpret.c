9a36f6887c631f2e638805f1e91ee2be90ba955e
Tor Andersson <tor@ghostscript.com>
2009-08-23 16:35:47 +0200

    Allow negative font sizes.

--- mupdf/pdf_interpret.c
+++ mupdf/pdf_interpret.c
@@ -855,10 +855,10 @@ Lsetcolor:
 				return fz_rethrow(error, "cannot load font");
 
 			gstate->size = fz_toreal(csi->stack[1]);
-			if (gstate->size <= 0.0)
+			if (gstate->size < -1000.0)
 			{
-				gstate->size = 1.0;
-				fz_warn("font size negative, capping to %g", gstate->size);
+				gstate->size = -1000.0;
+				fz_warn("font size too large, capping to %g", gstate->size);
 			}
 			if (gstate->size > 1000.0)
 			{
