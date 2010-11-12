3a5813b32f917c7668d663fd5cb7538251a23301
Tor Andersson <tor@ghostscript.com>
2010-11-12 12:58:22 +0000

    Detect flate/runlength decompression bombs.

--- fitz/stm_read.c
+++ fitz/stm_read.c
@@ -70,6 +70,9 @@ fz_readall(fz_buffer **bufp, fz_stream *stm, int initial)
 		if (buf->len == buf->cap)
 			fz_growbuffer(buf);
 
+		if (buf->len > initial * 100)
+			return fz_throw("compression bomb detected");
+
 		n = fz_read(stm, buf->data + buf->len, buf->cap - buf->len);
 		if (n < 0)
 		{
