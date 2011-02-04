6e99a4c2803a4d5b0cdaaf1e27b425dfa3d1fd1d
Tor Andersson <tor@ghostscript.com>
2011-02-04 10:29:02 +0000

    Don't reset stream wp pointer at end of file, since that will clobber streams opened with openbuffer.

--- fitz/stm_read.c
+++ fitz/stm_read.c
@@ -47,7 +47,7 @@ fz_fillbuffer(fz_stream *stm)
 	n = fz_read(stm, stm->bp, stm->ep - stm->bp);
 	if (n < 0)
 		fz_catch(n, "read error; treating as end of file");
-	else
+	else if (n > 0)
 	{
 		stm->rp = stm->bp;
 		stm->wp = stm->bp + n;
