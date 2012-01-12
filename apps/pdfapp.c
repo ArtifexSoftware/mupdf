157f5cfb9d6dd25fc1ef80dcb2daabd1eaa7716c
Tor Andersson <tor.andersson@artifex.com>
2012-01-12 02:15:32 +0100

    Update copyright notices for 2012.

--- apps/pdfapp.c
+++ apps/pdfapp.c
@@ -36,7 +36,7 @@ char *pdfapp_version(pdfapp_t *app)
 {
 	return
 		"MuPDF 0.9\n"
-		"Copyright 2006-2011 Artifex Software, Inc.\n";
+		"Copyright 2006-2012 Artifex Software, Inc.\n";
 }
 
 char *pdfapp_usage(pdfapp_t *app)
