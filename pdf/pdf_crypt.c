57d4f42f76a8a019915f9e28f3d451590fb08af9
Sebastian Rasmussen <sebras@gmail.com>
2012-08-14 20:32:19 +0200

    Adjust out of range tests for encryption key lengths
    Encryption keys for rev. 4 and prior may at most be 128-bits.
    Encryption keys for rev. 5/6 may only be 256-bits long
    
    Thanks to zeniko for pointing this out.

--- pdf/pdf_crypt.c
+++ pdf/pdf_crypt.c
@@ -315,13 +315,9 @@ pdf_parse_crypt_filter(fz_context *ctx, pdf_crypt_filter *cf, pdf_crypt *crypt, 
 		fz_throw(ctx, "invalid key length: %d", cf->length);
 
 	if ((crypt->r == 1 || crypt->r == 2 || crypt->r == 4) &&
-		(cf->length < 0 || cf->length > 256))
+		(cf->length < 0 || cf->length > 128))
 		fz_throw(ctx, "invalid key length: %d", cf->length);
-	if (crypt->r == 5 &&
-		(cf->length != 128 && cf->length != 192 && cf->length != 256))
-		fz_throw(ctx, "invalid key length: %d", cf->length);
-	if (crypt->r == 6 &&
-		(cf->length != 256))
+	if ((crypt->r == 5 || crypt->r == 6) && cf->length != 256)
 		fz_throw(ctx, "invalid key length: %d", cf->length);
 }
 
