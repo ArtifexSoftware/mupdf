5f8e096b9090577ed93072448339a6500a5178f3
Paul Gardiner <paulg.artifex@glidos.net>
2012-09-19 15:03:37 +0100

    Forms: ensure result from alert callback is passed back to caller

--- pdf/pdf_event.c
+++ pdf/pdf_event.c
@@ -19,12 +19,16 @@ fz_alert_event *fz_access_alert_event(fz_doc_event *event)
 
 void pdf_event_issue_alert(pdf_document *doc, fz_alert_event *alert)
 {
-	fz_alert_event_internal ievent;
-	ievent.base.type = FZ_DOCUMENT_EVENT_ALERT;
-	ievent.alert = *alert;
-
 	if (doc->event_cb)
+	{
+		fz_alert_event_internal ievent;
+		ievent.base.type = FZ_DOCUMENT_EVENT_ALERT;
+		ievent.alert = *alert;
+
 		doc->event_cb((fz_doc_event *)&ievent, doc->event_cb_data);
+
+		*alert = ievent.alert;
+	}
 }
 
 void pdf_set_doc_event_callback(pdf_document *doc, fz_doc_event_cb *fn, void *data)
