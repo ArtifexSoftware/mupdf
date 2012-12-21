bb80076d0addf2c6c0a2532fc48c4f75f92cec66
Robin Watts <robin.watts@artifex.com>
2012-12-21 13:59:01 +0000

    Bug 693503: Fix SEGV in pdf_function.
    The pdf function code only expects a maximum of FZ_MAX_COLORS
    component functions in a sampling function; more functions than
    this causes a buffer overflow. Add some checks to avoid this.
    
    Problem found in 1219.pdf.SIGSEGV.fc0.246, a test file supplied by
    Mateusz "j00ru" Jurczyk and Gynvael Coldwind of the Google Security
    Team using Address Sanitizer. Many thanks!

--- pdf/pdf_shade.c
+++ pdf/pdf_shade.c
@@ -382,7 +382,15 @@ pdf_load_shading_dict(pdf_document *xref, pdf_obj *dict, fz_matrix transform)
 		{
 			funcs = pdf_array_len(obj);
 			if (funcs != 1 && funcs != shade->colorspace->n)
+			{
+				funcs = 0;
 				fz_throw(ctx, "incorrect number of shading functions");
+			}
+			if (funcs > FZ_MAX_COLORS)
+			{
+				funcs = 0;
+				fz_throw(ctx, "too many shading functions");
+			}
 
 			if (type == 1)
 				in = 2;
