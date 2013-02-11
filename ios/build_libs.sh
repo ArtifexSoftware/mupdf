bc131fc0b8de2af766281eed31efbc4e13efa72a
Tor Andersson <tor.andersson@artifex.com>
2013-02-11 16:16:47 +0100

    Apple broke external scripts yet again.
    Disable the automatic "make generate" step from the build, since it
    doesn't work anymore. There appears to be some hidden environment
    setting used by the compiler which breaks external make compiling
    for host while Xcode is set to cross compile.
    
    This is a pointless waste of time to track down and fix, so now you
    will have to run the "make generate" step manually.

--- ios/build_libs.sh
+++ ios/build_libs.sh
@@ -5,7 +5,11 @@
 # Also see "iOS" section in Makerules.
 
 echo Generating cmap and font files
-make -C .. generate || exit 1
+echo "Apple broke Xcode external targets yet again, and I can't be bothered to fix it."
+echo "Run the 'make generate' command manually from now on!"
+echo "If you see an error while running GEN, you've forgotten."
+
+# make -C .. verbose=yes generate || exit 1
 
 export OS=ios
 export build=$(echo $CONFIGURATION | tr A-Z a-z)
@@ -14,7 +18,7 @@ case $ARCHS in
 	armv6) ARCHFLAGS="-arch armv6 -mno-thumb" ;;
 	armv7) ARCHFLAGS="-arch armv7 -mthumb" ;;
 	i386) ARCHFLAGS="-arch i386" ;;
-	*) echo "Unknown architecture!"; exit 1 ;;
+	*) echo "Unknown architecture:" $ARCHS; exit 1 ;;
 esac
 
 export CFLAGS="$ARCHFLAGS -isysroot $SDKROOT"
