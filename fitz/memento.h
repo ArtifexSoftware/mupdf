016fb33f6805cac5af0cdd04899b9da39d190590
Sebastian Rasmussen <sebras@gmail.com>
2013-04-28 14:26:21 +0200

    Fix copyright statements

--- fitz/memento.h
+++ fitz/memento.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001-2012 Artifex Software, Inc.
+/* Copyright (C) 2001-2013 Artifex Software, Inc.
    All Rights Reserved.
 
    This software is provided AS-IS with no warranty, either express or
