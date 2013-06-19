d0b1c20e8d94ed3eabaa6ec07036edad8b32c92a
Tor Andersson <tor.andersson@artifex.com>
2013-06-19 16:24:01 +0200

    Move pixmap md5 summing into pixmap.c

--- fitz/res_pixmap.c
+++ fitz/res_pixmap.c
@@ -1049,3 +1049,14 @@ fz_pixmap_set_resolution(fz_pixmap *pix, int res)
 	pix->xres = res;
 	pix->yres = res;
 }
+
+void
+fz_md5_pixmap(fz_pixmap *pix, unsigned char digest[16])
+{
+	fz_md5 md5;
+
+	fz_md5_init(&md5);
+	if (pix)
+		fz_md5_update(&md5, pix->samples, pix->w * pix->h * pix->n);
+	fz_md5_final(&md5, digest);
+}
