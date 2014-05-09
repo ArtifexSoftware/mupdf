7db69f3b4a2732d993a23418344cda23924c8749
Simon Bünzli <zeniko@gmail.com>
2014-05-09 19:56:15 +0200

    correctly bound image mask clippings in bbox device
    Same as for fz_bbox_fill_image_mask, fz_bbox_clip_image_mask must
    transform the unit rectangle to get the bounding bbox.

--- source/fitz/bbox-device.c
+++ source/fitz/bbox-device.c
@@ -119,7 +119,7 @@ fz_bbox_clip_stroke_text(fz_device *dev, fz_text *text, fz_stroke_state *stroke,
 static void
 fz_bbox_clip_image_mask(fz_device *dev, fz_image *image, const fz_rect *rect, const fz_matrix *ctm)
 {
-	fz_rect r = *rect;
+	fz_rect r = fz_unit_rect;
 	fz_bbox_add_rect(dev, fz_transform_rect(&r, ctm), 1);
 }
 
