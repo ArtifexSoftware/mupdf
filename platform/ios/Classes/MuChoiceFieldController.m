986ec1d2fc07d1487c9865dc639b6a01651d6b2b
Joseph Heenan <joseph@emobix.co.uk>
2014-10-02 19:54:23 +0200

    iOS: Remove empty didReceiveMemoryWarning implementation

--- platform/ios/Classes/MuChoiceFieldController.m
+++ platform/ios/Classes/MuChoiceFieldController.m
@@ -33,12 +33,6 @@
 	// Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
-}
-
 - (void)dealloc
 {
 	[okayBlock release];
