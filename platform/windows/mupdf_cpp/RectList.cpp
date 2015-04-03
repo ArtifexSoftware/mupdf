4c9611cb3d9f18322f8e77f8ba493799d77c0a0a
Robin Watts <robin.watts@artifex.com>
2015-04-03 12:01:49 +0100

    Remove gsview from mupdf repo.
    It has its own repo now. See http://git.ghostscript.com/?p=gsview.git

--- platform/windows/mupdf_cpp/RectList.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include "RectList.h"
-
-namespace mupdf_cpp
-{
-	RectList::RectList(void)
-	{
-	}
-}
--- platform/windows/mupdfwinrt/utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Windows.h"
-using namespace Platform;
-
-String^ char_to_String(const char *char_in);
-char* String_to_char(String^ text);
