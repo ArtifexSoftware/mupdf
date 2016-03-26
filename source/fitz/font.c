5a72b781cb1a44951c60a2a91c8de6e0b97f03f5
Robin Watts <robin.watts@artifex.com>
2016-03-26 09:30:14 +0000

    Add missing hb_lock/unlock around shaper freeing.
    This was causing blocks to be free'd rather than fz_free'd.

--- source/fitz/font.c
+++ source/fitz/font.c
@@ -154,7 +154,9 @@ fz_drop_font(fz_context *ctx, fz_font *font)
 	fz_free(ctx, font->bbox_table);
 	fz_free(ctx, font->width_table);
 	fz_free(ctx, font->advance_cache);
+	hb_lock(ctx);
 	hb_font_destroy(font->shaper);
+	hb_unlock(ctx);
 	fz_free(ctx, font);
 }
 
