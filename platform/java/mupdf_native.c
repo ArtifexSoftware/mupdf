7adb22c9cdcb41c47629b6734cac5cea1a4713af
Tor Andersson <tor.andersson@artifex.com>
2016-04-06 00:04:50 +0200

    Fix java build for Bidi enum renames.

--- platform/java/mupdf_native.c
+++ platform/java/mupdf_native.c
@@ -2967,7 +2967,7 @@ FUN(Text_showGlyph)(JNIEnv *env, jobject self, jobject font_, jobject matrix_, j
 		return;
 
 	fz_try(ctx)
-		fz_show_glyph(ctx, text, font, &trm, glyph, unicode, wmode, 0, FZ_DIR_UNSET, FZ_LANG_UNSET);
+		fz_show_glyph(ctx, text, font, &trm, glyph, unicode, wmode, 0, FZ_BIDI_NEUTRAL, FZ_LANG_UNSET);
 	fz_catch(ctx)
 		jni_rethrow(env, ctx);
 }
@@ -2989,7 +2989,7 @@ FUN(Text_showString)(JNIEnv *env, jobject self, jobject font_, jobject matrix_, 
 		return;
 
 	fz_try(ctx)
-		fz_show_string(ctx, text, font, &trm, string, wmode, 0, FZ_DIR_UNSET, FZ_LANG_UNSET);
+		fz_show_string(ctx, text, font, &trm, string, wmode, 0, FZ_BIDI_NEUTRAL, FZ_LANG_UNSET);
 	fz_always(ctx)
 		(*env)->ReleaseStringUTFChars(env, string_, string);
 	fz_catch(ctx)
