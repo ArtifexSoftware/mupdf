a7c3d73078f9e0cfc12d7eee86d0e2de197768ab
Tor Andersson <tor.andersson@artifex.com>
2016-04-25 15:43:26 +0200

    Add 'mutool run' documentation.

---
+++ docs/mutool/examples/pdf-create-lowlevel.js
@@ -0,0 +1,63 @@
+// Create a PDF from scratch.
+
+// This example creates a new PDF file from scratch, using only the low level APIs.
+// This assumes a basic working knowledge of the PDF file format.
+
+// Create a new empty document with no pages.
+var pdf = new PDFDocument()
+
+// Create and add a font resource.
+var font = pdf.addObject({
+	Type: "Font",
+	Subtype: "Type1",
+	Encoding: "WinAnsiEncoding",
+	BaseFont: "Times-Roman",
+})
+
+// Create and add an image resource:
+// Allocate a slot for a new object and get a reference to it.
+var image = pdf.createObject()
+// Write a dictionary object into the slot.
+image.writeObject({
+	Type: "XObject",
+	Subtype: "Image",
+	Width: 4,
+	Height: 2,
+	BitsPerComponent: 8,
+	ColorSpace: "DeviceGray",
+	// The compression filter to be used:
+	Filter: "ASCIIHexDecode",
+})
+// Write raw stream data into the slot; hex encoded
+// to match the Filter entry in the dictionary.
+image.writeRawStream("004488CCEEBB7733&gt;")
+
+// Create resource dictionary.
+var resources = pdf.addObject({
+	Font: { Tm: font },
+	XObject: { Im0: image },
+})
+
+// Create content stream.
+var buffer = new Buffer()
+buffer.writeLine("10 10 280 330 re s")
+buffer.writeLine("q 200 0 0 200 50 100 cm /Im0 Do Q")
+buffer.writeLine("BT /Tm 16 Tf 50 50 TD (Hello, world!) Tj ET")
+var contents = pdf.addStream(buffer)
+
+// Create page object.
+var page = pdf.addObject({
+	Type: "Page",
+	MediaBox: [0,0,300,350],
+	Contents: contents,
+	Resources: resources,
+})
+
+// Insert page object into page tree.
+var pagetree = pdf.getTrailer().Root.Pages
+pagetree.Count = 1
+pagetree.Kids = [ page ]
+page.Parent = pagetree
+
+// Save the document.
+pdf.save("out.pdf")
---
+++ docs/mutool/examples/pdf-merge.js
@@ -0,0 +1,66 @@
+// A re-implementation of "mutool merge" in JavaScript.
+
+function graftObject(dstDoc, srcDoc, srcObj, map) {
+	var srcNum, dstRef, dstObj
+	if (!map)
+		map = []
+	if (srcObj.isIndirect()) {
+		srcNum = srcObj.toIndirect()
+		if (map[srcNum])
+			return map[srcNum]
+		map[srcNum] = dstRef = dstDoc.createObject()
+		dstRef.writeObject(graftObject(dstDoc, srcDoc, srcObj.resolve(), map))
+		if (srcObj.isStream())
+			dstRef.writeRawStream(srcObj.readRawStream())
+		return dstRef
+	}
+	if (srcObj.isArray()) {
+		dstObj = dstDoc.newArray()
+		srcObj.forEach(function (key, val) {
+			dstObj[key] = graftObject(dstDoc, srcDoc, val, map)
+		})
+		return dstObj
+	}
+	if (srcObj.isDictionary()) {
+		dstObj = dstDoc.newDictionary()
+		srcObj.forEach(function (key, val) {
+			dstObj[key] = graftObject(dstDoc, srcDoc, val, map)
+		})
+		return dstObj
+	}
+	return srcObj /* primitive objects are not bound to a document */
+}
+
+function copyPage(dstDoc, srcDoc, pageNumber, map) {
+	var srcPage, dstPage
+	srcPage = srcDoc.findPage(pageNumber)
+	dstPage = dstDoc.newDictionary()
+	if (srcPage.MediaBox) dstPage.MediaBox = graftObject(dstDoc, srcDoc, srcPage.MediaBox, map)
+	if (srcPage.Rotate) dstPage.Rotate = graftObject(dstDoc, srcDoc, srcPage.Rotate, map)
+	if (srcPage.Resources) dstPage.Resources = graftObject(dstDoc, srcDoc, srcPage.Resources, map)
+	if (srcPage.Contents) dstPage.Contents = graftObject(dstDoc, srcDoc, srcPage.Contents, map)
+	dstDoc.insertPage(-1, dstDoc.addObject(dstPage))
+}
+
+function copyAllPages(dstDoc, srcDoc) {
+	var k, n = srcDoc.countPages()
+	var srcMap = []
+	for (k = 0; k < n; ++k)
+		copyPage(dstDoc, srcDoc, k, srcMap)
+}
+
+function pdfmerge() {
+	var srcDoc, dstDoc, i
+
+	dstDoc = new PDFDocument()
+	for (i = 2; i < argv.length; ++i) {
+		srcDoc = new PDFDocument(argv[i])
+		copyAllPages(dstDoc, srcDoc)
+	}
+	dstDoc.save(argv[1])
+}
+
+if (argv.length < 3)
+	print("usage: mutool run pdf-merge.js output.pdf input1.pdf input2.pdf ...")
+else
+	pdfmerge()
