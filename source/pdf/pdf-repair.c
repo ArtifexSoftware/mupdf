ArtifexSoftware/mupdf

commit 4a4e6adae4c1a0e9ab3b6fad477edfe26c1a2aca

Use 'size_t' instead of int as appropriate.
This silences the many warnings we get when building for x64
in windows.

This does not address any of the warnings we get in thirdparty
libraries - in particular harfbuzz. These look (at a quick
glance) harmless though.

source/pdf/pdf-repair.c
@@ -281,7 +281,9 @@ pdf_repair_xref(fz_context *ctx, pdf_document *doc)
 	int stm_len;
 	pdf_token tok;
 	int next;
-	int i, n, c;
+	int i;
+	size_t j, n;
+	int c;
 	pdf_lexbuf *buf = &doc->lexbuf.base;
 	int num_roots = 0;
 	int max_roots = 0;
@@ -316,7 +318,7 @@ pdf_repair_xref(fz_context *ctx, pdf_document *doc)
 		n = fz_read(ctx, doc->file, (unsigned char *)buf->scratch, fz_mini(buf->size, 1024));
 
 		fz_seek(ctx, doc->file, 0, 0);
-		for (i = 0; i < n - 4; i++)
+		for (j = 0; j < n - 4; j++)
 		{
 			if (memcmp(&buf->scratch[i], "%PDF", 4) == 0)
 			{

commit 46f8fdf94481fab0c347e90443e8fa1723b763e6

Fix typo due to switching from int to size_t.
Commit 4a4e6adae4c1a0e9ab3b6fad477edfe26c1a2aca introduced a typo
when doing the int -> size_t conversion. This caused a coverity
warning which is now fixed.

source/pdf/pdf-repair.c
@@ -320,9 +320,9 @@ pdf_repair_xref(fz_context *ctx, pdf_document *doc)
 		fz_seek(ctx, doc->file, 0, 0);
 		for (j = 0; j < n - 4; j++)
 		{
-			if (memcmp(&buf->scratch[i], "%PDF", 4) == 0)
+			if (memcmp(&buf->scratch[j], "%PDF", 4) == 0)
 			{
-				fz_seek(ctx, doc->file, i + 8, 0); /* skip "%PDF-X.Y" */
+				fz_seek(ctx, doc->file, j + 8, 0); /* skip "%PDF-X.Y" */
 				break;
 			}
 		}