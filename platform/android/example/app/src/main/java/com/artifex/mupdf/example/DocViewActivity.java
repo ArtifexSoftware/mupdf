93456e1c3f9bd3edfc5b03d317b36b902b01b6da
Tor Andersson <tor.andersson@artifex.com>
2016-07-06 21:13:36 +0200

    android: Fix mixed tabs/spaces in DocViewActivity.java.

--- platform/android/example/app/src/main/java/com/artifex/mupdf/example/DocViewActivity.java
+++ platform/android/example/app/src/main/java/com/artifex/mupdf/example/DocViewActivity.java
@@ -8,23 +8,22 @@ import com.artifex.mupdf.android.DocView;
 
 public class DocViewActivity extends Activity
 {
-    private DocView mDocView;
+	private DocView mDocView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 
-        //  set up UI
+		//  set up UI
 		setContentView(R.layout.activity_doc_view);
-        mDocView = (DocView)findViewById(R.id.doc_view);
+		mDocView = (DocView)findViewById(R.id.doc_view);
 
 		//  get the file path
 		Uri uri = getIntent().getData();
 		final String path = Uri.decode(uri.getEncodedPath());
 
-        //  start the view
-        mDocView.start(path);
+		//  start the view
+		mDocView.start(path);
 	}
-
 }
