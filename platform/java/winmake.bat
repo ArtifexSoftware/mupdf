e427d4320db35d4160c9d9aaed16145dd88f873e
Robin Watts <robin.watts@artifex.com>
2016-07-11 17:56:09 +0100

    MSVC: Add DebugJava/ReleaseJava configurations.
    These build the mupdf desktop java viewer.

---
+++ platform/java/winmake.bat
@@ -0,0 +1,17 @@
+@echo Cleaning
+echo bogus > example\bogus.class
+del /Q example\*.class
+echo bogus > com\artifex\mupdf\fitz\bogus.class
+del /Q com\artifex\mupdf\fitz\*.class
+
+@echo Building Viewer
+javac -source 1.7 -target 1.7 example/Viewer.java
+
+@echo Building JNI classes
+javac -source 1.7 -target 1.7 com/artifex/mupdf/fitz/*.java
+
+@echo Importing DLL (built using VS solution)
+@copy ..\win32\%1\javaviewerlib.dll mupdf_java.dll /y
+
+@echo Packaging into jar (incomplete as missing manifest)
+jar cf javaviewer.jar mupdf_java.dll com\artifex\mupdf\fitz\*.java example\viewer.java
