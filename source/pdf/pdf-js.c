b136e23b7e58025e7769dd21629ffda958b922df
Sebastian Rasmussen <sebras@gmail.com>
2016-07-24 01:40:05 +0800

    Remove function not being called.

--- source/pdf/pdf-js.c
+++ source/pdf/pdf-js.c
@@ -729,6 +729,5 @@ int pdf_js_supported(fz_context *ctx, pdf_document *doc) { return 0; }
 void pdf_js_setup_event(pdf_js *js, pdf_js_event *e) { }
 pdf_js_event *pdf_js_get_event(pdf_js *js) { return NULL; }
 void pdf_js_execute(pdf_js *js, char *code) { }
-void pdf_js_execute_count(pdf_js *js, char *code, int count) { }
 
 #endif /* FZ_ENABLE_JS */
