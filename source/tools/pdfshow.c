ArtifexSoftware/mupdf

commit c22e6a6dc2bf6acbac955bd5fbdd896329dfd725

Use fz_output instead of FILE* for most of our output needs.
Use fz_output in debug printing functions.
Use fz_output in pdfshow.
Use fz_output in fz_trace_device instead of stdout.
Use fz_output in pdf-write.c.

Rename fz_new_output_to_filename to fz_new_output_with_path.
Add seek and tell to fz_output.
Remove unused functions like fz_fprintf.
Fix typo in pdf_print_obj.

source/tools/pdfshow.c
@@ -4,10 +4,9 @@
 
 #include "mupdf/pdf.h"
 
-static FILE *out = NULL;
-
 static pdf_document *doc = NULL;
 static fz_context *ctx = NULL;
+static fz_output *out = NULL;
 static int showbinary = 0;
 static int showdecode = 1;
 static int showcolumn;
@@ -26,9 +25,9 @@ static void showtrailer(void)
 {
 	if (!doc)
 		fz_throw(ctx, FZ_ERROR_GENERIC, "no file specified");
-	fprintf(out, "trailer\n");
-	pdf_fprint_obj(ctx, out, pdf_trailer(ctx, doc), 0);
-	fprintf(out, "\n");
+	fz_printf(ctx, out, "trailer\n");
+	pdf_print_obj(ctx, out, pdf_trailer(ctx, doc), 0);
+	fz_printf(ctx, out, "\n");
 }
 
 static void showencrypt(void)
@@ -40,9 +39,9 @@ static void showencrypt(void)
 	encrypt = pdf_dict_get(ctx, pdf_trailer(ctx, doc), PDF_NAME_Encrypt);
 	if (!encrypt)
 		fz_throw(ctx, FZ_ERROR_GENERIC, "document not encrypted");
-	fprintf(out, "encryption dictionary\n");
-	pdf_fprint_obj(ctx, out, pdf_resolve_indirect(ctx, encrypt), 0);
-	fprintf(out, "\n");
+	fz_printf(ctx, out, "encryption dictionary\n");
+	pdf_print_obj(ctx, out, pdf_resolve_indirect(ctx, encrypt), 0);
+	fz_printf(ctx, out, "\n");
 }
 
 static void showxref(void)
@@ -50,7 +49,7 @@ static void showxref(void)
 	if (!doc)
 		fz_throw(ctx, FZ_ERROR_GENERIC, "no file specified");
 	pdf_print_xref(ctx, doc);
-	fprintf(out, "\n");
+	fz_printf(ctx, out, "\n");
 }
 
 static void showpagetree(void)
@@ -66,9 +65,9 @@ static void showpagetree(void)
 	for (i = 0; i < count; i++)
 	{
 		ref = pdf_lookup_page_obj(ctx, doc, i);
-		fprintf(out, "page %d = %d %d R\n", i + 1, pdf_to_num(ctx, ref), pdf_to_gen(ctx, ref));
+		fz_printf(ctx, out, "page %d = %d %d R\n", i + 1, pdf_to_num(ctx, ref), pdf_to_gen(ctx, ref));
 	}
-	fprintf(out, "\n");
+	fz_printf(ctx, out, "\n");
 }
 
 static void showsafe(unsigned char *buf, int n)
@@ -113,7 +112,7 @@ static void showstream(int num, int gen)
 		if (n == 0)
 			break;
 		if (showbinary)
-			fwrite(buf, 1, n, out);
+			fz_write(ctx, out, buf, n);
 		else
 			showsafe(buf, n);
 	}
@@ -138,19 +137,19 @@ static void showobject(int num, int gen)
 		}
 		else
 		{
-			fprintf(out, "%d %d obj\n", num, gen);
-			pdf_fprint_obj(ctx, out, obj, 0);
-			fprintf(out, "stream\n");
+			fz_printf(ctx, out, "%d %d obj\n", num, gen);
+			pdf_print_obj(ctx, out, obj, 0);
+			fz_printf(ctx, out, "stream\n");
 			showstream(num, gen);
-			fprintf(out, "endstream\n");
-			fprintf(out, "endobj\n\n");
+			fz_printf(ctx, out, "endstream\n");
+			fz_printf(ctx, out, "endobj\n\n");
 		}
 	}
 	else
 	{
-		fprintf(out, "%d %d obj\n", num, gen);
-		pdf_fprint_obj(ctx, out, obj, 0);
-		fprintf(out, "endobj\n\n");
+		fz_printf(ctx, out, "%d %d obj\n", num, gen);
+		pdf_print_obj(ctx, out, obj, 0);
+		fz_printf(ctx, out, "endobj\n\n");
 	}
 
 	pdf_drop_obj(ctx, obj);
@@ -179,15 +178,15 @@ static void showgrep(char *filename)
 
 			pdf_sort_dict(ctx, obj);
 
-			fprintf(out, "%s:%d: ", filename, i);
-			pdf_fprint_obj(ctx, out, obj, 1);
+			fz_printf(ctx, out, "%s:%d: ", filename, i);
+			pdf_print_obj(ctx, out, obj, 1);
 
 			pdf_drop_obj(ctx, obj);
 		}
 	}
 
-	fprintf(out, "%s:trailer: ", filename);
-	pdf_fprint_obj(ctx, out, pdf_trailer(ctx, doc), 1);
+	fz_printf(ctx, out, "%s:trailer: ", filename);
+	pdf_print_obj(ctx, out, pdf_trailer(ctx, doc), 1);
 }
 
 static void showoutline(void)
@@ -198,7 +197,7 @@ static void showoutline(void)
 	fz_var(out);
 	fz_try(ctx)
 	{
-		out = fz_new_output_with_file(ctx, stdout, 0);
+		out = fz_new_output_with_file_ptr(ctx, stdout, 0);
 		fz_print_outline(ctx, out, outline);
 	}
 	fz_always(ctx)
@@ -219,6 +218,13 @@ int pdfshow_main(int argc, char **argv)
 	char *output = NULL;
 	int c;
 
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
+	if (!ctx)
+	{
+		fprintf(stderr, "cannot initialise context\n");
+		exit(1);
+	}
+
 	while ((c = fz_getopt(argc, argv, "p:o:be")) != -1)
 	{
 		switch (c)
@@ -236,23 +242,10 @@ int pdfshow_main(int argc, char **argv)
 
 	filename = argv[fz_optind++];
 
-	out = stdout;
 	if (output)
-	{
-		out = fz_fopen(output, "wb");
-		if (!out)
-		{
-			fprintf(stderr, "cannot open output file: '%s'\n", output);
-			exit(1);
-		}
-	}
-
-	ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
-	if (!ctx)
-	{
-		fprintf(stderr, "cannot initialise context\n");
-		exit(1);
-	}
+		out = fz_new_output_with_path(ctx, output, 0);
+	else
+		out = fz_new_output_with_file_ptr(ctx, stdout, 0);
 
 	fz_var(doc);
 	fz_try(ctx)
@@ -284,9 +277,7 @@ int pdfshow_main(int argc, char **argv)
 	{
 	}
 
-	if (out != stdout)
-		fclose(out);
-
+	fz_drop_output(ctx, out);
 	pdf_close_document(ctx, doc);
 	fz_drop_context(ctx);
 	return 0;

include/mupdf/fitz/buffer.h
@@ -110,28 +110,19 @@ void fz_trim_buffer(fz_context *ctx, fz_buffer *buf);
 */
 void fz_buffer_cat(fz_context *ctx, fz_buffer *buf, fz_buffer *extra);
 
-void fz_write_buffer(fz_context *ctx, fz_buffer *buf, const void *data, int len);
-
-void fz_write_buffer_byte(fz_context *ctx, fz_buffer *buf, int val);
-
-void fz_write_buffer_rune(fz_context *ctx, fz_buffer *buf, int val);
-
-void fz_write_buffer_bits(fz_context *ctx, fz_buffer *buf, int val, int bits);
-
-void fz_write_buffer_pad(fz_context *ctx, fz_buffer *buf);
-
 /*
-	fz_buffer_printf: print formatted to a buffer. The buffer will grow
-	as required.
+	fz_write_buffer*: write to a buffer.
+	fz_buffer_printf: print formatted to a buffer.
+	fz_buffer_cat_pdf_string: Print a string using PDF syntax and escapes.
+	The buffer will grow as required.
 */
+void fz_write_buffer(fz_context *ctx, fz_buffer *buf, const void *data, int len);
+void fz_write_buffer_byte(fz_context *ctx, fz_buffer *buf, int val);
+void fz_write_buffer_rune(fz_context *ctx, fz_buffer *buf, int val);
+void fz_write_buffer_bits(fz_context *ctx, fz_buffer *buf, int val, int bits);
+void fz_write_buffer_pad(fz_context *ctx, fz_buffer *buf);
 int fz_buffer_printf(fz_context *ctx, fz_buffer *buffer, const char *fmt, ...);
 int fz_buffer_vprintf(fz_context *ctx, fz_buffer *buffer, const char *fmt, va_list args);
-
-/*
-	fz_buffer_printf: print a string formatted as a pdf string to a buffer.
-	The buffer will grow.
-*/
-void
-fz_buffer_cat_pdf_string(fz_context *ctx, fz_buffer *buffer, const char *text);
+void fz_buffer_cat_pdf_string(fz_context *ctx, fz_buffer *buffer, const char *text);
 
 #endif

include/mupdf/fitz/device.h
@@ -274,10 +274,9 @@ struct fz_cookie_s
 };
 
 /*
-	fz_new_trace_device: Create a device to print a debug trace of
-	all device calls.
+	fz_new_trace_device: Create a device to print a debug trace of all device calls.
 */
-fz_device *fz_new_trace_device(fz_context *ctx);
+fz_device *fz_new_trace_device(fz_context *ctx, fz_output *out);
 
 /*
 	fz_new_bbox_device: Create a device to compute the bounding

include/mupdf/fitz/font.h
@@ -97,8 +97,6 @@ void fz_decouple_type3_font(fz_context *ctx, fz_font *font, void *t3doc);
 float fz_advance_glyph(fz_context *ctx, fz_font *font, int glyph);
 int fz_encode_character(fz_context *ctx, fz_font *font, int unicode);
 
-#ifndef NDEBUG
-void fz_print_font(fz_context *ctx, FILE *out, fz_font *font);
-#endif
+void fz_print_font(fz_context *ctx, fz_output *out, fz_font *font);
 
 #endif

include/mupdf/fitz/function.h
@@ -16,9 +16,7 @@ void fz_eval_function(fz_context *ctx, fz_function *func, const float *in, int i
 fz_function *fz_keep_function(fz_context *ctx, fz_function *func);
 void fz_drop_function(fz_context *ctx, fz_function *func);
 unsigned int fz_function_size(fz_context *ctx, fz_function *func);
-#ifndef NDEBUG
-void pdf_debug_function(fz_context *ctx, fz_function *func);
-#endif
+void fz_print_function(fz_context *ctx, fz_output *out, fz_function *func);
 
 enum
 {
@@ -33,9 +31,7 @@ struct fz_function_s
 	int m;					/* number of input values */
 	int n;					/* number of output values */
 	void (*evaluate)(fz_context *ctx, fz_function *func, const float *in, float *out);
-#ifndef NDEBUG
-	void (*debug)(fz_context *ctx, fz_function *func);
-#endif
+	void (*print)(fz_context *ctx, fz_output *out, fz_function *func);
 };
 
 #endif

include/mupdf/fitz/hash.h
@@ -3,6 +3,7 @@
 
 #include "mupdf/fitz/system.h"
 #include "mupdf/fitz/context.h"
+#include "mupdf/fitz/output.h"
 
 /*
  * Generic hash-table with fixed-length keys.
@@ -24,9 +25,7 @@ int fz_hash_len(fz_context *ctx, fz_hash_table *table);
 void *fz_hash_get_key(fz_context *ctx, fz_hash_table *table, int idx);
 void *fz_hash_get_val(fz_context *ctx, fz_hash_table *table, int idx);
 
-#ifndef NDEBUG
-void fz_print_hash(fz_context *ctx, FILE *out, fz_hash_table *table);
-void fz_print_hash_details(fz_context *ctx, FILE *out, fz_hash_table *table, void (*details)(FILE *, void *));
-#endif
+void fz_print_hash(fz_context *ctx, fz_output *out, fz_hash_table *table);
+void fz_print_hash_details(fz_context *ctx, fz_output *out, fz_hash_table *table, void (*details)(fz_context*, fz_output*, void*));
 
 #endif

include/mupdf/fitz/output-pnm.h
@@ -8,30 +8,28 @@
 #include "mupdf/fitz/bitmap.h"
 
 /*
-	fz_write_pnm: Save a pixmap as a pnm
-
-	filename: The filename to save as (including extension).
+	fz_write_pnm: Save a pixmap as a PNM image file.
 */
 void fz_write_pnm(fz_context *ctx, fz_pixmap *pixmap, char *filename);
 
+void fz_output_pnm(fz_context *ctx, fz_output *out, fz_pixmap *pixmap);
 void fz_output_pnm_header(fz_context *ctx, fz_output *out, int w, int h, int n);
 void fz_output_pnm_band(fz_context *ctx, fz_output *out, int w, int h, int n, int band, int bandheight, unsigned char *p);
 
 /*
-	fz_write_pam: Save a pixmap as a pam
-
-	filename: The filename to save as (including extension).
+	fz_write_pam: Save a pixmap as a PAM image file.
 */
 void fz_write_pam(fz_context *ctx, fz_pixmap *pixmap, char *filename, int savealpha);
 
+void fz_output_pam(fz_context *ctx, fz_output *out, fz_pixmap *pixmap, int savealpha);
 void fz_output_pam_header(fz_context *ctx, fz_output *out, int w, int h, int n, int savealpha);
 void fz_output_pam_band(fz_context *ctx, fz_output *out, int w, int h, int n, int band, int bandheight, unsigned char *sp, int savealpha);
 
 /*
-	fz_write_pbm: Save a bitmap as a pbm
-
-	filename: The filename to save as (including extension).
+	fz_write_pbm: Save a bitmap as a PBM image file.
 */
 void fz_write_pbm(fz_context *ctx, fz_bitmap *bitmap, char *filename);
+
+void fz_output_pbm(fz_context *ctx, fz_output *out, fz_bitmap *bitmap);
 
 #endif

include/mupdf/fitz/output.h
@@ -4,6 +4,7 @@
 #include "mupdf/fitz/system.h"
 #include "mupdf/fitz/context.h"
 #include "mupdf/fitz/buffer.h"
+#include "mupdf/fitz/string.h"
 
 /*
 	Generic output streams - generalise between outputting to a file,
@@ -12,52 +13,47 @@
 typedef struct fz_output_s fz_output;
 
 /*
-	fz_new_output_with_file: Open an output stream onto a FILE *.
-
-	The stream does NOT take ownership of the FILE *.
+	fz_new_output_with_file: Open an output stream that writes to a FILE *.
+	fz_new_output_with_path: Open an output stream that writes to a file.
+	fz_new_output_with_buffer: Open an output stream that writes into a buffer.
 */
-fz_output *fz_new_output_with_file(fz_context *, FILE *, int close);
-
-/*
-	fz_new_output_to_filename: Open an output stream to a filename.
-*/
-fz_output *fz_new_output_to_filename(fz_context *, const char *filename);
-
-/*
-	fz_new_output_with_buffer: Open an output stream onto a buffer.
-
-	The stream does NOT take ownership of the buffer.
-*/
+fz_output *fz_new_output_with_file_ptr(fz_context *, FILE *, int close);
+fz_output *fz_new_output_with_path(fz_context *, const char *filename, int append);
 fz_output *fz_new_output_with_buffer(fz_context *, fz_buffer *);
 
 /*
-	fz_printf: fprintf equivalent for output streams.
-*/
-int fz_printf(fz_context *, fz_output *, const char *, ...);
-
-/*
-	fz_puts: fputs equivalent for output streams.
-*/
-int fz_puts(fz_context *, fz_output *, const char *);
-
-/*
 	fz_write: fwrite equivalent for output streams.
+	fz_printf: fprintf equivalent for output streams. See fz_snprintf.
+	fz_vprintf: vfprintf equivalent for output streams. See fz_vsnprintf.
+	fz_puts: fputs equivalent for output streams.
+	fz_putc: fputc equivalent for output streams.
+	fz_putrune: fputrune equivalent for output streams.
 */
+void fz_printf(fz_context *ctx, fz_output *out, const char *fmt, ...);
+void fz_vprintf(fz_context *ctx, fz_output *out, const char *fmt, va_list ap);
 int fz_write(fz_context *, fz_output *out, const void *data, int len);
+#define fz_puts(C,O,S) fz_write(C, O, (S), strlen(S))
+#define fz_putc(C,O,B) fz_write_byte(C, O, B)
+#define fz_putrune(C,O,R) fz_write_rune(C, O, R)
 
 /*
-	fz_putc: putc equivalent for output streams.
+	fz_seek_output: Seek to the specified file position. Throw an error on unseekable outputs.
+	fz_tell_output: Return the current file position. Throw an error on unseekable outputs.
 */
-void fz_putc(fz_context *, fz_output *out, char c);
+void fz_seek_output(fz_context *ctx, fz_output *out, fz_off_t off, int whence);
+fz_off_t fz_tell_output(fz_context *ctx, fz_output *out);
 
 /*
-	fz_drop_output: Close a previously opened fz_output stream.
-
-	Note: whether or not this closes the underlying output method is
-	method dependent. FILE * streams created by fz_new_output_with_file
-	are NOT closed.
+	fz_drop_output: Close and free an output stream.
 */
 void fz_drop_output(fz_context *, fz_output *);
+
+/*
+	fz_write_int32be: Write a big-endian 32-bit binary integer.
+	fz_write_int32le: Write a little-endian 32-bit binary integer.
+	fz_write_byte: Write a single byte.
+	fz_write_rune: Write a UTF-8 encoded unicode character.
+*/
 
 static inline int fz_write_int32be(fz_context *ctx, fz_output *out, int x)
 {
@@ -83,20 +79,19 @@ static inline int fz_write_int32le(fz_context *ctx, fz_output *out, int x)
 	return fz_write(ctx, out, data, 4);
 }
 
-static inline void
-fz_write_byte(fz_context *ctx, fz_output *out, int x)
+static inline void fz_write_byte(fz_context *ctx, fz_output *out, int x)
 {
 	char data = x;
 
 	fz_write(ctx, out, &data, 1);
 }
 
-/*
-	fz_vfprintf: Our customised vfprintf routine. Same supported
-	format specifiers as for fz_vsnprintf.
-*/
-int fz_vfprintf(fz_context *ctx, FILE *file, const char *fmt, va_list ap);
-int fz_fprintf(fz_context *ctx, FILE *file, const char *fmt, ...);
+static inline void fz_write_rune(fz_context *ctx, fz_output *out, int rune)
+{
+	char data[10];
+
+	fz_write(ctx, out, data, fz_runetochar(data, rune));
+}
 
 /*
 	fz_vsnprintf: Our customised vsnprintf routine. Takes %c, %d, %o, %s, %u, %x, as usual.

commit baafc0a167221f60c584923b6dc294801c959f45

Re-add line endings for mutool show grep.
Commit c22e6a6dc2bf6acbac955bd5fbdd896329dfd725
accidentally removed the line endings for:
mutool show file.pdf grep

source/tools/pdfshow.c
@@ -180,6 +180,7 @@ static void showgrep(char *filename)
 
 			fz_printf(ctx, out, "%s:%d: ", filename, i);
 			pdf_print_obj(ctx, out, obj, 1);
+			fz_printf(ctx, out, "\n");
 
 			pdf_drop_obj(ctx, obj);
 		}
@@ -187,6 +188,7 @@ static void showgrep(char *filename)
 
 	fz_printf(ctx, out, "%s:trailer: ", filename);
 	pdf_print_obj(ctx, out, pdf_trailer(ctx, doc), 1);
+	fz_printf(ctx, out, "\n");
 }
 
 static void showoutline(void)