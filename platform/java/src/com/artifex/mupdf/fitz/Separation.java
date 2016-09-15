bcaf68e8838f8ef9a656a57c9146ce66aca35f08
Sebastian Rasmussen <sebras@gmail.com>
2016-09-15 12:56:11 +0800

    JNI: Rename member variable to fit with usage.
    Page_getSeparation() does the corresponding conversion.

--- platform/java/src/com/artifex/mupdf/fitz/Separation.java
+++ platform/java/src/com/artifex/mupdf/fitz/Separation.java
@@ -3,13 +3,13 @@ package com.artifex.mupdf.fitz;
 public class Separation
 {
 	public String name;
-	public int rgba;
+	public int bgra;
 	public int cmyk;
 
-	public Separation(String name, int rgba, int cmyk)
+	public Separation(String name, int bgra, int cmyk)
 	{
 		this.name = name;
-		this.rgba = rgba;
+		this.bgra = bgra;
 		this.cmyk = cmyk;
 	}
 }
