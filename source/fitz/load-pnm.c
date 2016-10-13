ArtifexSoftware/mupdf

commit c2acdba7e9b72e18afdd3aa8dfdf66088b1ea1be

pnm: Interpret b/w image colors correctly.

source/fitz/load-pnm.c
@@ -182,9 +182,9 @@ pnm_ascii_read_image(fz_context *ctx, struct info *pnm, unsigned char *p, unsign
 					p = pnm_read_number(ctx, p, e, &v);
 					p = pnm_read_white(ctx, p, e, 0);
 					if (v == 0)
-						*dp = 0x00;
-					else
 						*dp = 0xff;
+					else
+						*dp = 0x00;
 
 					dp++;
 				}
@@ -248,9 +248,9 @@ pnm_binary_read_image(fz_context *ctx, struct info *pnm, unsigned char *p, unsig
 				for (x = 0; x < pnm->width; x++)
 				{
 					if (*p & (1 << (7 - (x & 0x7))))
-						*dp = 0x00;
-					else
 						*dp = 0xff;
+					else
+						*dp = 0x00;
 
 					dp++;
 					if ((x & 0x7) == 7)

commit 18f07aea2f40164b3da68f155be1303d09fd2741

pnm: Interpret binary b/w image samples correctly.
Commit c2acdba7e9b72e18afdd3aa8dfdf66088b1ea1be mistakenly inverted
the interpretation of samples for both both binary and ascii b/w PNM
images when, at the time, there was only a mistake for ascii images.

source/fitz/load-pnm.c
@@ -354,7 +354,7 @@ pnm_binary_read_image(fz_context *ctx, struct info *pnm, unsigned char *p, unsig
 			{
 				for (x = 0; x < w; x++)
 				{
-					*dp++ = (*p & (1 << (7 - (x & 0x7)))) ? 0xff : 0x00;
+					*dp++ = (*p & (1 << (7 - (x & 0x7)))) ? 0x00 : 0xff;
 					if ((x & 0x7) == 7)
 						p++;
 				}