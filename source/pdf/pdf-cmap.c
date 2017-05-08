181b2b135ce6527aa8a86da9bd0cdf41c2f68ca9
Robin Watts <robin.watts@artifex.com>
2017-05-09 00:30:12 +0100

    Fix leak of dictionary in CMAP.

--- source/pdf/pdf-cmap.c
+++ source/pdf/pdf-cmap.c
@@ -19,6 +19,7 @@ pdf_drop_cmap_imp(fz_context *ctx, fz_storable *cmap_)
 	fz_free(ctx, cmap->ranges);
 	fz_free(ctx, cmap->xranges);
 	fz_free(ctx, cmap->mranges);
+	fz_free(ctx, cmap->dict);
 	fz_free(ctx, cmap);
 }
 
