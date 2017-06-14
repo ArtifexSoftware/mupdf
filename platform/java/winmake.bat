a1857dfdc5d4b7988b19e59092dc5a80794569d4
Robin Watts <robin.watts@artifex.com>
2017-06-14 19:49:07 +0100

    platform/java/winmake.bat - Fix due to sources being moved.
    The java sources were pushed down into 'src' which broke the
    .bat file.

--- platform/java/winmake.bat
+++ platform/java/winmake.bat
@@ -1,17 +1,18 @@
+@echo off
 @echo Cleaning
 echo bogus > example\bogus.class
 del /Q example\*.class
-echo bogus > com\artifex\mupdf\fitz\bogus.class
-del /Q com\artifex\mupdf\fitz\*.class
+echo bogus > src\com\artifex\mupdf\fitz\bogus.class
+del /Q src\com\artifex\mupdf\fitz\*.class
 
 @echo Building Viewer
-javac -source 1.7 -target 1.7 example/Viewer.java
+javac -classpath src -sourcepath src -source 1.7 -target 1.7 example/Viewer.java
 
 @echo Building JNI classes
-javac -source 1.7 -target 1.7 com/artifex/mupdf/fitz/*.java
+javac -sourcepath src -source 1.7 -target 1.7 src/com/artifex/mupdf/fitz/*.java
 
 @echo Importing DLL (built using VS solution)
 @copy ..\win32\%1\javaviewerlib.dll mupdf_java.dll /y
 
 @echo Packaging into jar (incomplete as missing manifest)
-jar cf mupdf-java-viewer.jar mupdf_java.dll com\artifex\mupdf\fitz\*.class example\*.class
+jar cf mupdf-java-viewer.jar mupdf_java.dll src\com\artifex\mupdf\fitz\*.class example\*.class
