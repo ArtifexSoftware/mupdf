afcbd53de886f9ceb96f6a795d3f1e45a4e0f8f7
Tor Andersson <tor.andersson@artifex.com>
2017-06-21 15:24:10 +0200

    Print full md5 checksums in link key debug prints.

--- source/fitz/colorspace.c
+++ source/fitz/colorspace.c
@@ -176,9 +176,20 @@ fz_cmp_link_key(fz_context *ctx, void *k0_, void *k1_)
 static void
 fz_format_link_key(fz_context *ctx, char *s, int n, void *key_)
 {
+	static const char *hex = "0123456789abcdef";
 	fz_link_key *key = (fz_link_key *)key_;
-	fz_snprintf(s, n, "(link src_md5[%d %d %d %d] dst_md5[%d %d %d %d]) ",
-		key->src_md5[0], key->src_md5[1], key->src_md5[2], key->src_md5[3], key->dst_md5[0], key->dst_md5[1], key->dst_md5[2], key->dst_md5[3]);
+	char sm[33], dm[33];
+	int i;
+	for (i = 0; i < 16; ++i)
+	{
+		sm[i*2+0] = hex[key->src_md5[i]>>4];
+		sm[i*2+1] = hex[key->src_md5[i]&15];
+		dm[i*2+0] = hex[key->dst_md5[i]>>4];
+		dm[i*2+1] = hex[key->dst_md5[i]&15];
+	}
+	sm[32] = 0;
+	dm[32] = 0;
+	fz_snprintf(s, n, "(link src_md5=%s dst_md5=%s)", sm, dm);
 }
 
 static int
