5b506261a266b1843302d7f0a74222671495feaa
Robin Watts <robin.watts@artifex.com>
2017-07-03 18:58:55 +0100

    Don't leak cmap->tree if we abort cmap loading before sort.

--- source/pdf/pdf-cmap.c
+++ source/pdf/pdf-cmap.c
@@ -20,6 +20,7 @@ pdf_drop_cmap_imp(fz_context *ctx, fz_storable *cmap_)
 	fz_free(ctx, cmap->xranges);
 	fz_free(ctx, cmap->mranges);
 	fz_free(ctx, cmap->dict);
+	fz_free(ctx, cmap->tree);
 	fz_free(ctx, cmap);
 }
 
