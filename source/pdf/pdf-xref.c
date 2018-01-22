d9bc8c6f7fb2e3ec7035bebaaee0edcf59287705
Sebastian Rasmussen <sebras@gmail.com>
2018-01-22 17:56:20 +0100

    Bug 698885: When parsing PDF version, make sure to initialize buffer.
    Thanks to oss-fuzz for reporting this.

--- source/pdf/pdf-xref.c
+++ source/pdf/pdf-xref.c
@@ -590,7 +590,7 @@ pdf_load_version(fz_context *ctx, pdf_document *doc)
 
 	fz_seek(ctx, doc->file, 0, SEEK_SET);
 	fz_read_line(ctx, doc->file, buf, sizeof buf);
-	if (memcmp(buf, "%PDF-", 5) != 0)
+	if (strlen(buf) < 5 || memcmp(buf, "%PDF-", 5) != 0)
 		fz_throw(ctx, FZ_ERROR_GENERIC, "cannot recognize version marker");
 
 	doc->version = 10 * (fz_atof(buf+5) + 0.05f);
