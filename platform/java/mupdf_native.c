e9349d8fc30de9871698e13fd35d61b457cccbc5
Tor Andersson <tor.andersson@artifex.com>
2018-03-12 14:48:10 +0100

    jni: Temporary fix to make sure annot AP field is updated.

--- platform/java/mupdf_native.c
+++ platform/java/mupdf_native.c
@@ -9093,7 +9093,11 @@ FUN(PDFAnnotation_updateAppearance)(JNIEnv *env, jobject self)
 	if (!ctx || !annot) return;
 
 	fz_try(ctx)
+	{
+		pdf_dict_del(ctx, annot->obj, PDF_NAME_AP); /* nuke old AP */
 		pdf_update_appearance(ctx, annot->page->doc, annot);
+		pdf_update_annot(ctx, annot); /* ensure new AP is put into annot */
+	}
 	fz_catch(ctx)
 		jni_rethrow(env, ctx);
 }
