cd0632e90df78ca269f56bccfd3019c292f9c315
Tor Andersson <tor.andersson@artifex.com>
2018-05-09 13:04:17 +0200

    Remove incorrect assertion.
    A 'null' pdf_obj is also NULL.

--- source/pdf/pdf-xref.c
+++ source/pdf/pdf-xref.c
@@ -2037,7 +2037,6 @@ pdf_obj *
 pdf_load_object(fz_context *ctx, pdf_document *doc, int num)
 {
 	pdf_xref_entry *entry = pdf_cache_object(ctx, doc, num);
-	assert(entry->obj != NULL);
 	return pdf_keep_obj(ctx, entry->obj);
 }
 
