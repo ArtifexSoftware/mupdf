fd23f7d93704d1ba0b690c264a6618311392f3ec
Sebastian Rasmussen <sebras@gmail.com>
2018-09-05 17:46:18 +0800

    jni: Remove redundant public access modifier.
    Abstract methods in interfaces offer no implementation and are
    implicitly declared public, making a public access modifier
    redundant.

--- platform/java/src/com/artifex/mupdf/fitz/PathWalker.java
+++ platform/java/src/com/artifex/mupdf/fitz/PathWalker.java
@@ -2,8 +2,8 @@ package com.artifex.mupdf.fitz;
 
 public interface PathWalker
 {
-	public void moveTo(float x, float y);
-	public void lineTo(float x, float y);
-	public void curveTo(float cx1, float cy1, float cx2, float cy2, float ex, float ey);
-	public void closePath();
+	void moveTo(float x, float y);
+	void lineTo(float x, float y);
+	void curveTo(float cx1, float cy1, float cx2, float cy2, float ex, float ey);
+	void closePath();
 }
