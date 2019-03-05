787adf30a9edb36b1fb3cdcd30f14ef7cb83b5b1
Sebastian Rasmussen <sebras@gmail.com>
2019-03-05 21:51:08 +0800

    Bug 700714: Make function stubs prototypes identical.
    This resolves a compilation error when js is disabled.

--- source/pdf/pdf-js.c
+++ source/pdf/pdf-js.c
@@ -970,11 +970,11 @@ void pdf_drop_js(fz_context *ctx, pdf_js *js) { }
 void pdf_enable_js(fz_context *ctx, pdf_document *doc) { }
 void pdf_disable_js(fz_context *ctx, pdf_document *doc) { }
 int pdf_js_supported(fz_context *ctx, pdf_document *doc) { return 0; }
-void pdf_js_event_init(pdf_js *js, pdf_obj *target, const char *value) { }
-void pdf_js_event_init_keystroke(pdf_js *js, pdf_obj *target, const char *value, pdf_keystroke_event *evt) { }
-void pdf_js_event_result_keystroke(pdf_js *js, pdf_keystroke_event *evt) { }
+void pdf_js_event_init(pdf_js *js, pdf_obj *target, const char *value, int willCommit) { }
+void pdf_js_event_init_keystroke(pdf_js *js, pdf_obj *target, pdf_keystroke_event *event) { }
+int pdf_js_event_result_keystroke(pdf_js *js, pdf_keystroke_event *evt) { return 1; }
 int pdf_js_event_result(pdf_js *js) { return 1; }
 char *pdf_js_event_value(pdf_js *js) { return ""; }
-void pdf_js_execute(pdf_js *js, const char *name, char *code) { }
+void pdf_js_execute(pdf_js *js, const char *name, const char *source) { }
 
 #endif /* FZ_ENABLE_JS */
