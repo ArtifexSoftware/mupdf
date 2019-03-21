03fd6221c10a5e66cac029b099ab29d7db7bb0e0
Sebastian Rasmussen <sebras@gmail.com>
2019-03-22 03:06:11 +0800

    Free the correct colorspace pointer.

--- source/fitz/pixmap.c
+++ source/fitz/pixmap.c
@@ -105,7 +105,7 @@ fz_new_pixmap_with_data(fz_context *ctx, fz_colorspace *colorspace, int w, int h
 		}
 		fz_catch(ctx)
 		{
-			fz_drop_colorspace(ctx, colorspace);
+			fz_drop_colorspace(ctx, pix->colorspace);
 			fz_free(ctx, pix);
 			fz_rethrow(ctx);
 		}
