c38b438ac05491302b45451cbdef2aaae0927e7d
Tor Andersson <tor.andersson@artifex.com>
2019-04-17 11:41:01 +0200

    Bump version number.

--- include/mupdf/fitz/version.h
+++ include/mupdf/fitz/version.h
@@ -1,9 +1,9 @@
 #ifndef MUPDF_FITZ_VERSION_H
 #define MUPDF_FITZ_VERSION_H
 #ifndef FZ_VERSION
-#define FZ_VERSION "1.14.0"
+#define FZ_VERSION "1.15.0"
 #define FZ_VERSION_MAJOR 1
-#define FZ_VERSION_MINOR 14
+#define FZ_VERSION_MINOR 15
 #define FZ_VERSION_PATCH 0
 #endif
 #endif
