265ffe80390ccb87b262a0f1046d042b3c8d6627
Tor Andersson <tor@ccxvii.net>
2019-04-18 12:06:20 +0200

    Fix scaling of Lab values for LCMS.

--- source/fitz/colorspace.c
+++ source/fitz/colorspace.c
@@ -3237,8 +3237,8 @@ static void
 clamp_lab_icc(fz_colorspace *cs, const float *src, float *dst)
 {
 	dst[0] = (fz_clamp(src[0], 0, 100)) / 100.0f;
-	dst[1] = (fz_clamp(src[1], -128, 127) + 128.0f) / 256;
-	dst[2] = (fz_clamp(src[2], -128, 127) + 128.0f) / 256;
+	dst[1] = (fz_clamp(src[1], -128, 127) + 128.0f) / 255;
+	dst[2] = (fz_clamp(src[2], -128, 127) + 128.0f) / 255;
 }
 
 /* Embedded icc profiles could have different range */
