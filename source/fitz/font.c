db8f77b7b25c4b66e31cfcd3bd3b78f2cde52afa
Sebastian Rasmussen <sebras@gmail.com>
2019-07-04 12:49:46 +0200

    Drop music/math fonts cached in font context.
    Commit b330704579c6ae6af9bda24b375a98cdcb55d6d9 added the new
    music/math fonts to the font context, but the drop function for
    the font context was never updated to also drop them.

--- source/fitz/font.c
+++ source/fitz/font.c
@@ -399,6 +399,8 @@ void fz_drop_font_context(fz_context *ctx)
 		}
 		fz_drop_font(ctx, ctx->font->symbol1);
 		fz_drop_font(ctx, ctx->font->symbol2);
+		fz_drop_font(ctx, ctx->font->math);
+		fz_drop_font(ctx, ctx->font->music);
 		fz_drop_font(ctx, ctx->font->emoji);
 		fz_free(ctx, ctx->font);
 		ctx->font = NULL;
