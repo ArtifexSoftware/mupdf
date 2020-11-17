7217c53940b098d2a48e4e3bc00c1e19cacd8283
Robin Watts <Robin.Watts@artifex.com>
2020-11-17 19:02:02 +0000

    Coverity 207650 (and others): unchecked return value.
    We normally check the return value to pdf_obj_mark. In some
    circumstances (namely where we have checked in advance that the
    object is not marked) we don't need to check it. Indicate that
    by casting to void.

--- source/pdf/pdf-xref.c
+++ source/pdf/pdf-xref.c
@@ -1705,7 +1705,7 @@ pdf_load_obj_stm(fz_context *ctx, pdf_document *doc, int num, pdf_lexbuf *buf, i
 
 	fz_try(ctx)
 	{
-		pdf_mark_obj(ctx, objstm);
+		(void)pdf_mark_obj(ctx, objstm);
 
 		count = pdf_dict_get_int(ctx, objstm, PDF_NAME(N));
 		first = pdf_dict_get_int(ctx, objstm, PDF_NAME(First));
@@ -3123,8 +3123,8 @@ check_unchanged_between(fz_context *ctx, pdf_document *doc, pdf_changes *changes
 				/* Different objects, so lock them */
 				if (!pdf_obj_marked(ctx, nobj) && !pdf_obj_marked(ctx, oobj))
 				{
-					pdf_mark_obj(ctx, nobj);
-					pdf_mark_obj(ctx, oobj);
+					(void)pdf_mark_obj(ctx, nobj);
+					(void)pdf_mark_obj(ctx, oobj);
 					marked = 1;
 				}
 			}
@@ -3526,7 +3526,7 @@ check_field(fz_context *ctx, pdf_document *doc, pdf_changes *changes, pdf_obj *o
 		pdf_obj *t;
 		int is_locked;
 
-		pdf_mark_obj(ctx, obj);
+		(void)pdf_mark_obj(ctx, obj);
 
 		/* Do this within the try, so we can catch any problems */
 		doc->xref_base = o_xref_base+1;
@@ -3834,7 +3834,7 @@ find_locked_fields_aux(fz_context *ctx, pdf_obj *field, pdf_locked_fields *field
 	{
 		pdf_obj *kids, *v, *ft;
 
-		pdf_mark_obj(ctx, field);
+		(void)pdf_mark_obj(ctx, field);
 
 		v = pdf_dict_get(ctx, field, PDF_NAME(V));
 		if (v == NULL)
