454e89af28789a0d5d358668ef2c75ec113910c7
Julian Smith <julian.smith@artifex.com>
2020-12-16 14:20:51 +0000

    scripts/mupdfwrap.py: on OpenBSD, allow use of Python API without setting LD_LIBRARY_PATH.
    On OpenBSD we can load mupdf.so etc by passing absolute paths to
    ctypes.cdll.LoadLibrary(). Unfortunately this does not seem to work on linux.

--- scripts/mupdfwrap.py
+++ scripts/mupdfwrap.py
@@ -5013,11 +5013,37 @@ def build_swig( build_dirs, container_classnames, language='python', swig='swig'
         swig_cpp_old = swig_cpp + '-0'
         shutil.copy2(swig_cpp, swig_cpp_old)
 
+    # We will be modifying the generated mupdf.py, so need to always remove old
+    # one.
+    jlib.remove(swig_py)
+
     jlib.build(
             (swig_i, include1, include2),
             (swig_cpp, swig_py),
             command,
             )
+
+    mupdf_py_prefix = ''
+    if os.uname()[0] == 'OpenBSD':
+        mupdf_py_prefix = textwrap.dedent(
+                f'''
+                # Explicitly load required .so's using absolute paths, so that we
+                # work without needing LD_LIBRARY_PATH to be defined.
+                #
+                import ctypes
+                import os
+                import importlib
+                for leaf in ('libmupdf.so', 'libmupdfcpp.so', '_mupdf.so'):
+                    path = os.path.abspath(f'{{__file__}}/../{{leaf}}')
+                    print(f'path={{path}}')
+                    print(f'exists={{os.path.exists(path)}}')
+                    ctypes.cdll.LoadLibrary( path)
+                    print(f'have loaded {{path}}')
+                ''')
+    with open( swig_py) as f:
+        mupdf_py_content = mupdf_py_prefix + f.read()
+    with open( swig_py, 'w') as f:
+        f.write( mupdf_py_content)
 
     if swig_cpp_old:
         def read_all(path):
@@ -5555,7 +5581,11 @@ def main():
                 ld_library_path = os.path.abspath( f'{build_dirs.dir_so}')
                 pythonpath = build_dirs.dir_so
 
-                envs = f'LD_LIBRARY_PATH={ld_library_path} PYTHONPATH={pythonpath}'
+                if os.uname()[0] == 'OpenBSD':
+                    # We have special support to not require LD_LIBRARY_PATH.
+                    envs = f'PYTHONPATH={pythonpath}'
+                else:
+                    envs = f'LD_LIBRARY_PATH={ld_library_path} PYTHONPATH={pythonpath}'
 
                 log( 'running mupdf_test.py...')
 
