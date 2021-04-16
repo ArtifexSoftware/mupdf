cf85d82005be683105bbb5e10a713bac0c3130e3
Tor Andersson <tor.andersson@artifex.com>
2021-04-16 12:27:35 +0200

    Bug 703789: Avoid revisiting all outline nodes!
    Unmark outlines at the *end* of loading outlines, not in the middle.
    This prevents us from revisiting outlines that have already been loaded
    in another part of the tree in degenerate outline structures.

--- source/pdf/pdf-outline.c
+++ source/pdf/pdf-outline.c
@@ -2,13 +2,11 @@
 #include "mupdf/pdf.h"
 
 static fz_outline *
-pdf_load_outline_imp(fz_context *ctx, pdf_document *doc, pdf_obj *dict)
+pdf_load_outline_imp(fz_context *ctx, pdf_document *doc, pdf_obj *dict, pdf_obj *mark_list)
 {
 	fz_outline *node, **prev, *first = NULL;
 	pdf_obj *obj;
-	pdf_obj *odict = dict;
 
-	fz_var(dict);
 	fz_var(first);
 
 	fz_try(ctx)
@@ -18,6 +16,8 @@ pdf_load_outline_imp(fz_context *ctx, pdf_document *doc, pdf_obj *dict)
 		{
 			if (pdf_mark_obj(ctx, dict))
 				break;
+			pdf_array_push(ctx, mark_list, dict);
+
 			node = fz_new_outline(ctx);
 			*prev = node;
 			prev = &node->next;
@@ -41,7 +41,7 @@ pdf_load_outline_imp(fz_context *ctx, pdf_document *doc, pdf_obj *dict)
 			obj = pdf_dict_get(ctx, dict, PDF_NAME(First));
 			if (obj)
 			{
-				node->down = pdf_load_outline_imp(ctx, doc, obj);
+				node->down = pdf_load_outline_imp(ctx, doc, obj, mark_list);
 
 				obj = pdf_dict_get(ctx, dict, PDF_NAME(Count));
 				if (pdf_to_int(ctx, obj) > 0)
@@ -50,11 +50,6 @@ pdf_load_outline_imp(fz_context *ctx, pdf_document *doc, pdf_obj *dict)
 
 			dict = pdf_dict_get(ctx, dict, PDF_NAME(Next));
 		}
-	}
-	fz_always(ctx)
-	{
-		for (dict = odict; dict && pdf_obj_marked(ctx, dict); dict = pdf_dict_get(ctx, dict, PDF_NAME(Next)))
-			pdf_unmark_obj(ctx, dict);
 	}
 	fz_catch(ctx)
 	{
@@ -68,22 +63,37 @@ pdf_load_outline_imp(fz_context *ctx, pdf_document *doc, pdf_obj *dict)
 fz_outline *
 pdf_load_outline(fz_context *ctx, pdf_document *doc)
 {
-	pdf_obj *root, *obj, *first;
+	pdf_obj *root, *obj, *first, *mark_list;
 	fz_outline *outline = NULL;
+	int i;
 
-	root = pdf_dict_get(ctx, pdf_trailer(ctx, doc), PDF_NAME(Root));
-	obj = pdf_dict_get(ctx, root, PDF_NAME(Outlines));
-	first = pdf_dict_get(ctx, obj, PDF_NAME(First));
-	if (first)
+	mark_list = pdf_new_array(ctx, doc, 100);
+	fz_try(ctx)
 	{
-		/* cache page tree for fast link destination lookups */
-		pdf_load_page_tree(ctx, doc);
-		fz_try(ctx)
-			outline = pdf_load_outline_imp(ctx, doc, first);
-		fz_always(ctx)
-			pdf_drop_page_tree(ctx, doc);
-		fz_catch(ctx)
-			fz_rethrow(ctx);
+		root = pdf_dict_get(ctx, pdf_trailer(ctx, doc), PDF_NAME(Root));
+		obj = pdf_dict_get(ctx, root, PDF_NAME(Outlines));
+		first = pdf_dict_get(ctx, obj, PDF_NAME(First));
+		if (first)
+		{
+			/* cache page tree for fast link destination lookups */
+			pdf_load_page_tree(ctx, doc);
+			fz_try(ctx)
+				outline = pdf_load_outline_imp(ctx, doc, first, mark_list);
+			fz_always(ctx)
+				pdf_drop_page_tree(ctx, doc);
+			fz_catch(ctx)
+				fz_rethrow(ctx);
+		}
+	}
+	fz_always(ctx)
+	{
+		for (i = 0; i < pdf_array_len(ctx, mark_list); ++i)
+			pdf_unmark_obj(ctx, pdf_array_get(ctx, mark_list, i));
+		pdf_drop_obj(ctx, mark_list);
+	}
+	fz_catch(ctx)
+	{
+		fz_rethrow(ctx);
 	}
 
 	return outline;
