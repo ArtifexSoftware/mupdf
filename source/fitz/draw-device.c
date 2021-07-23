ArtifexSoftware/mupdf

commit b1e505d1824d4356e48ce9e11cc5a02d6fa785dc

Tweak where we gridfit images, so it happens before subarea.
Currently, we find any subarea of an image we can restrict our
rendering too, decode that area, and then grid fit that reduced
area. This means that rendering in tiles, we might get different
results to rendering the whole page - something that really ought
not to happen.

Move the gridfitting out of fz_paint_image (and
fz_paint_image_with_color) into the callers, before the subarea
calculations are done.

This restores sanity.

source/fitz/draw-device.c
@@ -1034,7 +1034,8 @@ fz_draw_fill_text(fz_context *ctx, fz_device *devp, const fz_text *text, fz_matr
 					fz_matrix mat;
 					mat.a = pixmap->w; mat.b = mat.c = 0; mat.d = pixmap->h;
 					mat.e = x + pixmap->x; mat.f = y + pixmap->y;
-					fz_paint_image(ctx, state->dest, &state->scissor, state->shape, state->group_alpha, pixmap, mat, alpha * 255, !(devp->hints & FZ_DONT_INTERPOLATE_IMAGES), devp->flags & FZ_DEVFLAG_GRIDFIT_AS_TILED, eop);
+					mat = fz_gridfit_matrix(devp->flags & FZ_DEVFLAG_GRIDFIT_AS_TILED, mat);
+					fz_paint_image(ctx, state->dest, &state->scissor, state->shape, state->group_alpha, pixmap, mat, alpha * 255, !(devp->hints & FZ_DONT_INTERPOLATE_IMAGES), eop);
 				}
 				fz_drop_glyph(ctx, glyph);
 			}
@@ -1123,7 +1124,8 @@ fz_draw_stroke_text(fz_context *ctx, fz_device *devp, const fz_text *text, const
 					fz_matrix mat;
 					mat.a = pixmap->w; mat.b = mat.c = 0; mat.d = pixmap->h;
 					mat.e = x + pixmap->x; mat.f = y + pixmap->y;
-					fz_paint_image(ctx, state->dest, &state->scissor, state->shape, state->group_alpha, pixmap, mat, alpha * 255, !(devp->hints & FZ_DONT_INTERPOLATE_IMAGES), devp->flags & FZ_DEVFLAG_GRIDFIT_AS_TILED, eop);
+					mat = fz_gridfit_matrix(devp->flags & FZ_DEVFLAG_GRIDFIT_AS_TILED, mat);
+					fz_paint_image(ctx, state->dest, &state->scissor, state->shape, state->group_alpha, pixmap, mat, alpha * 255, !(devp->hints & FZ_DONT_INTERPOLATE_IMAGES), eop);
 				}
 				fz_drop_glyph(ctx, glyph);
 			}
@@ -1680,37 +1682,11 @@ convert_pixmap_for_painting(fz_context *ctx, fz_pixmap *pixmap, fz_colorspace *m
 	return converted;
 }
 
-static void
-fz_draw_fill_image(fz_context *ctx, fz_device *devp, fz_image *image, fz_matrix in_ctm, float alpha, fz_color_params color_params)
+static fz_irect
+find_src_area_required(fz_matrix local_ctm, fz_image *image, fz_irect clip)
 {
-	fz_draw_device *dev = (fz_draw_device*)devp;
-	fz_matrix local_ctm = fz_concat(in_ctm, dev->transform);
-	fz_pixmap *pixmap;
-	int after;
-	int dx, dy;
-	fz_draw_state *state = &dev->stack[dev->top];
-	fz_colorspace *model;
-	fz_irect clip;
 	fz_matrix inverse;
 	fz_irect src_area;
-	fz_colorspace *src_cs;
-	fz_overprint op = { { 0 } };
-	fz_overprint *eop = &op;
-
-	if (alpha == 0)
-		return;
-
-	if (dev->top == 0 && dev->resolve_spots)
-		state = push_group_for_separations(ctx, dev, color_params, dev->default_cs);
-	model = state->dest->colorspace;
-
-	clip = fz_intersect_irect(fz_pixmap_bbox(ctx, state->dest), state->scissor);
-
-	if (image->w == 0 || image->h == 0 || fz_is_empty_irect(clip))
-		return;
-
-	if (color_params.op == 0)
-		eop = NULL;
 
 	/* ctm maps the image (expressed as the unit square) onto the
 	 * destination device. Reverse that to get a mapping from
@@ -1743,9 +1719,47 @@ fz_draw_fill_image(fz_context *ctx, fz_device *devp, fz_image *image, fz_matrix 
 		sane.x1 = image->w;
 		sane.y1 = image->h;
 		src_area = fz_intersect_irect(src_area, sane);
-		if (fz_is_empty_irect(src_area))
-			return;
 	}
+
+	return src_area;
+}
+
+static void
+fz_draw_fill_image(fz_context *ctx, fz_device *devp, fz_image *image, fz_matrix in_ctm, float alpha, fz_color_params color_params)
+{
+	fz_draw_device *dev = (fz_draw_device*)devp;
+	fz_matrix local_ctm = fz_concat(in_ctm, dev->transform);
+	fz_pixmap *pixmap;
+	int after;
+	int dx, dy;
+	fz_draw_state *state = &dev->stack[dev->top];
+	fz_colorspace *model;
+	fz_irect clip;
+	fz_irect src_area;
+	fz_colorspace *src_cs;
+	fz_overprint op = { { 0 } };
+	fz_overprint *eop = &op;
+
+	if (alpha == 0)
+		return;
+
+	if (dev->top == 0 && dev->resolve_spots)
+		state = push_group_for_separations(ctx, dev, color_params, dev->default_cs);
+	model = state->dest->colorspace;
+
+	clip = fz_intersect_irect(fz_pixmap_bbox(ctx, state->dest), state->scissor);
+
+	if (image->w == 0 || image->h == 0 || fz_is_empty_irect(clip))
+		return;
+
+	if (color_params.op == 0)
+		eop = NULL;
+
+	local_ctm = fz_gridfit_matrix(devp->flags & FZ_DEVFLAG_GRIDFIT_AS_TILED, local_ctm);
+
+	src_area = find_src_area_required(local_ctm, image, clip);
+	if (fz_is_empty_irect(src_area))
+		return;
 
 	pixmap = fz_get_pixmap_from_image(ctx, image, &src_area, &local_ctm, &dx, &dy);
 	src_cs = fz_default_colorspace(ctx, dev->default_cs, pixmap->colorspace);
@@ -1815,7 +1829,7 @@ fz_draw_fill_image(fz_context *ctx, fz_device *devp, fz_image *image, fz_matrix 
 				pixmap = convert_pixmap_for_painting(ctx, pixmap, model, src_cs, state->dest, color_params, dev, &eop);
 		}
 
-		fz_paint_image(ctx, state->dest, &state->scissor, state->shape, state->group_alpha, pixmap, local_ctm, alpha * 255, !(devp->hints & FZ_DONT_INTERPOLATE_IMAGES), devp->flags & FZ_DEVFLAG_GRIDFIT_AS_TILED, eop);
+		fz_paint_image(ctx, state->dest, &state->scissor, state->shape, state->group_alpha, pixmap, local_ctm, alpha * 255, !(devp->hints & FZ_DONT_INTERPOLATE_IMAGES), eop);
 
 		if (state->blendmode & FZ_BLEND_KNOCKOUT)
 			fz_knockout_end(ctx, dev);
@@ -1838,7 +1852,6 @@ fz_draw_fill_image_mask(fz_context *ctx, fz_device *devp, fz_image *image, fz_ma
 	int dx, dy;
 	fz_draw_state *state = &dev->stack[dev->top];
 	fz_irect clip;
-	fz_matrix inverse;
 	fz_irect src_area;
 	fz_colorspace *colorspace = NULL;
 	fz_overprint op = { { 0 } };
@@ -1859,40 +1872,11 @@ fz_draw_fill_image_mask(fz_context *ctx, fz_device *devp, fz_image *image, fz_ma
 	if (image->w == 0 || image->h == 0)
 		return;
 
-	/* ctm maps the image (expressed as the unit square) onto the
-	 * destination device. Reverse that to get a mapping from
-	 * the destination device to the source pixels. */
-	if (fz_try_invert_matrix(&inverse, local_ctm))
-	{
-		/* Not invertible. Could just bail? Use the whole image
-		 * for now. */
-		src_area.x0 = 0;
-		src_area.x1 = image->w;
-		src_area.y0 = 0;
-		src_area.y1 = image->h;
-	}
-	else
-	{
-		float exp;
-		fz_rect rect;
-		fz_irect sane;
-		/* We want to scale from image coords, not from unit square */
-		inverse = fz_post_scale(inverse, image->w, image->h);
-		/* Are we scaling up or down? exp < 1 means scaling down. */
-		exp = fz_matrix_max_expansion(inverse);
-		rect = fz_rect_from_irect(clip);
-		rect = fz_transform_rect(rect, inverse);
-		/* Allow for support requirements for scalers. */
-		rect = fz_expand_rect(rect, fz_max(exp, 1) * 4);
-		src_area = fz_irect_from_rect(rect);
-		sane.x0 = 0;
-		sane.y0 = 0;
-		sane.x1 = image->w;
-		sane.y1 = image->h;
-		src_area = fz_intersect_irect(src_area, sane);
-		if (fz_is_empty_irect(src_area))
-			return;
-	}
+	local_ctm = fz_gridfit_matrix(devp->flags & FZ_DEVFLAG_GRIDFIT_AS_TILED, local_ctm);
+
+	src_area = find_src_area_required(local_ctm, image, clip);
+	if (fz_is_empty_irect(src_area))
+		return;
 
 	pixmap = fz_get_pixmap_from_image(ctx, image, &src_area, &local_ctm, &dx, &dy);
 
@@ -1924,7 +1908,7 @@ fz_draw_fill_image_mask(fz_context *ctx, fz_device *devp, fz_image *image, fz_ma
 
 		eop = resolve_color(ctx, &op, color, colorspace, alpha, color_params, colorbv, state->dest);
 
-		fz_paint_image_with_color(ctx, state->dest, &state->scissor, state->shape, state->group_alpha, pixmap, local_ctm, colorbv, !(devp->hints & FZ_DONT_INTERPOLATE_IMAGES), devp->flags & FZ_DEVFLAG_GRIDFIT_AS_TILED, eop);
+		fz_paint_image_with_color(ctx, state->dest, &state->scissor, state->shape, state->group_alpha, pixmap, local_ctm, colorbv, !(devp->hints & FZ_DONT_INTERPOLATE_IMAGES), eop);
 
 		if (state->blendmode & FZ_BLEND_KNOCKOUT)
 			fz_knockout_end(ctx, dev);
@@ -1948,7 +1932,6 @@ fz_draw_clip_image_mask(fz_context *ctx, fz_device *devp, fz_image *image, fz_ma
 	fz_colorspace *model = state->dest->colorspace;
 	fz_irect clip;
 	fz_irect src_area;
-	fz_matrix inverse;
 
 	fz_var(pixmap);
 
@@ -1968,40 +1951,11 @@ fz_draw_clip_image_mask(fz_context *ctx, fz_device *devp, fz_image *image, fz_ma
 		return;
 	}
 
-	/* ctm maps the image (expressed as the unit square) onto the
-	 * destination device. Reverse that to get a mapping from
-	 * the destination device to the source pixels. */
-	if (fz_try_invert_matrix(&inverse, local_ctm))
-	{
-		/* Not invertible. Could just bail? Use the whole image
-		 * for now. */
-		src_area.x0 = 0;
-		src_area.x1 = image->w;
-		src_area.y0 = 0;
-		src_area.y1 = image->h;
-	}
-	else
-	{
-		float exp;
-		fz_rect rect;
-		fz_irect sane;
-		/* We want to scale from image coords, not from unit square */
-		inverse = fz_post_scale(inverse, image->w, image->h);
-		/* Are we scaling up or down? exp < 1 means scaling down. */
-		exp = fz_matrix_max_expansion(inverse);
-		rect = fz_rect_from_irect(clip);
-		rect = fz_transform_rect(rect, inverse);
-		/* Allow for support requirements for scalers. */
-		rect = fz_expand_rect(rect, fz_max(exp, 1) * 4);
-		src_area = fz_irect_from_rect(rect);
-		sane.x0 = 0;
-		sane.y0 = 0;
-		sane.x1 = image->w;
-		sane.y1 = image->h;
-		src_area = fz_intersect_irect(src_area, sane);
-		if (fz_is_empty_irect(src_area))
-			return;
-	}
+	local_ctm = fz_gridfit_matrix(devp->flags & FZ_DEVFLAG_GRIDFIT_AS_TILED, local_ctm);
+
+	src_area = find_src_area_required(local_ctm, image, clip);
+	if (fz_is_empty_irect(src_area))
+		return;
 
 	bbox = fz_irect_from_rect(fz_transform_rect(fz_unit_rect, local_ctm));
 	bbox = fz_intersect_irect(bbox, state->scissor);
@@ -2076,7 +2030,7 @@ fz_draw_clip_image_mask(fz_context *ctx, fz_device *devp, fz_image *image, fz_ma
 			fz_dump_blend(ctx, "/GA=", state[1].group_alpha);
 #endif
 
-		fz_paint_image(ctx, state[1].mask, &bbox, state[1].shape, state[1].group_alpha, pixmap, local_ctm, 255, !(devp->hints & FZ_DONT_INTERPOLATE_IMAGES), devp->flags & FZ_DEVFLAG_GRIDFIT_AS_TILED, 0);
+		fz_paint_image(ctx, state[1].mask, &bbox, state[1].shape, state[1].group_alpha, pixmap, local_ctm, 255, !(devp->hints & FZ_DONT_INTERPOLATE_IMAGES), 0);
 
 #ifdef DUMP_GROUP_BLENDS
 		fz_dump_blend(ctx, " to get ", state[1].mask);

source/fitz/draw-affine.c
@@ -3885,7 +3885,6 @@ fz_paint_image_imp(fz_context *ctx,
 	const byte *color,
 	int alpha,
 	int lerp_allowed,
-	int as_tiled,
 	const fz_overprint *eop)
 {
 	byte *dp, *sp, *hp, *gp;
@@ -3899,9 +3898,6 @@ fz_paint_image_imp(fz_context *ctx,
 
 	if (alpha == 0)
 		return;
-
-	/* grid fit the image */
-	ctm = fz_gridfit_matrix(as_tiled, ctm);
 
 	/* turn on interpolation for upscaled and non-rectilinear transforms */
 	dolerp = 0;
@@ -4089,14 +4085,14 @@ fz_paint_image_imp(fz_context *ctx,
 }
 
 void
-fz_paint_image_with_color(fz_context *ctx, fz_pixmap * FZ_RESTRICT dst, const fz_irect * FZ_RESTRICT scissor, fz_pixmap * FZ_RESTRICT shape, fz_pixmap * FZ_RESTRICT group_alpha, fz_pixmap * FZ_RESTRICT img, fz_matrix ctm, const byte * FZ_RESTRICT color, int lerp_allowed, int as_tiled, const fz_overprint * FZ_RESTRICT eop)
+fz_paint_image_with_color(fz_context *ctx, fz_pixmap * FZ_RESTRICT dst, const fz_irect * FZ_RESTRICT scissor, fz_pixmap * FZ_RESTRICT shape, fz_pixmap * FZ_RESTRICT group_alpha, fz_pixmap * FZ_RESTRICT img, fz_matrix ctm, const byte * FZ_RESTRICT color, int lerp_allowed, const fz_overprint * FZ_RESTRICT eop)
 {
 	assert(img->n == 1);
-	fz_paint_image_imp(ctx, dst, scissor, shape, group_alpha, img, ctm, color, 255, lerp_allowed, as_tiled, eop);
+	fz_paint_image_imp(ctx, dst, scissor, shape, group_alpha, img, ctm, color, 255, lerp_allowed, eop);
 }
 
 void
-fz_paint_image(fz_context *ctx, fz_pixmap * FZ_RESTRICT dst, const fz_irect * FZ_RESTRICT scissor, fz_pixmap * FZ_RESTRICT shape, fz_pixmap * FZ_RESTRICT group_alpha, fz_pixmap * FZ_RESTRICT img, fz_matrix ctm, int alpha, int lerp_allowed, int as_tiled, const fz_overprint * FZ_RESTRICT eop)
+fz_paint_image(fz_context *ctx, fz_pixmap * FZ_RESTRICT dst, const fz_irect * FZ_RESTRICT scissor, fz_pixmap * FZ_RESTRICT shape, fz_pixmap * FZ_RESTRICT group_alpha, fz_pixmap * FZ_RESTRICT img, fz_matrix ctm, int alpha, int lerp_allowed, const fz_overprint * FZ_RESTRICT eop)
 {
-	fz_paint_image_imp(ctx, dst, scissor, shape, group_alpha, img, ctm, NULL, alpha, lerp_allowed, as_tiled, eop);
+	fz_paint_image_imp(ctx, dst, scissor, shape, group_alpha, img, ctm, NULL, alpha, lerp_allowed, eop);
 }

source/fitz/draw-imp.h
@@ -435,8 +435,8 @@ fz_solid_color_painter_t *fz_get_solid_color_painter(int n, const unsigned char 
 fz_span_painter_t *fz_get_span_painter(int da, int sa, int n, int alpha, const fz_overprint * FZ_RESTRICT eop);
 fz_span_color_painter_t *fz_get_span_color_painter(int n, int da, const unsigned char * FZ_RESTRICT color, const fz_overprint * FZ_RESTRICT eop);
 
-void fz_paint_image(fz_context *ctx, fz_pixmap * FZ_RESTRICT dst, const fz_irect * FZ_RESTRICT scissor, fz_pixmap * FZ_RESTRICT shape, fz_pixmap * FZ_RESTRICT group_alpha, fz_pixmap * FZ_RESTRICT img, fz_matrix ctm, int alpha, int lerp_allowed, int gridfit_as_tiled, const fz_overprint * FZ_RESTRICT eop);
-void fz_paint_image_with_color(fz_context *ctx, fz_pixmap * FZ_RESTRICT dst, const fz_irect * FZ_RESTRICT scissor, fz_pixmap * FZ_RESTRICT shape, fz_pixmap * FZ_RESTRICT group_alpha, fz_pixmap * FZ_RESTRICT img, fz_matrix ctm, const unsigned char * FZ_RESTRICT colorbv, int lerp_allowed, int gridfit_as_tiled, const fz_overprint * FZ_RESTRICT eop);
+void fz_paint_image(fz_context *ctx, fz_pixmap * FZ_RESTRICT dst, const fz_irect * FZ_RESTRICT scissor, fz_pixmap * FZ_RESTRICT shape, fz_pixmap * FZ_RESTRICT group_alpha, fz_pixmap * FZ_RESTRICT img, fz_matrix ctm, int alpha, int lerp_allowed, const fz_overprint * FZ_RESTRICT eop);
+void fz_paint_image_with_color(fz_context *ctx, fz_pixmap * FZ_RESTRICT dst, const fz_irect * FZ_RESTRICT scissor, fz_pixmap * FZ_RESTRICT shape, fz_pixmap * FZ_RESTRICT group_alpha, fz_pixmap * FZ_RESTRICT img, fz_matrix ctm, const unsigned char * FZ_RESTRICT colorbv, int lerp_allowed, const fz_overprint * FZ_RESTRICT eop);
 
 void fz_paint_pixmap(fz_pixmap * FZ_RESTRICT dst, const fz_pixmap * FZ_RESTRICT src, int alpha);
 void fz_paint_pixmap_alpha(fz_pixmap * FZ_RESTRICT dst, const fz_pixmap * FZ_RESTRICT src, int alpha);

commit 45003f3921774577968e209a5f6900022a5cc9bf

Remember to push empty clip onto draw stack in error case.
Commit b1e505d1824d4356e48ce9e11cc5a02d6fa785dc accidentally omitted
pushing an empty clip onto the draw stack when returning early from
fz_draw_clip_image_mask(). The calling function assumes that the
clip is always pushed and so will always pop the clip. This situation
caused a later function to access the already popped clip causing a
segmentation fault.

This fixes OSS-fuzz issue 36463.

source/fitz/draw-device.c
@@ -1955,7 +1955,14 @@ fz_draw_clip_image_mask(fz_context *ctx, fz_device *devp, fz_image *image, fz_ma
 
 	src_area = find_src_area_required(local_ctm, image, clip);
 	if (fz_is_empty_irect(src_area))
+	{
+#ifdef DUMP_GROUP_BLENDS
+		dump_spaces(dev->top-1, "Clip (image mask) (empty source area) begin\n");
+#endif
+		state[1].scissor = fz_empty_irect;
+		state[1].mask = NULL;
 		return;
+	}
 
 	bbox = fz_irect_from_rect(fz_transform_rect(fz_unit_rect, local_ctm));
 	bbox = fz_intersect_irect(bbox, state->scissor);