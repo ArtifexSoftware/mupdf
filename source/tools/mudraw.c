7bafcc647c10764f0b19cabbb33712f1f5dc6295
Robin Watts <Robin.Watts@artifex.com>
2021-12-16 19:07:49 +0000

    Bug 704704: Fix mutool banding issues.
    A while ago, I tried a commit to solve this. I now look
    back on that code, and can't see that it makes any sense.
    So, revert commit eaec63901caa0e72009528f430b790e8b87aac1e
    here, and try a new fix.
    
    The problem was not in the draw_device implementation, but
    rather in how we were calling it. Currently we feed in the
    same bitmap stripe each time (y=0 to y=50 say), and alter
    the ctm each time. This works fine for everything other than
    patterns, whose phase is determined by the position on the
    page.
    
    Accordingly, we update mutool to pass in the bitmap stripe
    with different spatial coords each time, and leave the ctm
    alone. This correctly allows the draw device to position the
    patterns.

--- source/tools/mudraw.c
+++ source/tools/mudraw.c
@@ -1010,13 +1010,13 @@ static void dodrawpage(fz_context *ctx, fz_page *page, fz_display_list *list, in
 					memset(&workers[band].cookie, 0, sizeof(fz_cookie));
 					workers[band].list = list;
 					workers[band].pix = fz_new_pixmap_with_bbox(ctx, colorspace, band_ibounds, seps, alpha);
+					workers[band].pix->y += band * band_height;
 					fz_set_pixmap_resolution(ctx, workers[band].pix, resolution, resolution);
 					workers[band].running = 1;
 #ifndef DISABLE_MUTHREADS
 					DEBUG_THREADS(("Worker %d, Pre-triggering band %d\n", band, band));
 					mu_trigger_semaphore(&workers[band].start);
 #endif
-					ctm.f -= drawheight;
 				}
 				pix = workers[0].pix;
 			}
@@ -1096,6 +1096,7 @@ static void dodrawpage(fz_context *ctx, fz_page *page, fz_display_list *list, in
 				{
 					worker_t *w = &workers[band % num_workers];
 					w->band = band + num_workers;
+					w->pix->y = band_ibounds.y0 + w->band * band_height;
 					w->ctm = ctm;
 					w->tbounds = tbounds;
 					memset(&w->cookie, 0, sizeof(fz_cookie));
@@ -1105,7 +1106,8 @@ static void dodrawpage(fz_context *ctx, fz_page *page, fz_display_list *list, in
 					mu_trigger_semaphore(&w->start);
 #endif
 				}
-				ctm.f -= drawheight;
+				if (num_workers <= 0)
+					pix->y += band_height;
 			}
 
 			if (output_format != OUT_PCLM && output_format != OUT_OCR_PDF)
