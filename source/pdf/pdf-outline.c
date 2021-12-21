5a6ce81da92c97c3b30a6a2a887494215c689e24
Sebastian Rasmussen <sebras@gmail.com>
2021-12-22 00:11:09 +0100

    Bug 704786: Support repairing prev pointers in outline tree.
    The reported file has a broken outline.
    
    It contains an outline node (777 0 R) that has children. Its /First
    entry points to a child node (778 0 R) that has a /Prev entry. This
    is unexpected according to the specification.
    
    Moreover that /Prev entry points to an outline node (776 0 R)
    that does not have a /Next entry pointing back to the child node
    (778 0 R), but rather is part of a different linked list of outline
    nodes.
    
    Let MuPDF handle this by assuming that all /First and /Next entries
    are correct and deduce what /Prev ought to point to. If no /Prev
    entry is expected then remove it if it exists. If the /Prev entry
    points to the wrong node then point it to the expected outline node.

--- source/pdf/pdf-outline.c
+++ source/pdf/pdf-outline.c
@@ -65,7 +65,7 @@ pdf_test_outline(fz_context *ctx, pdf_document *doc, pdf_obj *dict, pdf_mark_lis
 			if (last_diff)
 				fz_throw(ctx, FZ_ERROR_GENERIC, "Outline last pointer still bad or missing despite repair");
 		}
-		else if (parent_diff || last_diff)
+		else if (parent_diff || prev_diff || last_diff)
 		{
 			if (*fixed == 0)
 				pdf_begin_operation(ctx, doc, "Repair outline nodes");
@@ -75,6 +75,14 @@ pdf_test_outline(fz_context *ctx, pdf_document *doc, pdf_obj *dict, pdf_mark_lis
 		{
 			fz_warn(ctx, "Bad or missing parent pointer in outline tree, repairing");
 			pdf_dict_put(ctx, dict, PDF_NAME(Parent), expected_parent);
+		}
+		if (prev_diff)
+		{
+			fz_warn(ctx, "Bad or missing prev pointer in outline tree, repairing");
+			if (expected_prev)
+				pdf_dict_put(ctx, dict, PDF_NAME(Prev), expected_prev);
+			else
+				pdf_dict_del(ctx, dict, PDF_NAME(Prev));
 		}
 		if (last_diff)
 		{
