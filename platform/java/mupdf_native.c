ArtifexSoftware/mupdf

commit a2bcbc5e7abc141a3614d42c02f81493d658eade

Add DOM.attributes Java API.
Missed from initial commit.

Also add dom.c to MSVC project.

platform/java/mupdf_native.c
@@ -102,6 +102,7 @@ static jclass cls_Document;
 static jclass cls_DocumentWriter;
 static jclass cls_DocumentWriter_OCRListener;
 static jclass cls_DOM;
+static jclass cls_DOMAttribute;
 static jclass cls_FitzInputStream;
 static jclass cls_FloatArray;
 static jclass cls_Font;
@@ -180,6 +181,8 @@ static jfieldID fid_DocumentWriter_ocrlistener;
 static jfieldID fid_DocumentWriter_pointer;
 static jfieldID fid_Document_pointer;
 static jfieldID fid_DOM_pointer;
+static jfieldID fid_DOMAttribute_attribute;
+static jfieldID fid_DOMAttribute_value;
 static jfieldID fid_FitzInputStream_closed;
 static jfieldID fid_FitzInputStream_markpos;
 static jfieldID fid_FitzInputStream_pointer;
@@ -297,6 +300,7 @@ static jmethodID mid_DisplayList_init;
 static jmethodID mid_DocumentWriter_OCRListener_progress;
 static jmethodID mid_Document_init;
 static jmethodID mid_DOM_init;
+static jmethodID mid_DOMAttribute_init;
 static jmethodID mid_FitzInputStream_init;
 static jmethodID mid_Font_init;
 static jmethodID mid_Image_init;
@@ -835,6 +839,11 @@ static int find_fids(JNIEnv *env)
 	fid_DOM_pointer = get_field(&err, env, "pointer", "J");
 	mid_DOM_init = get_method(&err, env, "<init>", "(J)V");
 
+	cls_DOMAttribute = get_class(&err, env, PKG"DOM$DOMAttribute");
+	fid_DOMAttribute_attribute = get_field(&err, env, "attribute", "Ljava/lang/string;");
+	fid_DOMAttribute_value = get_field(&err, env, "value", "Ljava/lang/string;");
+	mid_DOMAttribute_init = get_method(&err, env, "<init>", "()V");
+
 	cls_FitzInputStream = get_class(&err, env, PKG"FitzInputStream");
 	fid_FitzInputStream_pointer = get_field(&err, env, "pointer", "J");
 	fid_FitzInputStream_markpos = get_field(&err, env, "markpos", "J");
@@ -1153,9 +1162,12 @@ static void lose_fids(JNIEnv *env)
 	(*env)->DeleteGlobalRef(env, cls_DisplayList);
 	(*env)->DeleteGlobalRef(env, cls_Document);
 	(*env)->DeleteGlobalRef(env, cls_DocumentWriter);
+	(*env)->DeleteGlobalRef(env, cls_DOM);
+	(*env)->DeleteGlobalRef(env, cls_DOMAttribute);
 	(*env)->DeleteGlobalRef(env, cls_FitzInputStream);
 	(*env)->DeleteGlobalRef(env, cls_FloatArray);
 	(*env)->DeleteGlobalRef(env, cls_Font);
+	(*env)->DeleteGlobalRef(env, cls_HTMLStory);
 	(*env)->DeleteGlobalRef(env, cls_IOException);
 	(*env)->DeleteGlobalRef(env, cls_IllegalArgumentException);
 	(*env)->DeleteGlobalRef(env, cls_Image);

platform/java/jni/dom.c
@@ -215,6 +215,63 @@ FUN(DOM_attribute)(JNIEnv *env, jobject self, jstring jatt)
 	return to_String_safe(ctx, env, val);
 }
 
+JNIEXPORT jobjectArray JNICALL
+FUN(DOM_attributes)(JNIEnv *env, jobject self)
+{
+	fz_context *ctx = get_context(env);
+	fz_xml *dom = from_DOM_safe(env, self);
+	const char *att = NULL;
+	const char *val = NULL;
+	jobjectArray jarr;
+	jstring jatt, jval;
+	jobject jobj;
+	size_t i;
+	int n;
+
+	fz_try(ctx)
+	{
+		n = 0;
+		while (1)
+		{
+			val = fz_dom_get_attribute(ctx, dom, n, &att);
+			if (att == NULL)
+				break;
+			n++;
+		}
+	}
+	fz_catch(ctx)
+		jni_rethrow(env, ctx);
+
+	jarr = (*env)->NewObjectArray(env, n, cls_DOMAttribute, NULL);
+	if (!jarr) jni_throw_run(env, "cannot create attribute array");
+
+	for (i = 0; i < (size_t)n; i++)
+	{
+		fz_try(ctx)
+			val = fz_dom_get_attribute(ctx, dom, i, &att);
+		fz_catch(ctx)
+			jni_rethrow(env, ctx);
+
+		jobj = (*env)->NewObject(env, cls_DOMAttribute, mid_DOMAttribute_init);
+		if (!jobj) jni_throw_run(env, "cannot create DOMAttribute");
+		jatt = (*env)->NewStringUTF(env, att);
+		if (!jatt) jni_throw_run(env, "cannot create String from attribute");
+		if (val == NULL)
+			jval = NULL;
+		else {
+			jval = (*env)->NewStringUTF(env, val);
+			if (!jval) jni_throw_run(env, "cannot create String from attribute");
+		}
+		(*env)->SetObjectField(env, jobj, fid_DOMAttribute_attribute, jatt);
+		(*env)->SetObjectField(env, jobj, fid_DOMAttribute_value, jval);
+		(*env)->SetObjectArrayElement(env, jarr, i, jobj);
+		if ((*env)->ExceptionCheck(env))
+			return NULL;
+	}
+
+	return jarr;
+}
+
 JNIEXPORT jobject JNICALL
 FUN(DOM_find)(JNIEnv *env, jobject self, jstring jtag, jstring jatt, jstring jval)
 {

platform/java/src/com/artifex/mupdf/fitz/DOM.java
@@ -57,6 +57,11 @@ public class DOM
 	public native DOM addAttribute(String att, String val);
 	public native DOM removeAttribute(String att);
 	public native String attribute(String att);
+	public static class DOMAttribute {
+		String attribute;
+		String value;
+	};
+	public native DOMAttribute[] attributes();
 	public native DOM find(String tag, String att, String val);
 	public native DOM findNext(String tag, String att, String val);
 }

platform/win32/javaviewerlib.vcxproj
@@ -302,6 +302,10 @@
       <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='DebugJava|x64'">true</ExcludedFromBuild>
       <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='ReleaseJava|x64'">true</ExcludedFromBuild>
     </ClCompile>
+    <ClCompile Include="..\java\jni\dom.c">
+      <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='ReleaseJava|Win32'">true</ExcludedFromBuild>
+      <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='DebugJava|Win32'">true</ExcludedFromBuild>
+    </ClCompile>
     <ClCompile Include="..\java\jni\drawdevice.c">
       <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='ReleaseJava|Win32'">true</ExcludedFromBuild>
       <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='DebugJava|Win32'">true</ExcludedFromBuild>

platform/win32/javaviewerlib.vcxproj.filters
@@ -294,6 +294,9 @@
     <ClCompile Include="..\java\jni\xml.c">
       <Filter>jni</Filter>
     </ClCompile>
+    <ClCompile Include="..\java\jni\dom.c">
+      <Filter>jni</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="..\java\mupdf_native.h" />

commit 8fa84d8cd352b841b75a89291a6deea68c172387

java: Fix typo in DOM API JNI initialization.
Bug was introduced in commit a2bcbc5e7abc141a3614d42c02f81493d658eade.

platform/java/mupdf_native.c
@@ -840,8 +840,8 @@ static int find_fids(JNIEnv *env)
 	mid_DOM_init = get_method(&err, env, "<init>", "(J)V");
 
 	cls_DOMAttribute = get_class(&err, env, PKG"DOM$DOMAttribute");
-	fid_DOMAttribute_attribute = get_field(&err, env, "attribute", "Ljava/lang/string;");
-	fid_DOMAttribute_value = get_field(&err, env, "value", "Ljava/lang/string;");
+	fid_DOMAttribute_attribute = get_field(&err, env, "attribute", "Ljava/lang/String;");
+	fid_DOMAttribute_value = get_field(&err, env, "value", "Ljava/lang/String;");
 	mid_DOMAttribute_init = get_method(&err, env, "<init>", "()V");
 
 	cls_FitzInputStream = get_class(&err, env, PKG"FitzInputStream");