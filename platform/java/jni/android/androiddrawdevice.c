ArtifexSoftware/mupdf

commit 3bd8ce45a9a29c8131deab37df17c24b1ece162b

Wrap jni_rethrow() and family in macros.
Commit c51cc4c3da519ffdf0fa43d680518ab995332e8b started to
return from JNI functions using comma expressions. Unfortunately
this also meant returning the void return value from jni_rethrow()
in JNI function returning void. Neither gcc nor clang complained
about this (with our normal set of warnings), but MSVC caught it.

Accordingly, we move to using macros for jni_rethrow() and similar
jni_throwing "functions". These wrap up the rethrow and the return
into a single invocation. The values we return here are never
important because the caller should spot that we have raised an
exception before it takes notice of the value we pass back.

Thus, we can have jni_throw_whatever() always end with a return 0,
which will work, whether it's an int, a long, or a pointer expected
as the return value. To cope with throwing in void contexts, however,
we need to also have jni_throw_whatever_void();

platform/java/jni/android/androiddrawdevice.c
@@ -54,7 +54,7 @@ newNativeAndroidDrawDevice(JNIEnv *env, jobject self, fz_context *ctx, jobject o
 	{
 		fz_drop_pixmap(ctx, pixmap);
 		fz_free(ctx, ninfo);
-		return jni_rethrow(env, ctx), 0;
+		jni_rethrow(env, ctx);
 	}
 
 	/* lockNativeDevice will already have raised a JNI error if there was one. */
@@ -76,7 +76,7 @@ static int androidDrawDevice_lock(JNIEnv *env, NativeDeviceInfo *info)
 	fz_context *ctx = get_context(env);
 	size_t size = info->width * info->height * 4;
 
-	if (!ctx) return jni_throw_run(env, "no context in DrawDevice call"), 1;
+	if (!ctx) jni_throw_run(env, "no context in DrawDevice call");
 
 	assert(info);
 	assert(info->object);
@@ -92,7 +92,7 @@ static int androidDrawDevice_lock(JNIEnv *env, NativeDeviceInfo *info)
 	if (ret != ANDROID_BITMAP_RESULT_SUCCESS)
 	{
 		info->pixmap->samples = NULL;
-		return jni_throw_run(env, "bitmap lock failed in DrawDevice call"), 1;
+		jni_throw_run(env, "bitmap lock failed in DrawDevice call");
 	}
 
 	/* Now offset pixels to allow for the page offsets */
@@ -128,13 +128,11 @@ FUN(android_AndroidDrawDevice_invertLuminance)(JNIEnv *env, jobject self)
 		return;
 
 	fz_try(ctx)
-	{
 		fz_invert_pixmap_luminance(ctx, info->pixmap);
-	}
 	fz_always(ctx)
 		unlockNativeDevice(env, info);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx);
+		jni_rethrow_void(env, ctx);
 }
 
 JNIEXPORT jlong JNICALL
@@ -146,19 +144,19 @@ FUN(android_AndroidDrawDevice_newNative)(JNIEnv *env, jclass self, jobject jbitm
 	int ret;
 
 	if (!ctx) return 0;
-	if (!jbitmap) return jni_throw_arg(env, "bitmap must not be null"), 0;
+	if (!jbitmap) jni_throw(env, "bitmap must not be null");
 
 	if ((ret = AndroidBitmap_getInfo(env, jbitmap, &info)) != ANDROID_BITMAP_RESULT_SUCCESS)
-		return jni_throw_run(env, "new DrawDevice failed to get bitmap info"), 0;
+		jni_throw_run(env, "new DrawDevice failed to get bitmap info");
 	if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888)
-		return jni_throw_run(env, "new DrawDevice failed as bitmap format is not RGBA_8888"), 0;
+		jni_throw_run(env, "new DrawDevice failed as bitmap format is not RGBA_8888");
 	if (info.stride != info.width * 4)
-		return jni_throw_run(env, "new DrawDevice failed as bitmap width != stride"), 0;
+		jni_throw_run(env, "new DrawDevice failed as bitmap width != stride");
 
 	fz_try(ctx)
 		device = newNativeAndroidDrawDevice(env, self, ctx, jbitmap, info.width, info.height, androidDrawDevice_lock, androidDrawDevice_unlock, xOrigin, yOrigin, pX0, pY0, pX1, pY1);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx), 0;
+		jni_rethrow(env, ctx);
 
 	return device;
 }

platform/java/jni/android/androidimage.c
@@ -12,16 +12,16 @@ FUN(AndroidImage_newImageFromBitmap)(JNIEnv *env, jobject self, jobject jbitmap,
 	int ret;
 
 	if (!ctx) return 0;
-	if (!jbitmap) return jni_throw_arg(env, "bitmap must not be null"), 0;
+	if (!jbitmap) jni_throw_arg(env, "bitmap must not be null");
 
 	if (mask && mask->mask)
-		return jni_throw_run(env, "new Image failed as mask cannot be masked"), 0;
+		jni_throw_run(env, "new Image failed as mask cannot be masked");
 	if ((ret = AndroidBitmap_getInfo(env, jbitmap, &info)) != ANDROID_BITMAP_RESULT_SUCCESS)
-		return jni_throw_run(env, "new Image failed to get bitmap info"), 0;
+		jni_throw_run(env, "new Image failed to get bitmap info");
 	if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888)
-		return jni_throw_run(env, "new Image failed as bitmap format is not RGBA_8888"), 0;
+		jni_throw_run(env, "new Image failed as bitmap format is not RGBA_8888");
 	if (info.stride != info.width)
-		return jni_throw_run(env, "new Image failed as bitmap width != stride"), 0;
+		jni_throw_run(env, "new Image failed as bitmap width != stride");
 
 	fz_var(pixmap);
 
@@ -49,7 +49,7 @@ FUN(AndroidImage_newImageFromBitmap)(JNIEnv *env, jobject self, jobject jbitmap,
 	fz_always(ctx)
 		fz_drop_pixmap(ctx, pixmap);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx), 0;
+		jni_rethrow(env, ctx);
 
 	return jlong_cast(image);
 }

platform/java/jni/buffer.c
@@ -17,12 +17,12 @@ FUN(Buffer_newNativeBuffer)(JNIEnv *env, jobject self, jint n)
 	fz_buffer *buf = NULL;
 
 	if (!ctx) return 0;
-	if (n < 0) return jni_throw_arg(env, "n cannot be negative"), 0;
+	if (n < 0) jni_throw_arg(env, "n cannot be negative");
 
 	fz_try(ctx)
 		buf = fz_new_buffer(ctx, n);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx), 0;
+		jni_rethrow(env, ctx);
 
 	return jlong_cast(buf);
 }
@@ -48,7 +48,7 @@ FUN(Buffer_readByte)(JNIEnv *env, jobject self, jint jat)
 	unsigned char *data;
 
 	if (!ctx || !buf) return -1;
-	if (jat < 0) return jni_throw_oob(env, "at is negative"), -1;
+	if (jat < 0) jni_throw_oob(env, "at is negative");
 
 	len = fz_buffer_storage(ctx, buf, &data);
 	if (at >= len)
@@ -70,8 +70,8 @@ FUN(Buffer_readBytes)(JNIEnv *env, jobject self, jint jat, jobject jbs)
 	unsigned char *data;
 
 	if (!ctx || !buf) return -1;
-	if (jat < 0) return jni_throw_oob(env, "at is negative"), -1;
-	if (!jbs) return jni_throw_arg(env, "buffer must not be null"), -1;
+	if (jat < 0) jni_throw_oob(env, "at is negative");
+	if (!jbs) jni_throw_arg(env, "buffer must not be null");
 
 	len = fz_buffer_storage(ctx, buf, &data);
 	if (at >= len)
@@ -83,7 +83,7 @@ FUN(Buffer_readBytes)(JNIEnv *env, jobject self, jint jat, jobject jbs)
 	len = fz_minz(len, remaining_input);
 
 	bs = (*env)->GetByteArrayElements(env, jbs, NULL);
-	if (!bs) return jni_throw_io(env, "cannot get bytes to read"), -1;
+	if (!bs) jni_throw_io(env, "cannot get bytes to read");
 
 	memcpy(bs, &data[at], len);
 	(*env)->ReleaseByteArrayElements(env, jbs, bs, 0);
@@ -105,13 +105,13 @@ FUN(Buffer_readBytesInto)(JNIEnv *env, jobject self, jint jat, jobject jbs, jint
 	unsigned char *data;
 
 	if (!ctx || !buf) return -1;
-	if (jat < 0) return jni_throw_oob(env, "at is negative"), -1;
-	if (!jbs) return jni_throw_arg(env, "buffer must not be null"), -1;
-	if (joff < 0) return jni_throw_oob(env, "offset is negative"), -1;
-	if (jlen < 0) return jni_throw_oob(env, "length is negative"), -1;
+	if (jat < 0) jni_throw_oob(env, "at is negative");
+	if (!jbs) jni_throw_arg(env, "buffer must not be null");
+	if (joff < 0) jni_throw_oob(env, "offset is negative");
+	if (jlen < 0) jni_throw_oob(env, "length is negative");
 
 	bslen = (*env)->GetArrayLength(env, jbs);
-	if (len > bslen - off) return jni_throw_oob(env, "offset + length is outside of buffer"), -1;
+	if (len > bslen - off) jni_throw_oob(env, "offset + length is outside of buffer");
 
 	blen = fz_buffer_storage(ctx, buf, &data);
 	if (at >= blen)
@@ -120,7 +120,7 @@ FUN(Buffer_readBytesInto)(JNIEnv *env, jobject self, jint jat, jobject jbs, jint
 	len = fz_minz(len, blen - at);
 
 	bs = (*env)->GetByteArrayElements(env, jbs, NULL);
-	if (!bs) return jni_throw_io(env, "cannot get bytes to read"), -1;
+	if (!bs) jni_throw_io(env, "cannot get bytes to read");
 
 	memcpy(&bs[off], &data[at], len);
 	(*env)->ReleaseByteArrayElements(env, jbs, bs, 0);
@@ -139,7 +139,7 @@ FUN(Buffer_writeByte)(JNIEnv *env, jobject self, jbyte b)
 	fz_try(ctx)
 		fz_append_byte(ctx, buf, b);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx);
+		jni_rethrow_void(env, ctx);
 }
 
 JNIEXPORT void JNICALL
@@ -151,18 +151,18 @@ FUN(Buffer_writeBytes)(JNIEnv *env, jobject self, jobject jbs)
 	jbyte *bs = NULL;
 
 	if (!ctx || !buf) return;
-	if (!jbs) return jni_throw_arg(env, "buffer must not be null");
+	if (!jbs) jni_throw_arg_void(env, "buffer must not be null");
 
 	len = (*env)->GetArrayLength(env, jbs);
 	bs = (*env)->GetByteArrayElements(env, jbs, NULL);
-	if (!bs) return jni_throw_io(env, "cannot get bytes to write");
+	if (!bs) jni_throw_io_void(env, "cannot get bytes to write");
 
 	fz_try(ctx)
 		fz_append_data(ctx, buf, bs, len);
 	fz_always(ctx)
 		(*env)->ReleaseByteArrayElements(env, jbs, bs, JNI_ABORT);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx);
+		jni_rethrow_void(env, ctx);
 }
 
 JNIEXPORT void JNICALL
@@ -176,22 +176,22 @@ FUN(Buffer_writeBytesFrom)(JNIEnv *env, jobject self, jobject jbs, jint joff, ji
 	jsize bslen = 0;
 
 	if (!ctx || !buf) return;
-	if (!jbs) return jni_throw_arg(env, "buffer must not be null");
+	if (!jbs) jni_throw_arg_void(env, "buffer must not be null");
 
 	bslen = (*env)->GetArrayLength(env, jbs);
-	if (joff < 0) return jni_throw_oob(env, "offset is negative");
-	if (jlen < 0) return jni_throw_oob(env, "length is negative");
-	if (off + len >= bslen) return jni_throw_oob(env, "offset + length is outside of buffer");
+	if (joff < 0) jni_throw_oob_void(env, "offset is negative");
+	if (jlen < 0) jni_throw_oob_void(env, "length is negative");
+	if (off + len >= bslen) jni_throw_oob_void(env, "offset + length is outside of buffer");
 
 	bs = (*env)->GetByteArrayElements(env, jbs, NULL);
-	if (!bs) return jni_throw_io(env, "cannot get bytes to write");
+	if (!bs) jni_throw_io_void(env, "cannot get bytes to write");
 
 	fz_try(ctx)
 		fz_append_data(ctx, buf, &bs[off], len);
 	fz_always(ctx)
 		(*env)->ReleaseByteArrayElements(env, jbs, bs, JNI_ABORT);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx);
+		jni_rethrow_void(env, ctx);
 }
 
 JNIEXPORT void JNICALL
@@ -202,12 +202,12 @@ FUN(Buffer_writeBuffer)(JNIEnv *env, jobject self, jobject jbuf)
 	fz_buffer *cat = from_Buffer(env, jbuf);
 
 	if (!ctx || !buf) return;
-	if (!cat) return jni_throw_arg(env, "buffer must not be null");
+	if (!cat) jni_throw_arg_void(env, "buffer must not be null");
 
 	fz_try(ctx)
 		fz_append_buffer(ctx, buf, cat);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx);
+		jni_rethrow_void(env, ctx);
 }
 
 JNIEXPORT void JNICALL
@@ -221,7 +221,7 @@ FUN(Buffer_writeRune)(JNIEnv *env, jobject self, jint rune)
 	fz_try(ctx)
 		fz_append_rune(ctx, buf, rune);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx);
+		jni_rethrow_void(env, ctx);
 }
 
 JNIEXPORT void JNICALL
@@ -232,7 +232,7 @@ FUN(Buffer_writeLine)(JNIEnv *env, jobject self, jstring jline)
 	const char *line = NULL;
 
 	if (!ctx || !buf) return;
-	if (!jline) return jni_throw_arg(env, "line must not be null");
+	if (!jline) jni_throw_arg_void(env, "line must not be null");
 
 	line = (*env)->GetStringUTFChars(env, jline, NULL);
 	if (!line) return;
@@ -245,7 +245,7 @@ FUN(Buffer_writeLine)(JNIEnv *env, jobject self, jstring jline)
 	fz_always(ctx)
 		(*env)->ReleaseStringUTFChars(env, jline, line);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx);
+		jni_rethrow_void(env, ctx);
 }
 
 JNIEXPORT void JNICALL
@@ -257,7 +257,7 @@ FUN(Buffer_writeLines)(JNIEnv *env, jobject self, jobject jlines)
 	jsize len = 0;
 
 	if (!ctx || !buf) return;
-	if (!jlines) return jni_throw_arg(env, "lines must not be null");
+	if (!jlines) jni_throw_arg_void(env, "lines must not be null");
 
 	len = (*env)->GetArrayLength(env, jlines);
 
@@ -282,7 +282,7 @@ FUN(Buffer_writeLines)(JNIEnv *env, jobject self, jobject jlines)
 		fz_always(ctx)
 			(*env)->ReleaseStringUTFChars(env, jline, line);
 		fz_catch(ctx)
-			return jni_rethrow(env, ctx);
+			jni_rethrow_void(env, ctx);
 	}
 }
 
@@ -306,5 +306,5 @@ FUN(Buffer_save)(JNIEnv *env, jobject self, jstring jfilename)
 		if (filename)
 			(*env)->ReleaseStringUTFChars(env, jfilename, filename);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx);
+		jni_rethrow_void(env, ctx);
 }

platform/java/jni/context.c
@@ -123,7 +123,7 @@ static fz_context *get_context(JNIEnv *env)
 	fz_context *ctx = (fz_context *)
 #ifdef _WIN32
 		TlsGetValue(context_key);
-	if (ctx == NULL && GetLastError() != ERROR_SUCCESS) return jni_throw_run(env, "cannot get context"), NULL;
+	if (ctx == NULL && GetLastError() != ERROR_SUCCESS) jni_throw_run(env, "cannot get context");
 #else
 		pthread_getspecific(context_key);
 #endif
@@ -132,12 +132,12 @@ static fz_context *get_context(JNIEnv *env)
 		return ctx;
 
 	ctx = fz_clone_context(base_context);
-	if (!ctx) return jni_throw_oom(env, "failed to clone fz_context"), NULL;
+	if (!ctx) jni_throw_oom(env, "failed to clone fz_context");
 
 #ifdef _WIN32
-	if (TlsSetValue(context_key, ctx) == 0) return jni_throw_run(env, "cannot store context"), NULL;
+	if (TlsSetValue(context_key, ctx) == 0) jni_throw_run(env, "cannot store context");
 #else
-	if (pthread_setspecific(context_key, ctx) != 0) return jni_throw_run(env, "cannot store context"), NULL;
+	if (pthread_setspecific(context_key, ctx) != 0) jni_throw_run(env, "cannot store context");
 #endif
 	return ctx;
 }

platform/java/jni/cookie.c
@@ -21,7 +21,7 @@ FUN(Cookie_newNative)(JNIEnv *env, jobject self)
 	fz_try(ctx)
 		cookie = fz_malloc_struct(ctx, fz_cookie);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx), 0;
+		jni_rethrow(env, ctx);
 
 	return jlong_cast(cookie);
 }

platform/java/jni/device.c
@@ -389,7 +389,7 @@ static fz_device *fz_new_java_device(fz_context *ctx, JNIEnv *env, jclass cls)
 	fz_catch(ctx)
 	{
 		fz_drop_device(ctx, &dev->super);
-		return jni_rethrow(env, ctx), NULL;
+		jni_rethrow(env, ctx);
 	}
 
 	return (fz_device *)dev;
@@ -406,7 +406,7 @@ FUN(Device_newNative)(JNIEnv *env, jclass cls)
 	fz_try(ctx)
 		dev = fz_new_java_device(ctx, env, cls);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx), 0;
+		jni_rethrow(env, ctx);
 
 	return jlong_cast(dev);
 }

platform/java/jni/displaylist.c
@@ -13,7 +13,7 @@ FUN(DisplayList_newNative)(JNIEnv *env, jobject self, jobject jmediabox)
 	fz_try(ctx)
 		list = fz_new_display_list(ctx, mediabox);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx), 0;
+		jni_rethrow(env, ctx);
 
 	return jlong_cast(list);
 }
@@ -31,7 +31,7 @@ FUN(DisplayList_run)(JNIEnv *env, jobject self, jobject jdev, jobject jctm, jobj
 	int err;
 
 	if (!ctx || !list) return;
-	if (!dev) return jni_throw_arg(env, "device must not be null");
+	if (!dev) jni_throw_arg_void(env, "device must not be null");
 
 	/* Use a scissor rectangle if one is supplied */
 	if (jrect)
@@ -47,7 +47,7 @@ FUN(DisplayList_run)(JNIEnv *env, jobject self, jobject jdev, jobject jctm, jobj
 	fz_always(ctx)
 		unlockNativeDevice(env, info);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx);
+		jni_rethrow_void(env, ctx);
 }
 
 JNIEXPORT void JNICALL
@@ -74,7 +74,7 @@ FUN(DisplayList_toPixmap)(JNIEnv *env, jobject self, jobject jctm, jobject jcs, 
 	fz_try(ctx)
 		pixmap = fz_new_pixmap_from_display_list(ctx, list, ctm, cs, alpha);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx), NULL;
+		jni_rethrow(env, ctx);
 
 	return to_Pixmap_safe_own(ctx, env, pixmap);
 }
@@ -107,7 +107,7 @@ FUN(DisplayList_toStructuredText)(JNIEnv *env, jobject self, jstring joptions)
 			(*env)->ReleaseStringUTFChars(env, joptions, options);
 	}
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx), NULL;
+		jni_rethrow(env, ctx);
 
 	return to_StructuredText_safe_own(ctx, env, text);
 }
@@ -122,7 +122,7 @@ FUN(DisplayList_search)(JNIEnv *env, jobject self, jstring jneedle)
 	int n = 0;
 
 	if (!ctx || !list) return NULL;
-	if (!jneedle) return jni_throw_arg(env, "needle must not be null"), NULL;
+	if (!jneedle) jni_throw_arg(env, "needle must not be null");
 
 	needle = (*env)->GetStringUTFChars(env, jneedle, NULL);
 	if (!needle) return NULL;
@@ -132,7 +132,7 @@ FUN(DisplayList_search)(JNIEnv *env, jobject self, jstring jneedle)
 	fz_always(ctx)
 		(*env)->ReleaseStringUTFChars(env, jneedle, needle);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx), NULL;
+		jni_rethrow(env, ctx);
 
 	return to_QuadArray_safe(ctx, env, hits, n);
 }

platform/java/jni/displaylistdevice.c
@@ -8,14 +8,14 @@ FUN(DisplayListDevice_newNative)(JNIEnv *env, jclass cls, jobject jlist)
 	fz_device *device = NULL;
 
 	if (!ctx) return 0;
-	if (!list) return jni_throw_arg(env, "list must not be null"), 0;
+	if (!list) jni_throw_arg(env, "list must not be null");
 
 	fz_var(device);
 
 	fz_try(ctx)
 		device = fz_new_list_device(ctx, list);
 	fz_catch(ctx)
-		return jni_rethrow(env, ctx), 0;
+		jni_rethrow(env, ctx);
 
 	return jlong_cast(device);
 }

commit ec9f6dc5cffcfa2419ba35cf2386d5197c53463e

Return error, not success when unable to lock native device resource.
Commit 3bd8ce45a9a29c8131deab37df17c24b1ece162b accidentally changed
the return value upon thrown exceptions in the locking function.

This meant that even if the AndroidDrawDevice failed to lock the bitmap
it would set the pixmap sample pointer to NULL, throw an exception
and then return success to the caller. The caller newNativeAndroidDrawDevice()
detects errors based on the return value, because it indicates success
it would proceed to clear the pixmap samples. Since the samples pointer
was set to NULL this would generate a segmentation fault.

By instead indicating failure in the return value the caller will stop
using the pixmap samples, free all resources and propagate the error.
This is implemented in this commit and thus avoids the segmentation fault.

platform/java/jni/android/androiddrawdevice.c
@@ -100,7 +100,7 @@ static int androidDrawDevice_lock(JNIEnv *env, NativeDeviceInfo *info)
 
 	if (!ctx)
 	{
-		jni_throw_run(env, "no context in DrawDevice call");
+		jni_throw_run_imp(env, "no context in DrawDevice call");
 		return 1;
 	}
 
@@ -118,7 +118,7 @@ static int androidDrawDevice_lock(JNIEnv *env, NativeDeviceInfo *info)
 	if (ret != ANDROID_BITMAP_RESULT_SUCCESS)
 	{
 		info->pixmap->samples = NULL;
-		jni_throw_run(env, "bitmap lock failed in DrawDevice call");
+		jni_throw_run_imp(env, "bitmap lock failed in DrawDevice call");
 		return 1;
 	}
 