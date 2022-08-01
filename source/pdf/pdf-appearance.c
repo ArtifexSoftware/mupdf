dc448dc8df17eaaefd4c1555e535847be386d0e2
Sebastian Rasmussen <sebras@gmail.com>
2022-08-01 15:59:19 +0200

    Include entire line cap in annotation rect.

--- source/pdf/pdf-appearance.c
+++ source/pdf/pdf-appearance.c
@@ -279,7 +279,7 @@ pdf_write_line_cap_appearance(fz_context *ctx, fz_buffer *buf, fz_rect *rect,
 		fz_append_printf(ctx, buf, "%g %g %g %g %g %g c\n", x-m, y-r, x-r, y-m, x-r, y);
 		fz_append_printf(ctx, buf, "%g %g %g %g %g %g c\n", x-r, y+m, x-m, y+r, x, y+r);
 		maybe_stroke_and_fill(ctx, buf, sc, ic);
-		include_cap(rect, x, y, r);
+		include_cap(rect, x, y, r + w/2);
 	}
 	else if (cap == PDF_NAME(Diamond))
 	{
@@ -305,13 +305,21 @@ pdf_write_line_cap_appearance(fz_context *ctx, fz_buffer *buf, fz_rect *rect,
 	else if (cap == PDF_NAME(Butt))
 	{
 		float r = fz_max(3, w * 3);
+		float buttdx = dx * w / 2;
+		float buttdy = dy * w / 2;
 		fz_point a = { x-dy*r, y+dx*r };
 		fz_point b = { x+dy*r, y-dx*r };
+		fz_point a1 = { a.x + buttdx, a.y + buttdy };
+		fz_point a2 = { a.x - buttdx, a.y - buttdy };
+		fz_point b1 = { b.x + buttdx, b.y + buttdy };
+		fz_point b2 = { b.x - buttdx, b.y - buttdy };
 		fz_append_printf(ctx, buf, "%g %g m\n", a.x, a.y);
 		fz_append_printf(ctx, buf, "%g %g l\n", b.x, b.y);
 		maybe_stroke(ctx, buf, sc);
-		*rect = fz_include_point_in_rect(*rect, a);
-		*rect = fz_include_point_in_rect(*rect, b);
+		*rect = fz_include_point_in_rect(*rect, a1);
+		*rect = fz_include_point_in_rect(*rect, a2);
+		*rect = fz_include_point_in_rect(*rect, b1);
+		*rect = fz_include_point_in_rect(*rect, b2);
 	}
 	else if (cap == PDF_NAME(ROpenArrow))
 	{
