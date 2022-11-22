f7dcf4b7a1e96a83b850f5c4520a4ce201ecbccb
Robin Watts <Robin.Watts@artifex.com>
2022-11-22 19:36:08 +0000

    Bug 706094: Fix mutool clean garbage collection.
    A while ago, we spotted that pdf_objcmp was comparing any 2 stream
    objects as being identical if the dict portions matched, without
    considering any differences in the stream sections.
    
    We didn't want to take the hit of having pdf_objcmp compare stream
    contents, so we modified the code so that any stream objects would
    always differ.
    
    Unfortunately, mutool clean was relying on stream dicts being matched
    so it could do its own comparison of the stream contents. This meant
    that mutool clean with garbage >= 4 was failing to properly common up
    repeated streams.
    
    To solve that here, we have introduced a new pdf_objcmp_deep that
    performs as pdf_objcmp does, but also checks the stream contents.
    We make mutool clean (actually removeduplicateobjs in pdf-write.c)
    call that, and correct operation is restored.

--- source/pdf/pdf-object.c
+++ source/pdf/pdf-object.c
@@ -495,8 +495,8 @@ int pdf_objcmp_resolve(fz_context *ctx, pdf_obj *a, pdf_obj *b)
 	return pdf_objcmp(ctx, a, b);
 }
 
-int
-pdf_objcmp(fz_context *ctx, pdf_obj *a, pdf_obj *b)
+static int
+do_objcmp(fz_context *ctx, pdf_obj *a, pdf_obj *b, int check_streams)
 {
 	int i;
 
@@ -573,6 +573,7 @@ pdf_objcmp(fz_context *ctx, pdf_obj *a, pdf_obj *b)
 		return 0;
 
 	case PDF_DICT:
+		/* FIXME: Isn't this assuming all dicts are sorted? */
 		if (DICT(a)->len != DICT(b)->len)
 			return DICT(a)->len - DICT(b)->len;
 		for (i = 0; i < DICT(a)->len; i++)
@@ -583,31 +584,86 @@ pdf_objcmp(fz_context *ctx, pdf_obj *a, pdf_obj *b)
 				return 1;
 		}
 		/* Dicts are identical, but if they are streams, we can only be sure
-		 * they are identical if the stream contents match. We don't currently
-		 * test for identical stream contents, so if they are streams, require
-		 * a to == b for a match. */
+		 * they are identical if the stream contents match. If '!check_streams',
+		 * then don't test for identical stream contents - only match if a == b.
+		 * Otherwise, do the full, painful, comparison. */
 		{
 			/* Slightly convoluted to know if something is a stream. */
 			pdf_document *doc = DICT(a)->doc;
-			int n = pdf_obj_parent_num(ctx, a);
-			pdf_xref_entry *entry = pdf_get_xref_entry(ctx, doc, n);
-			if (entry->obj == a && pdf_obj_num_is_stream(ctx, doc, n))
+			int ap = pdf_obj_parent_num(ctx, a);
+			int bp;
+			int a_is_stream = 0;
+			pdf_xref_entry *entrya = pdf_get_xref_entry(ctx, doc, ap);
+			pdf_xref_entry *entryb;
+			if (entrya->obj == a && pdf_obj_num_is_stream(ctx, doc, ap))
 			{
-				/* It's a stream, and we know a != b from above. So mismatch. */
-				return 1;
+				/* It's a stream, and we know a != b from above. */
+				if (!check_streams)
+					return 1; /* mismatch */
+				a_is_stream = 1;
 			}
-			n = pdf_obj_parent_num(ctx, b);
-			entry = pdf_get_xref_entry(ctx, doc, n);
-			if (entry->obj == b && pdf_obj_num_is_stream(ctx, doc, n))
+			bp = pdf_obj_parent_num(ctx, b);
+			entryb = pdf_get_xref_entry(ctx, doc, bp);
+			if (entryb->obj == b && pdf_obj_num_is_stream(ctx, doc, bp))
 			{
 				/* It's a stream, and we know a != b from above. So mismatch. */
-				return 1;
+				if (!check_streams || !a_is_stream)
+					return 1; /* mismatch */
+			}
+			else
+			{
+				/* b is not a stream. We match, iff a is not a stream. */
+				return a_is_stream;
+			}
+			/* So, if we get here, we know check_streams is true, and that both
+			 * a and b are streams. */
+			{
+				fz_buffer *sa = NULL;
+				fz_buffer *sb = NULL;
+				int differ = 1;
+
+				fz_var(sa);
+				fz_var(sb);
+
+				fz_try(ctx)
+				{
+					unsigned char *dataa, *datab;
+					size_t lena, lenb;
+					sa = pdf_load_raw_stream_number(ctx, doc, ap);
+					sb = pdf_load_raw_stream_number(ctx, doc, bp);
+					lena = fz_buffer_storage(ctx, sa, &dataa);
+					lenb = fz_buffer_storage(ctx, sb, &datab);
+					if (lena == lenb && memcmp(dataa, datab, lena) == 0)
+						differ = 0;
+				}
+				fz_always(ctx)
+				{
+					fz_drop_buffer(ctx, sa);
+					fz_drop_buffer(ctx, sb);
+				}
+				fz_catch(ctx)
+				{
+					fz_rethrow(ctx);
+				}
+				return differ;
 			}
 		}
 
 		return 0;
 	}
 	return 1;
+}
+
+int
+pdf_objcmp(fz_context *ctx, pdf_obj *a, pdf_obj *b)
+{
+	return do_objcmp(ctx, a, b, 0);
+}
+
+int
+pdf_objcmp_deep(fz_context *ctx, pdf_obj *a, pdf_obj *b)
+{
+	return do_objcmp(ctx, a, b, 1);
 }
 
 int pdf_name_eq(fz_context *ctx, pdf_obj *a, pdf_obj *b)
--- source/pdf/pdf-write.c
+++ source/pdf/pdf-write.c
@@ -717,72 +717,23 @@ static void removeduplicateobjs(fz_context *ctx, pdf_document *doc, pdf_write_st
 		for (other = 1; other < num; other++)
 		{
 			pdf_obj *a, *b;
-			int newnum, streama = 0, streamb = 0, differ = 0;
+			int newnum;
 
 			if (num == other || num >= opts->list_len || !opts->use_list[num] || !opts->use_list[other])
 				continue;
 
 			/* TODO: resolve indirect references to see if we can omit them */
 
-			/*
-			 * Comparing stream objects data contents would take too long.
-			 *
-			 * pdf_obj_num_is_stream calls pdf_cache_object and ensures
-			 * that the xref table has the objects loaded.
-			 */
-			fz_try(ctx)
-			{
-				streama = pdf_obj_num_is_stream(ctx, doc, num);
-				streamb = pdf_obj_num_is_stream(ctx, doc, other);
-				differ = streama || streamb;
-				if (streama && streamb && opts->do_garbage >= 4)
-					differ = 0;
-			}
-			fz_catch(ctx)
-			{
-				/* Assume different */
-				differ = 1;
-			}
-			if (differ)
-				continue;
-
 			a = pdf_get_xref_entry_no_null(ctx, doc, num)->obj;
 			b = pdf_get_xref_entry_no_null(ctx, doc, other)->obj;
-
-			if (pdf_objcmp(ctx, a, b))
-				continue;
-
-			if (streama && streamb)
+			if (opts->do_garbage >= 4)
 			{
-				/* Check to see if streams match too. */
-				fz_buffer *sa = NULL;
-				fz_buffer *sb = NULL;
-
-				fz_var(sa);
-				fz_var(sb);
-
-				differ = 1;
-				fz_try(ctx)
-				{
-					unsigned char *dataa, *datab;
-					size_t lena, lenb;
-					sa = pdf_load_raw_stream_number(ctx, doc, num);
-					sb = pdf_load_raw_stream_number(ctx, doc, other);
-					lena = fz_buffer_storage(ctx, sa, &dataa);
-					lenb = fz_buffer_storage(ctx, sb, &datab);
-					if (lena == lenb && memcmp(dataa, datab, lena) == 0)
-						differ = 0;
-				}
-				fz_always(ctx)
-				{
-					fz_drop_buffer(ctx, sa);
-					fz_drop_buffer(ctx, sb);
-				}
-				fz_catch(ctx)
-				{
-					fz_rethrow(ctx);
-				}
-				if (differ)
+				if (pdf_objcmp_deep(ctx, a, b))
+					continue;
+			}
+			else
+			{
+				if (pdf_objcmp(ctx, a, b))
 					continue;
 			}
 
