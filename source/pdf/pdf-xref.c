ArtifexSoftware/mupdf

commit 88e801f635d3585dc6f87a7b3cdd8f24b8c8d108

Abandon trying to load linearized files with encryption.
Previously we'd have tried to load the xref stream without
knowing how to decrypt it, and would have failed hard.

Now we drop back to normal reading.

source/pdf/pdf-xref.c
@@ -1276,6 +1276,9 @@ pdf_read_new_xref(fz_context *ctx, pdf_document *doc)
 		if (pdf_is_indirect(ctx, pdf_array_get(ctx, obj, 2)))
 			fz_throw(ctx, FZ_ERROR_GENERIC, "xref stream object field 3 width an indirect object");
 
+		if (doc->file_reading_linearly && pdf_dict_get(ctx, trailer, PDF_NAME(Encrypt)))
+			fz_throw(ctx, FZ_ERROR_GENERIC, "Cannot read linearly with encryption");
+
 		w0 = pdf_array_get_int(ctx, obj, 0);
 		w1 = pdf_array_get_int(ctx, obj, 1);
 		w2 = pdf_array_get_int(ctx, obj, 2);
@@ -1404,10 +1407,13 @@ pdf_read_xref_sections(fz_context *ctx, pdf_document *doc, int64_t ofs, int read
 {
 	int i, len, cap;
 	int64_t *offsets;
+	int populated = 0;
 
 	len = 0;
 	cap = 10;
 	offsets = fz_malloc_array(ctx, cap, int64_t);
+
+	fz_var(populated);
 
 	fz_try(ctx)
 	{
@@ -1431,6 +1437,7 @@ pdf_read_xref_sections(fz_context *ctx, pdf_document *doc, int64_t ofs, int read
 			offsets[len++] = ofs;
 
 			pdf_populate_next_xref_level(ctx, doc);
+			populated = 1;
 			ofs = read_xref_section(ctx, doc, ofs);
 			if (!read_previous)
 				break;
@@ -1442,6 +1449,9 @@ pdf_read_xref_sections(fz_context *ctx, pdf_document *doc, int64_t ofs, int read
 	}
 	fz_catch(ctx)
 	{
+		/* Undo pdf_populate_next_xref_level if we've done that already. */
+		if (populated)
+			doc->num_xref_sections--;
 		fz_rethrow(ctx);
 	}
 }

commit 8e5fdac37133eefa09ca6652744b67603943feb8

Drop xref subsection reverted upon exception reading xref.
Commit 88e801f635d3585dc6f87a7b3cdd8f24b8c8d108 introduced
this regression which was picked up by OSS-fuzz in issue 55460.

pdf_read_xref_sections() first handles xref section recursion and then
calls pdf_populate_next_xref_level() before setting the populated flag.

At the beginning of read_xref_section() the trailer is obtained through a
call to pdf_read_xref(). The trailer is then inserted into the latest xref
section by calling pdf_set_populating_xref_trailer().

Next read_xref_section() looks for XRefStm and if so tries to read the xref
at that file offset by calling pdf_read_xref(). The fuzzed file provided by
OSS-fuzz has such an XRefStm entry, but it points to a fuzzed offset, i.e.
it contains neither the string "xref" signifying the old xref format, not a
digit signifying the new xref format. Therefore pdf_read_xref() throws an
exception.

This exception is caught by read_xref_section() which correctly drops its
local reference to the trailer, before rethrowing. The rethrown exception
is caught by pdf_read_xref_sections() which checks the populated flag which
indicates that the exception handling should decrement num_xref_sections in
order to undo what pdf_populate_next_xref_level() has previously done.

That sounds reasonable, but there is a problem.

Later pdf_drop_xref_sections() calls pdf_drop_xref_sections_imp() and
supplies the array of xref sections as well as the decremented
num_xref_sections. pdf_drop_xref_sections_imp() iterates over and drops the
resources referenced by each xref section, including any trailer. But since
the number of xref sections was decremented the last xref section is
ignored, which means that any resources referenced by that xref section
(e.g. the trailer mentioned above) will leak.

The obvious way to plug this leak is to drop any such resources referenced
by the xref section by calling pdf_drop_xref_subsec() directly from

source/pdf/pdf-xref.c
@@ -1513,7 +1513,10 @@ pdf_read_xref_sections(fz_context *ctx, pdf_document *doc, int64_t ofs, int read
 	{
 		/* Undo pdf_populate_next_xref_level if we've done that already. */
 		if (populated)
+		{
+			pdf_drop_xref_subsec(ctx, &doc->xref_sections[doc->num_xref_sections - 1]);
 			doc->num_xref_sections--;
+		}
 		fz_rethrow(ctx);
 	}
 }