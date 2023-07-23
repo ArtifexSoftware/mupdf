3f3de914a38ef440cf99c4b0c6f0f043c5fdffbe
Sebastian Rasmussen <sebras@gmail.com>
2023-07-24 00:21:17 +0200

    Check remaining data size before parsing bmp array header.
    This fixes oss-fuzz 59390.

--- source/fitz/load-bmp.c
+++ source/fitz/load-bmp.c
@@ -1262,7 +1262,15 @@ fz_load_bmp_subimage(fz_context *ctx, const unsigned char *buf, size_t len, int 
 	{
 		p = begin + nextoffset;
 
-		if (is_bitmap_array(p))
+		if (end - p < 14)
+			fz_throw(ctx, FZ_ERROR_GENERIC, "not enough data for bitmap array (%02x%02x) in bmp image", p[0], p[1]);
+
+		if (!is_bitmap_array(p))
+		{
+			fz_warn(ctx, "treating invalid subimage as end of file");
+			nextoffset = 0;
+		}
+		else
 		{
 			/* read16(p+0) == type */
 			/* read32(p+2) == size of this header in bytes */
@@ -1271,16 +1279,14 @@ fz_load_bmp_subimage(fz_context *ctx, const unsigned char *buf, size_t len, int 
 			/* read16(p+12) == suitable pely dimensions */
 			p += 14;
 		}
-		else if (nextoffset > 0)
-			fz_throw(ctx, FZ_ERROR_GENERIC, "unexpected bitmap array magic (%02x%02x) in bmp image", p[0], p[1]);
 
 		if (end - begin < nextoffset)
 		{
 			fz_warn(ctx, "treating invalid next subimage offset as end of file");
 			nextoffset = 0;
 		}
-
-		subimage--;
+		else
+			subimage--;
 
 	} while (subimage >= 0 && nextoffset > 0);
 
@@ -1302,14 +1308,22 @@ fz_load_bmp_subimage_count(fz_context *ctx, const unsigned char *buf, size_t len
 {
 	const unsigned char *begin = buf;
 	const unsigned char *end = buf + len;
-	int nextoffset = 0;
+	uint32_t nextoffset = 0;
 	int count = 0;
 
 	do
 	{
 		const unsigned char *p = begin + nextoffset;
 
-		if (is_bitmap_array(p))
+		if (end - p < 14)
+			fz_throw(ctx, FZ_ERROR_GENERIC, "not enough data for bitmap array (%02x%02x) in bmp image", p[0], p[1]);
+
+		if (!is_bitmap_array(p))
+		{
+			fz_warn(ctx, "treating invalid subimage as end of file");
+			nextoffset = 0;
+		}
+		else
 		{
 			/* read16(p+0) == type */
 			/* read32(p+2) == size of this header in bytes */
@@ -1318,8 +1332,6 @@ fz_load_bmp_subimage_count(fz_context *ctx, const unsigned char *buf, size_t len
 			/* read16(p+12) == suitable pely dimensions */
 			p += 14;
 		}
-		else if (nextoffset > 0)
-			fz_throw(ctx, FZ_ERROR_GENERIC, "unexpected bitmap array magic (%02x%02x) in bmp image", p[0], p[1]);
 
 		if (end - begin < nextoffset)
 		{
