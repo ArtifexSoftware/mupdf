9b3054de45f7ec747c241dcfd5ee834448235f5f
Sebastian Rasmussen <sebras@gmail.com>
2023-11-29 23:37:50 +0100

    Mark variable fz_var so that a stale pointer is not freed upon exception.

--- source/pdf/pdf-xref.c
+++ source/pdf/pdf-xref.c
@@ -1570,6 +1570,7 @@ pdf_read_xref_sections(fz_context *ctx, pdf_document *doc, int64_t ofs, int read
 	offsets = fz_malloc_array(ctx, cap, int64_t);
 
 	fz_var(populated);
+	fz_var(offsets);
 
 	fz_try(ctx)
 	{
