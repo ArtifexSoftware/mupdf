e8a9bf4626837ac418eca8617176ba0f68542a61
Robin Watts <Robin.Watts@artifex.com>
2023-12-14 14:20:09 +0000

    Repurpose int param in begin_structure device calls.
    Previously, the int param given in begin_structure calls was
    intended to allow callees to spot the same structure element
    being reused - hence it was called 'uid'.
    
    Here we change this slightly. It is now called 'idx' because
    it gives the index into the array of sibling structure elements
    at that level.
    
    Consider for instance, the following structure tree:
    
      Book
       |- Document
       |   |- Chapter
       |   |   |- Section
       |   |   `- Section
       |   |- Chapter
       |   |   |- Section
       |   |   `- Section                    <- A
       |   |- Chapter
       |   |   |- Section
       |   |   `- Section
       |   `- Chapter
       |- Document
       |   |- Chapter
       |   |- Chapter
       |   |- Chapter
       |   `- Chapter
       `- Document
           |- Chapter
           |- Chapter
           |- Chapter
           `- Chapter
    
    Point A in this tree can be identified by being nested within
    the calls:
    
     begin_structure(Book, 0)
      begin_structure(Document, 0)
       begin_structure(Chapter, 1)
        begin_structure(Section, 1)
    
    This allows the correct structure to be derived even if the different
    elements are (perhaps for graphical composition reasons) sent in
    other than purely logical order.
    
    It also allows several pages to be sent and for the document structure
    to be correctly carried across between their pages.

--- source/pdf/pdf-xref.c
+++ source/pdf/pdf-xref.c
@@ -3146,6 +3146,7 @@ pdf_new_document(fz_context *ctx, fz_stream *file)
 	doc->super.page_label = pdf_page_label_imp;
 	doc->super.lookup_metadata = (fz_document_lookup_metadata_fn*)pdf_lookup_metadata;
 	doc->super.set_metadata = (fz_document_set_metadata_fn*)pdf_set_metadata;
+	doc->super.run_structure = (fz_document_run_structure_fn *)pdf_run_document_structure;
 
 	pdf_lexbuf_init(ctx, &doc->lexbuf.base, PDF_LEXBUF_LARGE);
 	doc->file = fz_keep_stream(ctx, file);
