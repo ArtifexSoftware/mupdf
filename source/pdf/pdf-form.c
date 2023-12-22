ArtifexSoftware/mupdf

commit b905397ff8551dd5ede84bffd0eec67ae664f1c6

Use convenience functions in more places.

source/pdf/pdf-form.c
@@ -267,7 +267,7 @@ static void reset_form_field(fz_context *ctx, pdf_document *doc, pdf_obj *field)
 				if (pdf_is_dict(ctx, n) && !pdf_dict_get(ctx, n, leafv))
 					leafv = NULL;
 
-				if (!leafv)
+				if (!pdf_is_name(ctx, leafv))
 					leafv = PDF_NAME(Off);
 				pdf_dict_put(ctx, field, PDF_NAME(AS), leafv);
 			}
@@ -905,7 +905,6 @@ void pdf_field_set_display(fz_context *ctx, pdf_obj *field, int d)
 	{
 		int mask = (PDF_ANNOT_IS_HIDDEN|PDF_ANNOT_IS_PRINT|PDF_ANNOT_IS_NO_VIEW);
 		int f = pdf_dict_get_int(ctx, field, PDF_NAME(F)) & ~mask;
-		pdf_obj *fo;
 
 		switch (d)
 		{
@@ -922,8 +921,7 @@ void pdf_field_set_display(fz_context *ctx, pdf_obj *field, int d)
 			break;
 		}
 
-		fo = pdf_new_int(ctx, f);
-		pdf_dict_put_drop(ctx, field, PDF_NAME(F), fo);
+		pdf_dict_put_int(ctx, field, PDF_NAME(F), f);
 	}
 	else
 	{
@@ -1328,7 +1326,7 @@ int pdf_choice_widget_value(fz_context *ctx, pdf_annot *tw, const char *opts[])
 void pdf_choice_widget_set_value(fz_context *ctx, pdf_annot *tw, int n, const char *opts[])
 {
 	pdf_annot *annot = (pdf_annot *)tw;
-	pdf_obj *optarr = NULL, *opt;
+	pdf_obj *optarr = NULL;
 	int i;
 
 	if (!annot)
@@ -1344,18 +1342,12 @@ void pdf_choice_widget_set_value(fz_context *ctx, pdf_annot *tw, int n, const ch
 			optarr = pdf_new_array(ctx, annot->page->doc, n);
 
 			for (i = 0; i < n; i++)
-			{
-				opt = pdf_new_text_string(ctx, opts[i]);
-				pdf_array_push_drop(ctx, optarr, opt);
-			}
+				pdf_array_push_text_string(ctx, optarr, opts[i]);
 
 			pdf_dict_put_drop(ctx, annot->obj, PDF_NAME(V), optarr);
 		}
 		else
-		{
-			opt = pdf_new_text_string(ctx, opts[0]);
-			pdf_dict_put_drop(ctx, annot->obj, PDF_NAME(V), opt);
-		}
+			pdf_dict_put_text_string(ctx, annot->obj, PDF_NAME(V), opts[0]);
 
 		/* FIXME: when n > 1, we should be regenerating the indexes */
 		pdf_dict_del(ctx, annot->obj, PDF_NAME(I));
@@ -1846,7 +1838,7 @@ lock_field(fz_context *ctx, pdf_obj *f)
 		!pdf_name_eq(ctx, pdf_dict_get(ctx, f, PDF_NAME(Subtype)), PDF_NAME(Widget)))
 		return;
 
-	pdf_dict_put(ctx, f, PDF_NAME(Ff), pdf_new_int(ctx, ff | PDF_FIELD_IS_READ_ONLY));
+	pdf_dict_put_int(ctx, f, PDF_NAME(Ff), ff | PDF_FIELD_IS_READ_ONLY);
 }
 
 static void
@@ -1908,15 +1900,12 @@ void pdf_signature_set_value(fz_context *ctx, pdf_document *doc, pdf_obj *field,
 		pdf_dict_put(ctx, v, PDF_NAME(Type), PDF_NAME(Sig));
 		pdf_dict_put_date(ctx, v, PDF_NAME(M), stime);
 
-		o = pdf_new_array(ctx, doc, 1);
-		pdf_dict_put(ctx, v, PDF_NAME(Reference), o);
-		r = pdf_new_dict(ctx, doc, 4);
-		pdf_array_put(ctx, o, 0, r);
+		o = pdf_dict_put_array(ctx, v, PDF_NAME(Reference), 1);
+		r = pdf_array_put_dict(ctx, o, 0, 4);
 		pdf_dict_put(ctx, r, PDF_NAME(Data), pdf_dict_get(ctx, pdf_trailer(ctx, doc), PDF_NAME(Root)));
 		pdf_dict_put(ctx, r, PDF_NAME(TransformMethod), PDF_NAME(FieldMDP));
 		pdf_dict_put(ctx, r, PDF_NAME(Type), PDF_NAME(SigRef));
-		t = pdf_new_dict(ctx, doc, 5);
-		pdf_dict_put(ctx, r, PDF_NAME(TransformParams), t);
+		t = pdf_dict_put_dict(ctx, r, PDF_NAME(TransformParams), 5);
 
 		l = pdf_dict_getp(ctx, field, "Lock/Action");
 		if (l)

platform/java/jni/pdfobject.c
@@ -561,7 +561,7 @@ FUN(PDFObject_putArrayString)(JNIEnv *env, jobject self, jint index, jstring jst
 	fz_try(ctx)
 	{
 		if (str)
-			pdf_array_put_drop(ctx, arr, index, pdf_new_string(ctx, str, strlen(str)));
+			pdf_array_put_string(ctx, arr, index, str, strlen(str));
 		else
 			pdf_array_put(ctx, arr, index, PDF_NULL);
 	}

source/pdf/pdf-annot.c
@@ -454,10 +454,7 @@ pdf_create_annot_raw(fz_context *ctx, pdf_page *page, enum pdf_annot_type type)
 
 		annot_arr = pdf_dict_get(ctx, page->obj, PDF_NAME(Annots));
 		if (!pdf_is_array(ctx, annot_arr))
-		{
-			annot_arr = pdf_new_array(ctx, doc, 0);
-			pdf_dict_put_drop(ctx, page->obj, PDF_NAME(Annots), annot_arr);
-		}
+			annot_arr = pdf_dict_put_array(ctx, page->obj, PDF_NAME(Annots), 0);
 
 		pdf_dict_put(ctx, annot_obj, PDF_NAME(Type), PDF_NAME(Annot));
 		pdf_dict_put_name(ctx, annot_obj, PDF_NAME(Subtype), type_str);
@@ -536,19 +533,16 @@ pdf_create_link(fz_context *ctx, pdf_page *page, fz_rect bbox, const char *uri)
 
 		annot_arr = pdf_dict_get(ctx, page->obj, PDF_NAME(Annots));
 		if (!pdf_is_array(ctx, annot_arr))
-		{
-			annot_arr = pdf_new_array(ctx, doc, 0);
-			pdf_dict_put_drop(ctx, page->obj, PDF_NAME(Annots), annot_arr);
-		}
+			annot_arr = pdf_dict_put_array(ctx, page->obj, PDF_NAME(Annots), 0);
 
 		pdf_dict_put(ctx, annot_obj, PDF_NAME(Type), PDF_NAME(Annot));
 		pdf_dict_put(ctx, annot_obj, PDF_NAME(Subtype), PDF_NAME(Link));
 		pdf_dict_put_rect(ctx, annot_obj, PDF_NAME(Rect), rect);
-		bs = pdf_new_dict(ctx, doc, 4);
+
+		bs = pdf_dict_put_array(ctx, annot_obj, PDF_NAME(BS), 4);
 		pdf_dict_put(ctx, bs, PDF_NAME(S), PDF_NAME(S));
 		pdf_dict_put(ctx, bs, PDF_NAME(Type), PDF_NAME(Border));
 		pdf_dict_put_int(ctx, bs, PDF_NAME(W), 0);
-		pdf_dict_put(ctx, annot_obj, PDF_NAME(BS), bs);
 
 		pdf_dict_put_drop(ctx, annot_obj, PDF_NAME(A),
 			pdf_new_action_from_link(ctx, doc, uri));
@@ -575,7 +569,6 @@ pdf_create_link(fz_context *ctx, pdf_page *page, fz_rect bbox, const char *uri)
 	}
 	fz_always(ctx)
 	{
-		pdf_drop_obj(ctx, bs);
 		pdf_drop_obj(ctx, annot_obj);
 		pdf_drop_obj(ctx, ind_obj);
 	}
@@ -1375,7 +1368,6 @@ pdf_set_annot_line_ending_styles(fz_context *ctx, pdf_annot *annot,
 		enum pdf_line_ending start_style,
 		enum pdf_line_ending end_style)
 {
-	pdf_document *doc = annot->page->doc;
 	pdf_obj *style;
 
 	begin_annot_op(ctx, annot, "Set line endings");
@@ -1383,8 +1375,7 @@ pdf_set_annot_line_ending_styles(fz_context *ctx, pdf_annot *annot,
 	fz_try(ctx)
 	{
 		check_allowed_subtypes(ctx, annot, PDF_NAME(LE), line_ending_subtypes);
-		style = pdf_new_array(ctx, doc, 2);
-		pdf_dict_put_drop(ctx, annot->obj, PDF_NAME(LE), style);
+		style = pdf_dict_put_array(ctx, annot->obj, PDF_NAME(LE), 2);
 		pdf_array_put_drop(ctx, style, 0, pdf_name_from_line_ending(ctx, start_style));
 		pdf_array_put_drop(ctx, style, 1, pdf_name_from_line_ending(ctx, end_style));
 		end_annot_op(ctx, annot);
@@ -2046,7 +2037,6 @@ static int pdf_annot_color_rgb(fz_context *ctx, pdf_obj *arr, float rgb[3])
 
 static void pdf_set_annot_color_imp(fz_context *ctx, pdf_annot *annot, pdf_obj *key, int n, const float *color, pdf_obj **allowed)
 {
-	pdf_document *doc = annot->page->doc;
 	pdf_obj *arr;
 
 	if (allowed)
@@ -2056,7 +2046,7 @@ static void pdf_set_annot_color_imp(fz_context *ctx, pdf_annot *annot, pdf_obj *
 	if (!color)
 		fz_throw(ctx, FZ_ERROR_GENERIC, "no color given");
 
-	arr = pdf_new_array(ctx, doc, n);
+	arr = pdf_dict_put_array(ctx, annot->obj, key, n);
 	fz_try(ctx)
 	{
 		switch (n)
@@ -2078,12 +2068,8 @@ static void pdf_set_annot_color_imp(fz_context *ctx, pdf_annot *annot, pdf_obj *
 		}
 	}
 	fz_catch(ctx)
-	{
-		pdf_drop_obj(ctx, arr);
 		fz_rethrow(ctx);
-	}
 
-	pdf_dict_put_drop(ctx, annot->obj, key, arr);
 	pdf_dirty_annot(ctx, annot);
 }
 
@@ -2297,8 +2283,7 @@ pdf_set_annot_line(fz_context *ctx, pdf_annot *annot, fz_point a, fz_point b)
 		a = fz_transform_point(a, inv_page_ctm);
 		b = fz_transform_point(b, inv_page_ctm);
 
-		line = pdf_new_array(ctx, annot->page->doc, 4);
-		pdf_dict_put_drop(ctx, annot->obj, PDF_NAME(L), line);
+		line = pdf_dict_put_array(ctx, annot->obj, PDF_NAME(L), 4);
 		pdf_array_push_real(ctx, line, a.x);
 		pdf_array_push_real(ctx, line, a.y);
 		pdf_array_push_real(ctx, line, b.x);
@@ -2379,7 +2364,6 @@ pdf_annot_vertex(fz_context *ctx, pdf_annot *annot, int i)
 void
 pdf_set_annot_vertices(fz_context *ctx, pdf_annot *annot, int n, const fz_point *v)
 {
-	pdf_document *doc = annot->page->doc;
 	fz_matrix page_ctm, inv_page_ctm;
 	pdf_obj *vertices;
 	fz_point point;
@@ -2396,14 +2380,13 @@ pdf_set_annot_vertices(fz_context *ctx, pdf_annot *annot, int n, const fz_point 
 		pdf_page_transform(ctx, annot->page, NULL, &page_ctm);
 		inv_page_ctm = fz_invert_matrix(page_ctm);
 
-		vertices = pdf_new_array(ctx, doc, n * 2);
+		vertices = pdf_dict_put_array(ctx, annot->obj, PDF_NAME(Vertices), n * 2);
 		for (i = 0; i < n; ++i)
 		{
 			point = fz_transform_point(v[i], inv_page_ctm);
 			pdf_array_push_real(ctx, vertices, point.x);
 			pdf_array_push_real(ctx, vertices, point.y);
 		}
-		pdf_dict_put_drop(ctx, annot->obj, PDF_NAME(Vertices), vertices);
 		end_annot_op(ctx, annot);
 	}
 	fz_catch(ctx)
@@ -2436,7 +2419,6 @@ void pdf_clear_annot_vertices(fz_context *ctx, pdf_annot *annot)
 
 void pdf_add_annot_vertex(fz_context *ctx, pdf_annot *annot, fz_point p)
 {
-	pdf_document *doc = annot->page->doc;
 	fz_matrix page_ctm, inv_page_ctm;
 	pdf_obj *vertices;
 
@@ -2451,10 +2433,7 @@ void pdf_add_annot_vertex(fz_context *ctx, pdf_annot *annot, fz_point p)
 
 		vertices = pdf_dict_get(ctx, annot->obj, PDF_NAME(Vertices));
 		if (!pdf_is_array(ctx, vertices))
-		{
-			vertices = pdf_new_array(ctx, doc, 32);
-			pdf_dict_put_drop(ctx, annot->obj, PDF_NAME(Vertices), vertices);
-		}
+			vertices = pdf_dict_put_array(ctx, annot->obj, PDF_NAME(Vertices), 32);
 
 		p = fz_transform_point(p, inv_page_ctm);
 		pdf_array_push_real(ctx, vertices, p.x);
@@ -2487,8 +2466,8 @@ void pdf_set_annot_vertex(fz_context *ctx, pdf_annot *annot, int i, fz_point p)
 		p = fz_transform_point(p, inv_page_ctm);
 
 		vertices = pdf_dict_get(ctx, annot->obj, PDF_NAME(Vertices));
-		pdf_array_put_drop(ctx, vertices, i * 2 + 0, pdf_new_real(ctx, p.x));
-		pdf_array_put_drop(ctx, vertices, i * 2 + 1, pdf_new_real(ctx, p.y));
+		pdf_array_put_real(ctx, vertices, i * 2 + 0, p.x);
+		pdf_array_put_real(ctx, vertices, i * 2 + 1, p.x);
 		end_annot_op(ctx, annot);
 	}
 	fz_catch(ctx)
@@ -2573,7 +2552,6 @@ pdf_annot_quad_point(fz_context *ctx, pdf_annot *annot, int idx)
 void
 pdf_set_annot_quad_points(fz_context *ctx, pdf_annot *annot, int n, const fz_quad *q)
 {
-	pdf_document *doc = annot->page->doc;
 	fz_matrix page_ctm, inv_page_ctm;
 	pdf_obj *quad_points;
 	fz_quad quad;
@@ -2590,7 +2568,7 @@ pdf_set_annot_quad_points(fz_context *ctx, pdf_annot *annot, int n, const fz_qua
 		pdf_page_transform(ctx, annot->page, NULL, &page_ctm);
 		inv_page_ctm = fz_invert_matrix(page_ctm);
 
-		quad_points = pdf_new_array(ctx, doc, n);
+		quad_points = pdf_dict_put_array(ctx, annot->obj, PDF_NAME(QuadPoints), n);
 		for (i = 0; i < n; ++i)
 		{
 			quad = fz_transform_quad(q[i], inv_page_ctm);
@@ -2603,7 +2581,7 @@ pdf_set_annot_quad_points(fz_context *ctx, pdf_annot *annot, int n, const fz_qua
 			pdf_array_push_real(ctx, quad_points, quad.lr.x);
 			pdf_array_push_real(ctx, quad_points, quad.lr.y);
 		}
-		pdf_dict_put_drop(ctx, annot->obj, PDF_NAME(QuadPoints), quad_points);
+
 		end_annot_op(ctx, annot);
 	}
 	fz_catch(ctx)
@@ -2638,7 +2616,6 @@ pdf_clear_annot_quad_points(fz_context *ctx, pdf_annot *annot)
 void
 pdf_add_annot_quad_point(fz_context *ctx, pdf_annot *annot, fz_quad quad)
 {
-	pdf_document *doc = annot->page->doc;
 	fz_matrix page_ctm, inv_page_ctm;
 	pdf_obj *quad_points;
 
@@ -2653,10 +2630,7 @@ pdf_add_annot_quad_point(fz_context *ctx, pdf_annot *annot, fz_quad quad)
 
 		quad_points = pdf_dict_get(ctx, annot->obj, PDF_NAME(QuadPoints));
 		if (!pdf_is_array(ctx, quad_points))
-		{
-			quad_points = pdf_new_array(ctx, doc, 8);
-			pdf_dict_put_drop(ctx, annot->obj, PDF_NAME(QuadPoints), quad_points);
-		}
+			quad_points = pdf_dict_put_array(ctx, annot->obj, PDF_NAME(QuadPoints), 8);
 
 		/* Contrary to the specification, the points within a QuadPoint are NOT ordered
 		 * in a counterclockwise fashion. Experiments with Adobe's implementation
@@ -2773,7 +2747,6 @@ pdf_annot_ink_list_stroke_vertex(fz_context *ctx, pdf_annot *annot, int i, int k
 void
 pdf_set_annot_ink_list(fz_context *ctx, pdf_annot *annot, int n, const int *count, const fz_point *v)
 {
-	pdf_document *doc = annot->page->doc;
 	fz_matrix page_ctm, inv_page_ctm;
 	pdf_obj *ink_list = NULL, *stroke;
 	fz_point point;
@@ -2790,11 +2763,11 @@ pdf_set_annot_ink_list(fz_context *ctx, pdf_annot *annot, int n, const int *coun
 		pdf_page_transform(ctx, annot->page, NULL, &page_ctm);
 		inv_page_ctm = fz_invert_matrix(page_ctm);
 
-		ink_list = pdf_new_array(ctx, doc, n);
+		ink_list = pdf_dict_put_array(ctx, annot->obj, PDF_NAME(InkList), n);
 		for (i = 0; i < n; ++i)
 		{
-			stroke = pdf_new_array(ctx, doc, count[i] * 2);
-			pdf_array_push_drop(ctx, ink_list, stroke);
+			stroke = pdf_array_push_array(ctx, ink_list, count[i] * 2);
+
 			/* Although we have dropped our reference to stroke,
 			 * it's still valid because we ink_list holds one, and
 			 * we hold a reference to that. */
@@ -2805,12 +2778,8 @@ pdf_set_annot_ink_list(fz_context *ctx, pdf_annot *annot, int n, const int *coun
 				pdf_array_push_real(ctx, stroke, point.y);
 			}
 		}
-		pdf_dict_put_drop(ctx, annot->obj, PDF_NAME(InkList), ink_list);
-		ink_list = NULL;
 		end_annot_op(ctx, annot);
 	}
-	fz_always(ctx)
-		pdf_drop_obj(ctx, ink_list);
 	fz_catch(ctx)
 	{
 		abandon_annot_op(ctx, annot);

source/pdf/pdf-clean-file.c
@@ -235,7 +235,7 @@ static int strip_outlines(fz_context *ctx, pdf_document *doc, pdf_obj *outlines,
 
 static void retainpages(fz_context *ctx, globals *glo, int argc, char **argv)
 {
-	pdf_obj *oldroot, *root, *pages, *kids, *countobj, *olddests;
+	pdf_obj *oldroot, *root, *pages, *kids, *olddests;
 	pdf_document *doc = glo->doc;
 	int argidx = 0;
 	pdf_obj *names_list = NULL;
@@ -265,7 +265,7 @@ static void retainpages(fz_context *ctx, globals *glo, int argc, char **argv)
 	pdf_update_object(ctx, doc, pdf_to_num(ctx, oldroot), root);
 
 	/* Create a new kids array with only the pages we want to keep */
-	kids = pdf_new_array(ctx, doc, 1);
+	kids = pdf_dict_put_array(ctx, pages, PDF_NAME(Kids), 1);
 
 	/* Retain pages specified */
 	while (argc - argidx)
@@ -288,10 +288,8 @@ static void retainpages(fz_context *ctx, globals *glo, int argc, char **argv)
 		argidx++;
 	}
 
-	/* Update page count and kids array */
-	countobj = pdf_new_int(ctx, pdf_array_len(ctx, kids));
-	pdf_dict_put_drop(ctx, pages, PDF_NAME(Count), countobj);
-	pdf_dict_put_drop(ctx, pages, PDF_NAME(Kids), kids);
+	/* Update page count */
+	pdf_dict_put_int(ctx, pages, PDF_NAME(Count), pdf_array_len(ctx, kids));
 
 	pagecount = pdf_count_pages(ctx, doc);
 	page_object_nums = fz_calloc(ctx, pagecount, sizeof(*page_object_nums));
@@ -307,11 +305,12 @@ static void retainpages(fz_context *ctx, globals *glo, int argc, char **argv)
 	 * it's safe at least. */
 	if (olddests)
 	{
-		pdf_obj *names = pdf_new_dict(ctx, doc, 1);
-		pdf_obj *dests = pdf_new_dict(ctx, doc, 1);
+		pdf_obj *names, *dests;
 		int len = pdf_dict_len(ctx, olddests);
 
-		names_list = pdf_new_array(ctx, doc, 32);
+		names = pdf_dict_put_dict(ctx, root, PDF_NAME(Names), 1);
+		dests = pdf_dict_put_dict(ctx, names, PDF_NAME(Dests), 1);
+		names_list = pdf_dict_put_array(ctx, dests, PDF_NAME(Names), 32);
 
 		for (i = 0; i < len; i++)
 		{
@@ -322,18 +321,11 @@ static void retainpages(fz_context *ctx, globals *glo, int argc, char **argv)
 			dest = pdf_array_get(ctx, dest ? dest : val, 0);
 			if (dest_is_valid_page(ctx, dest, page_object_nums, pagecount))
 			{
-				pdf_obj *key_str = pdf_new_string(ctx, pdf_to_name(ctx, key), strlen(pdf_to_name(ctx, key)));
-				pdf_array_push_drop(ctx, names_list, key_str);
+				pdf_array_push_string(ctx, names_list, pdf_to_name(ctx, key), strlen(pdf_to_name(ctx, key)));
 				pdf_array_push(ctx, names_list, val);
 			}
 		}
 
-		pdf_dict_put(ctx, dests, PDF_NAME(Names), names_list);
-		pdf_dict_put(ctx, names, PDF_NAME(Dests), dests);
-		pdf_dict_put(ctx, root, PDF_NAME(Names), names);
-
-		pdf_drop_obj(ctx, names);
-		pdf_drop_obj(ctx, dests);
 		pdf_drop_obj(ctx, olddests);
 	}
 

source/pdf/pdf-device.c
@@ -1091,7 +1091,6 @@ static void
 pdf_dev_begin_group(fz_context *ctx, fz_device *dev, fz_rect bbox, fz_colorspace *cs, int isolated, int knockout, int blendmode, float alpha)
 {
 	pdf_device *pdev = (pdf_device*)dev;
-	pdf_document *doc = pdev->doc;
 	int num;
 	pdf_obj *form_ref;
 	gstate *gs;
@@ -1109,10 +1108,9 @@ pdf_dev_begin_group(fz_context *ctx, fz_device *dev, fz_rect bbox, fz_colorspace
 		if (obj == NULL)
 		{
 			/* No, better make one */
-			obj = pdf_new_dict(ctx, doc, 2);
+			obj = pdf_dict_puts_dict(ctx, pdev->resources, text, 2);
 			pdf_dict_put(ctx, obj, PDF_NAME(Type), PDF_NAME(ExtGState));
 			pdf_dict_put_name(ctx, obj, PDF_NAME(BM), fz_blendmode_name(blendmode));
-			pdf_dict_putp_drop(ctx, pdev->resources, text, obj);
 		}
 	}
 

source/pdf/pdf-page.c
@@ -1609,8 +1609,7 @@ pdf_adjust_page_labels(fz_context *ctx, pdf_document *doc, int index, int adjust
 
 		// Increase/decrease the indices in the name tree
 		for (; i < pdf_array_len(ctx, range.nums); i += 2)
-			pdf_array_put_drop(ctx, range.nums, i,
-				pdf_new_int(ctx, pdf_array_get_int(ctx, range.nums, i) + adjust));
+			pdf_array_put_int(ctx, range.nums, i, pdf_array_get_int(ctx, range.nums, i) + adjust);
 
 		// TODO: delete page labels that have no effect (zero range)
 

source/pdf/pdf-parse.c
@@ -702,10 +702,7 @@ pdf_parse_dict(fz_context *ctx, pdf_document *doc, fz_stream *file, pdf_lexbuf *
 				if (tok == PDF_TOK_CLOSE_DICT || tok == PDF_TOK_NAME ||
 					(tok == PDF_TOK_KEYWORD && !strcmp(buf->scratch, "ID")))
 				{
-					val = pdf_new_int(ctx, a);
-					pdf_dict_put(ctx, dict, key, val);
-					pdf_drop_obj(ctx, val);
-					val = NULL;
+					pdf_dict_put_int(ctx, dict, key, a);
 					pdf_drop_obj(ctx, key);
 					key = NULL;
 					goto skip;

source/pdf/pdf-repair.c
@@ -648,10 +648,7 @@ pdf_repair_xref(fz_context *ctx, pdf_document *doc)
 		pdf_drop_obj(ctx, obj);
 		obj = NULL;
 
-		obj = pdf_new_int(ctx, maxnum + 1);
-		pdf_dict_put(ctx, pdf_trailer(ctx, doc), PDF_NAME(Size), obj);
-		pdf_drop_obj(ctx, obj);
-		obj = NULL;
+		pdf_dict_put_int(ctx, pdf_trailer(ctx, doc), PDF_NAME(Size), maxnum + 1);
 
 		if (roots)
 		{

commit 28e3e765728fdbf0dd97fcfbf9e044a20f42c0ba

No need to drop references from the convenience functions.
That is indeed the point of the convenince functions!
This was caused by commit b905397ff8551dd5ede84bffd0eec67ae664f1c6.

source/pdf/pdf-form.c
@@ -1954,9 +1954,6 @@ void pdf_signature_set_value(fz_context *ctx, pdf_document *doc, pdf_obj *field,
 	fz_always(ctx)
 	{
 		pdf_drop_obj(ctx, v);
-		pdf_drop_obj(ctx, o);
-		pdf_drop_obj(ctx, r);
-		pdf_drop_obj(ctx, t);
 		pdf_drop_obj(ctx, b);
 		fz_free(ctx, buf);
 	}