5912b5777627c6f8341f45b0841f6721409e2ee9
Sebastian Rasmussen <sebras@gmail.com>
2024-01-05 04:56:29 +0100

    Add PDF write option to set compression effort.

--- source/pdf/pdf-write.c
+++ source/pdf/pdf-write.c
@@ -3246,6 +3246,8 @@ pdf_parse_write_options(fz_context *ctx, pdf_write_options *opts, const char *ar
 		opts->do_compress_fonts = fz_option_eq(val, "yes");
 	if (fz_has_option(ctx, args, "compress-images", &val))
 		opts->do_compress_images = fz_option_eq(val, "yes");
+	if (fz_has_option(ctx, args, "compression-effort", &val))
+		opts->compression_effort = fz_atoi(val);
 	if (fz_has_option(ctx, args, "ascii", &val))
 		opts->do_ascii = fz_option_eq(val, "yes");
 	if (fz_has_option(ctx, args, "pretty", &val))
