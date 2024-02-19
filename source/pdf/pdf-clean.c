1861b030dcfb2b89db7f663221db366c1dd56156
Robin Watts <Robin.Watts@artifex.com>
2024-02-19 19:06:01 +0000

    Fix vanished whitespace in redaction filter.
    Previously, whenever we output an operator in the buffer, we'd
    have added trailing whitespace. This means that when we come to
    add the suffix of the page to mark the redacted areas, we'd know
    that we didn't need to insert any whitespace.
    
    Now we are much stingier with the whitespace we output. This means
    we will always need to output some whitespace before we add any
    extra instructions onto the end of the page.

--- source/pdf/pdf-clean.c
+++ source/pdf/pdf-clean.c
@@ -460,7 +460,7 @@ pdf_redact_end_page(fz_context *ctx, fz_buffer *buf, void *opaque)
 	pdf_obj *qp;
 	int i, n;
 
-	fz_append_string(ctx, buf, "0 g\n");
+	fz_append_string(ctx, buf, " 0 g\n");
 
 	for (annot = pdf_first_annot(ctx, page); annot; annot = pdf_next_annot(ctx, annot))
 	{
