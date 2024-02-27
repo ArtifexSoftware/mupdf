83b5d0ddd4f400e3bab72e3c78cbfa6ba1ee071a
Sebastian Rasmussen <sebras@gmail.com>
2024-02-27 13:57:34 +0800

    xps: Only set parsed point coords if all obtained.
    Prior to this commit parsing a point where the input string
    was the empty string would set the point coords to uninitialized
    values. Now, the output coords are left untouched unless enough
    coords can be parsed.

--- source/xps/xps-path.c
+++ source/xps/xps-path.c
@@ -28,12 +28,16 @@
 #include <stdlib.h>
 
 static char *
-xps_parse_float_array(fz_context *ctx, xps_document *doc, char *s, int num, float *x)
+xps_parse_float_array(fz_context *ctx, xps_document *doc, char *s, int num, int *obtained, float *x)
 {
 	int k = 0;
 
 	if (s == NULL || *s == 0)
+	{
+		if (obtained)
+			*obtained = k;
 		return NULL;
+	}
 
 	while (*s)
 	{
@@ -47,6 +51,8 @@ xps_parse_float_array(fz_context *ctx, xps_document *doc, char *s, int num, floa
 		if (++k == num)
 			break;
 	}
+	if (obtained)
+		*obtained = k;
 	return s;
 }
 
@@ -55,10 +61,14 @@ xps_parse_point(fz_context *ctx, xps_document *doc, char *s_in, float *x, float 
 {
 	char *s_out = s_in;
 	float xy[2];
+	int obtained = 0;
 
-	s_out = xps_parse_float_array(ctx, doc, s_out, 2, &xy[0]);
-	*x = xy[0];
-	*y = xy[1];
+	s_out = xps_parse_float_array(ctx, doc, s_out, 2, &obtained, &xy[0]);
+	if (obtained >= 2)
+	{
+		*x = xy[0];
+		*y = xy[1];
+	}
 	return s_out;
 }
 
