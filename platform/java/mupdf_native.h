ArtifexSoftware/mupdf

commit 8a2d046f46e1467d442aa1cc01c4a80036ef549e

Add the start of a simple Zugferd API.

platform/java/mupdf_native.h
@@ -3691,6 +3691,22 @@ extern "C" {
 #define com_artifex_mupdf_fitz_PDFDocument_PAGE_LABEL_ALPHA_UC 65L
 #undef com_artifex_mupdf_fitz_PDFDocument_PAGE_LABEL_ALPHA_LC
 #define com_artifex_mupdf_fitz_PDFDocument_PAGE_LABEL_ALPHA_LC 97L
+#undef com_artifex_mupdf_fitz_PDFDocument_NOT_ZUGFERD
+#define com_artifex_mupdf_fitz_PDFDocument_NOT_ZUGFERD 0L
+#undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_COMFORT
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_COMFORT 0L
+#undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC 1L
+#undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_EXTENDED
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_EXTENDED 2L
+#undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC_WL
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC_WL 3L
+#undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_MINIMUM
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_MINIMUM 4L
+#undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_XRECHNUNG
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_XRECHNUNG 5L
+#undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_UNKNOWN
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_UNKNOWN 6L
 /*
  * Class:     com_artifex_mupdf_fitz_PDFDocument
  * Method:    newNative
@@ -4338,6 +4354,30 @@ JNIEXPORT jint JNICALL Java_com_artifex_mupdf_fitz_PDFDocument_countAssociatedFi
  */
 JNIEXPORT jobject JNICALL Java_com_artifex_mupdf_fitz_PDFDocument_associatedFile
   (JNIEnv *, jobject, jint);
+
+/*
+ * Class:     com_artifex_mupdf_fitz_PDFDocument
+ * Method:    zugferdProfile
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_com_artifex_mupdf_fitz_PDFDocument_zugferdProfile
+  (JNIEnv *, jobject);
+
+/*
+ * Class:     com_artifex_mupdf_fitz_PDFDocument
+ * Method:    zugferdVersion
+ * Signature: ()F
+ */
+JNIEXPORT jfloat JNICALL Java_com_artifex_mupdf_fitz_PDFDocument_zugferdVersion
+  (JNIEnv *, jobject);
+
+/*
+ * Class:     com_artifex_mupdf_fitz_PDFDocument
+ * Method:    zugferdXML
+ * Signature: ()Lcom/artifex/mupdf/fitz/Buffer;
+ */
+JNIEXPORT jobject JNICALL Java_com_artifex_mupdf_fitz_PDFDocument_zugferdXML
+  (JNIEnv *, jobject);
 
 #ifdef __cplusplus
 }

platform/java/src/com/artifex/mupdf/fitz/PDFDocument.java
@@ -282,4 +282,17 @@ public class PDFDocument extends Document
 
 	public native int countAssociatedFiles();
 	public native PDFObject associatedFile(int idx);
+
+	public static final int NOT_ZUGFERD = 0;
+	public static final int ZUGFERD_COMFORT = 0;
+	public static final int ZUGFERD_BASIC = 1;
+	public static final int ZUGFERD_EXTENDED = 2;
+	public static final int ZUGFERD_BASIC_WL = 3;
+	public static final int ZUGFERD_MINIMUM = 4;
+	public static final int ZUGFERD_XRECHNUNG = 5;
+	public static final int ZUGFERD_UNKNOWN = 6;
+
+	public native int zugferdProfile();
+	public native float zugferdVersion();
+	public native Buffer zugferdXML();
 }

include/mupdf/pdf/zugferd.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2004-2024 Artifex Software, Inc.
+// Copyright (C) 2024 Artifex Software, Inc.
 //
 // This file is part of MuPDF.
 //
@@ -20,39 +20,30 @@
 // Artifex Software, Inc., 39 Mesa Street, Suite 108A, San Francisco,
 // CA 94129, USA, for further information.
 
-#ifndef MUPDF_PDF_H
-#define MUPDF_PDF_H
+#ifndef MUPDF_PDF_ZUGFERD_H
+#define MUPDF_PDF_ZUGFERD_H
 
-#include "mupdf/fitz.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include "mupdf/pdf/object.h"
 #include "mupdf/pdf/document.h"
-#include "mupdf/pdf/parse.h"
-#include "mupdf/pdf/xref.h"
-#include "mupdf/pdf/crypt.h"
 
-#include "mupdf/pdf/page.h"
-#include "mupdf/pdf/resource.h"
-#include "mupdf/pdf/cmap.h"
-#include "mupdf/pdf/font.h"
-#include "mupdf/pdf/interpret.h"
+enum pdf_zugferd_profile
+{
+	PDF_NOT_ZUGFERD = 0,
+	/* ZUGFeRD 1.0 */
+	PDF_ZUGFERD_COMFORT,
+	PDF_ZUGFERD_BASIC,
+	PDF_ZUGFERD_EXTENDED,
+	/* ZUGFeRD 2.01 */
+	PDF_ZUGFERD_BASIC_WL,
+	PDF_ZUGFERD_MINIMUM,
+	/* ZUGFeRD 2.2 */
+	PDF_ZUGFERD_XRECHNUNG,
+	PDF_ZUGFERD_UNKNOWN
+};
 
-#include "mupdf/pdf/annot.h"
-#include "mupdf/pdf/form.h"
-#include "mupdf/pdf/event.h"
-#include "mupdf/pdf/javascript.h"
+enum pdf_zugferd_profile pdf_zugferd_profile(fz_context *ctx, pdf_document *doc, float *version);
 
-#include "mupdf/pdf/clean.h"
-#include "mupdf/pdf/image-rewriter.h"
-#include "mupdf/pdf/zugferd.h"
+fz_buffer *pdf_zugferd_xml(fz_context *ctx, pdf_document *doc);
 
-
-#ifdef __cplusplus
-}
-#endif
+const char *pdf_zugferd_profile_to_string(fz_context *ctx, enum pdf_zugferd_profile profile);
 
 #endif

platform/java/jni/pdfdocument.c
@@ -1839,3 +1839,50 @@ FUN(PDFDocument_associatedFile)(JNIEnv *env, jobject self, jint idx)
 
 	return to_PDFObject_safe_own(ctx, env, af);
 }
+
+JNIEXPORT jint JNICALL
+FUN(PDFDocument_zugferdProfile)(JNIEnv *env, jobject self)
+{
+	fz_context *ctx = get_context(env);
+	pdf_document *doc = from_PDFDocument(env, self);
+	enum pdf_zugferd_profile p;
+	float version;
+
+	fz_try(ctx)
+		p = pdf_zugferd_profile(ctx, doc, &version);
+	fz_catch(ctx)
+		jni_rethrow(env, ctx);
+
+	return (int)p;
+}
+
+JNIEXPORT jfloat JNICALL
+FUN(PDFDocument_zugferdVersion)(JNIEnv *env, jobject self)
+{
+	fz_context *ctx = get_context(env);
+	pdf_document *doc = from_PDFDocument(env, self);
+	enum pdf_zugferd_profile p;
+	float version;
+
+	fz_try(ctx)
+		p = pdf_zugferd_profile(ctx, doc, &version);
+	fz_catch(ctx)
+		jni_rethrow(env, ctx);
+
+	return (jfloat)version;
+}
+
+JNIEXPORT jobject JNICALL
+FUN(PDFDocument_zugferdXML)(JNIEnv *env, jobject self)
+{
+	fz_context *ctx = get_context(env);
+	pdf_document *doc = from_PDFDocument(env, self);
+	fz_buffer *buf;
+
+	fz_try(ctx)
+		buf = pdf_zugferd_xml(ctx, doc);
+	fz_catch(ctx)
+		jni_rethrow(env, ctx);
+
+	return to_Buffer_safe_own(ctx, env, buf);
+}

platform/java/mupdf_native.c
@@ -591,6 +591,14 @@ static int check_enums()
 	valid &= com_artifex_mupdf_fitz_PDFDocument_LANGUAGE_zh == FZ_LANG_zh;
 	valid &= com_artifex_mupdf_fitz_PDFDocument_LANGUAGE_zh_Hans == FZ_LANG_zh_Hans;
 	valid &= com_artifex_mupdf_fitz_PDFDocument_LANGUAGE_zh_Hant == FZ_LANG_zh_Hant;
+	valid &= com_artifex_mupdf_fitz_PDFDocument_NOT_ZUGFERD == PDF_NOT_ZUGFERD;
+	valid &= com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_COMFORT == PDF_ZUGFERD_COMFORT;
+	valid &= com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC == PDF_ZUGFERD_BASIC;
+	valid &= com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_EXTENDED == PDF_ZUGFERD_EXTENDED;
+	valid &= com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC_WL == PDF_ZUGFERD_BASIC_WL;
+	valid &= com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_MINIMUM == PDF_ZUGFERD_MINIMUM;
+	valid &= com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_XRECHNUNG == PDF_ZUGFERD_XRECHNUNG;
+	valid &= com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_UNKNOWN == PDF_ZUGFERD_UNKNOWN;
 
 	valid &= com_artifex_mupdf_fitz_LinkDestination_LINK_DEST_FIT == FZ_LINK_DEST_FIT;
 	valid &= com_artifex_mupdf_fitz_LinkDestination_LINK_DEST_FIT_B == FZ_LINK_DEST_FIT_B;

platform/win32/libmupdf.vcxproj
@@ -678,6 +678,7 @@
     <ClCompile Include="..\..\source\pdf\pdf-write.c" />
     <ClCompile Include="..\..\source\pdf\pdf-xobject.c" />
     <ClCompile Include="..\..\source\pdf\pdf-xref.c" />
+    <ClCompile Include="..\..\source\pdf\pdf-zugferd.c" />
     <ClCompile Include="..\..\source\reflow\reflow-doc.c" />
     <ClCompile Include="..\..\source\svg\svg-color.c" />
     <ClCompile Include="..\..\source\svg\svg-doc.c" />
@@ -770,6 +771,7 @@
     <ClInclude Include="..\..\include\mupdf\pdf\parse.h" />
     <ClInclude Include="..\..\include\mupdf\pdf\resource.h" />
     <ClInclude Include="..\..\include\mupdf\pdf\xref.h" />
+    <ClInclude Include="..\..\include\mupdf\pdf\zugferd.h" />
     <ClInclude Include="..\..\include\mupdf\ucdn.h" />
     <ClInclude Include="..\..\source\fitz\bidi-imp.h" />
     <ClInclude Include="..\..\source\fitz\color-imp.h" />

platform/win32/libmupdf.vcxproj.filters
@@ -681,6 +681,9 @@
     <ClCompile Include="..\..\source\pdf\pdf-af.c">
       <Filter>pdf</Filter>
     </ClCompile>
+    <ClCompile Include="..\..\source\pdf\pdf-zugferd.c">
+      <Filter>pdf</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="..\..\source\fitz\draw-imp.h">
@@ -961,6 +964,9 @@
     </ClInclude>
     <ClInclude Include="..\..\source\fitz\deskew_sse.h">
       <Filter>fitz</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\include\mupdf\pdf\zugferd.h">
+      <Filter>!include\pdf</Filter>
     </ClInclude>
   </ItemGroup>
 </Project>

source/pdf/pdf-zugferd.c
@@ -1,0 +1,268 @@
+// Copyright (C) 2024 Artifex Software, Inc.
+//
+// This file is part of MuPDF.
+//
+// MuPDF is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// MuPDF is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with MuPDF. If not, see <https://www.gnu.org/licenses/agpl-3.0.en.html>
+//
+// Alternative licensing terms are available from the licensor.
+// For commercial licensing, see <https://www.artifex.com/> or contact
+// Artifex Software, Inc., 39 Mesa Street, Suite 108A, San Francisco,
+// CA 94129, USA, for further information.
+
+#include "mupdf/fitz.h"
+#include "mupdf/pdf.h"
+
+static int
+version100(const char *v)
+{
+	float f = fz_atof(v);
+
+	return (int)(100*f + 0.5f);
+}
+
+static enum pdf_zugferd_profile
+do_zugferd_profile(fz_context *ctx, pdf_document *doc, float *version, char **fname)
+{
+	pdf_obj *metadata = pdf_dict_getl(ctx, pdf_trailer(ctx, doc), PDF_NAME(Root), PDF_NAME(Metadata), NULL);
+	fz_buffer *buf;
+	fz_xml *xml = NULL;
+	fz_xml *x;
+	enum pdf_zugferd_profile ret = PDF_NOT_ZUGFERD;
+
+	if (version)
+		*version = 0;
+	if (fname)
+		*fname = NULL;
+
+	if (metadata == NULL)
+		return PDF_NOT_ZUGFERD;
+
+	buf = pdf_load_raw_stream_number(ctx, doc, pdf_to_num(ctx, metadata));
+
+	fz_var(xml);
+
+	fz_try(ctx)
+	{
+		xml = fz_parse_xml(ctx, buf, 0);
+
+		/* Version 1. */
+		x = fz_xml_find_dfs(xml, "Description", "xmlns:zf", "urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#");
+		if (x)
+		{
+			while (x)
+			{
+				/* The Version tag in the document appears to always be 1.0 */
+				const char *v = fz_xml_att(x, "zf:Version");
+				const char *cl = fz_xml_att(x, "zf:ConformanceLevel");
+				const char *df = fz_xml_att(x, "zf:DocumentFileName");
+				const char *dt = fz_xml_att(x, "zf:DocumentType");
+				if (v && dt && !strcmp(dt, "INVOICE"))
+				{
+					if (!cl)
+						fz_warn(ctx, "No conformance level specified");
+					else if (!strcmp(cl, "COMFORT"))
+						ret = PDF_ZUGFERD_COMFORT;
+					else if (!strcmp(cl, "BASIC"))
+						ret = PDF_ZUGFERD_BASIC;
+					else if (!strcmp(cl, "EXTENDED"))
+						ret = PDF_ZUGFERD_EXTENDED;
+
+					if (version100(v) != 100)
+						fz_warn(ctx, "Unexpected version");
+					*version = 1.0;
+
+					if (!df)
+						fz_warn(ctx, "ZUGFeRD doc is missing filename");
+					else if (strcmp(df, "ZUGFeRD-invoice.xml"))
+						fz_warn(ctx, "ZUGFeRD doc has non-standard filename");
+					if (fname)
+						*fname = fz_strdup(ctx, df); /* Nothing can throw after this */
+					break;
+				}
+
+				x = fz_xml_find_next_dfs(x, "Description", "xmlns:zf", "urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#");
+			}
+			break;
+		}
+
+		/* Version 2. */
+		x = fz_xml_find_dfs(xml, "Description", "xmlns:fx", "urn:zugferd:pdfa:CrossIndustryDocument:invoice:2p0#");
+		if (x)
+		{
+			while (x)
+			{
+				const char *v = fz_xml_att(x, "fx:Version");
+				const char *cl = fz_xml_att(x, "fx:ConformanceLevel");
+				const char *df = fz_xml_att(x, "fx:DocumentFileName");
+				const char *dt = fz_xml_att(x, "fx:DocumentType");
+				if (v && dt && !strcmp(dt, "INVOICE"))
+				{
+					if (!cl)
+						fz_warn(ctx, "No conformance level specified");
+					else if (!strcmp(cl, "EN 16931"))
+						ret = PDF_ZUGFERD_COMFORT;
+					else if (!strcmp(cl, "BASIC"))
+						ret = PDF_ZUGFERD_BASIC;
+					else if (!strcmp(cl, "EXTENDED"))
+						ret = PDF_ZUGFERD_EXTENDED;
+					else if (!strcmp(cl, "BASIC WL"))
+						ret = PDF_ZUGFERD_BASIC_WL;
+					else if (!strcmp(cl, "MINIMUM"))
+						ret = PDF_ZUGFERD_MINIMUM;
+
+					if (version100(v) != 100)
+						fz_warn(ctx, "Unexpected version");
+					*version = 2.0f;
+
+					if (!df)
+						fz_warn(ctx, "ZUGFeRD doc is missing filename");
+					else if (strcmp(df, "zugferd-invoice.xml"))
+						fz_warn(ctx, "ZUGFeRD doc has non-standard filename");
+					if (fname)
+						*fname = fz_strdup(ctx, df); /* Nothing can throw after this */
+					break;
+				}
+
+				x = fz_xml_find_next_dfs(x, "Description", "xmlns:fx", "urn:zugferd:pdfa:CrossIndustryDocument:invoice:2p0#");
+			}
+			break;
+		}
+
+		/* Version 2.1 + 2.11 */
+		x = fz_xml_find_dfs(xml, "Description", "xmlns:fx", "urn:factur-x:pdfa:CrossIndustryDocument:invoice:1p0#");
+		if (x)
+		{
+			while (x)
+			{
+				const char *v = fz_xml_text(fz_xml_down(fz_xml_find_down(x, "Version")));
+				const char *cl = fz_xml_text(fz_xml_down(fz_xml_find_down(x, "ConformanceLevel")));
+				const char *df = fz_xml_text(fz_xml_down(fz_xml_find_down(x, "DocumentFileName")));
+				const char *dt = fz_xml_text(fz_xml_down(fz_xml_find_down(x, "DocumentType")));
+				if (v && dt && !strcmp(dt, "INVOICE"))
+				{
+					if (!cl)
+						fz_warn(ctx, "No conformance level specified");
+					else if (!strcmp(cl, "EN 16931"))
+						ret = PDF_ZUGFERD_COMFORT;
+					else if (!strcmp(cl, "BASIC"))
+						ret = PDF_ZUGFERD_BASIC;
+					else if (!strcmp(cl, "EXTENDED"))
+						ret = PDF_ZUGFERD_EXTENDED;
+					else if (!strcmp(cl, "BASIC WL"))
+						ret = PDF_ZUGFERD_BASIC_WL;
+					else if (!strcmp(cl, "MINIMUM"))
+						ret = PDF_ZUGFERD_MINIMUM;
+					else if (!strcmp(cl, "XRECHNUNG"))
+						ret = PDF_ZUGFERD_XRECHNUNG;
+
+					if (ret == PDF_ZUGFERD_XRECHNUNG && version100(v) != 210)
+						fz_warn(ctx, "Unexpected version");
+					else if (ret != PDF_ZUGFERD_XRECHNUNG && version100(v) != 100)
+						fz_warn(ctx, "Unexpected version");
+					*version = 2.1f;
+
+					if (!df)
+						fz_warn(ctx, "ZUGFeRD doc is missing filename");
+					else if (ret == PDF_ZUGFERD_XRECHNUNG && strcmp(df, "xrechnung.xml"))
+						fz_warn(ctx, "ZUGFeRD doc has non-standard filename");
+					else if (ret != PDF_ZUGFERD_XRECHNUNG && strcmp(df, "factur-x.xml"))
+						fz_warn(ctx, "ZUGFeRD doc has non-standard filename");
+					if (fname)
+						*fname = fz_strdup(ctx, df); /* Nothing can throw after this */
+					break;
+				}
+
+				x = fz_xml_find_next_dfs(x, "Description", "xmlns:fx", "urn:factur-x:pdfa:CrossIndustryDocument:invoice:1p0#");
+			}
+			break;
+		}
+	}
+	fz_always(ctx)
+	{
+		fz_drop_xml(ctx, xml);
+		fz_drop_buffer(ctx, buf);
+	}
+	fz_catch(ctx)
+		fz_rethrow(ctx);
+
+	return ret;
+}
+
+enum pdf_zugferd_profile pdf_zugferd_profile(fz_context *ctx, pdf_document *doc, float *version)
+{
+	return do_zugferd_profile(ctx, doc, version, NULL);
+}
+
+fz_buffer *pdf_zugferd_xml(fz_context *ctx, pdf_document *doc)
+{
+	char *fname;
+	float version;
+	enum pdf_zugferd_profile p = do_zugferd_profile(ctx, doc, &version, &fname);
+	int count, i;
+	fz_buffer *buf = NULL;
+
+	if (p == PDF_NOT_ZUGFERD)
+	{
+		fz_free(ctx, fname);
+		return NULL;
+	}
+
+	fz_try(ctx)
+	{
+		count = pdf_count_document_associated_files(ctx, doc);
+		for (i = 0; i < count; i++)
+		{
+			pdf_obj *fs = pdf_document_associated_file(ctx, doc, i);
+			pdf_filespec_params params;
+
+			pdf_get_filespec_params(ctx, fs, &params);
+
+			if (!strcmp(fname, params.filename))
+			{
+				if (!pdf_is_embedded_file(ctx, fs))
+					fz_throw(ctx, FZ_ERROR_FORMAT, "ZUGFeRD XML was not embedded");
+
+				buf = pdf_load_embedded_file_contents(ctx, fs);
+				break;
+			}
+		}
+	}
+	fz_always(ctx)
+		fz_free(ctx, fname);
+	fz_catch(ctx)
+		fz_rethrow(ctx);
+
+	return buf;
+}
+
+const char *
+pdf_zugferd_profile_to_string(fz_context *ctx, enum pdf_zugferd_profile profile)
+{
+	static const char *strings[] =
+	{
+		"NOT ZUGFERD",
+		"COMFORT",
+		"BASIC",
+		"EXTENDED",
+		"BASIC WL",
+		"MINIMUM",
+		"XRECHNUNG",
+		"UNKNOWN"
+	};
+
+	if (profile < PDF_NOT_ZUGFERD || profile > PDF_ZUGFERD_UNKNOWN)
+		profile = PDF_ZUGFERD_UNKNOWN;
+
+	return strings[profile];
+}

commit abb0068e9d5b30b571f3663665fcd88ee5b9e22c

java: Set correct ZUGFeRD constant values.
Commit 8a2d046f46e1467d442aa1cc01c4a80036ef549e introduced the constants,
but accidentally at the wrong values, and while there is a run-time check,
this was apparently not run until now.

platform/java/mupdf_native.h
@@ -3694,19 +3694,19 @@ extern "C" {
 #undef com_artifex_mupdf_fitz_PDFDocument_NOT_ZUGFERD
 #define com_artifex_mupdf_fitz_PDFDocument_NOT_ZUGFERD 0L
 #undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_COMFORT
-#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_COMFORT 0L
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_COMFORT 1L
 #undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC
-#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC 1L
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC 2L
 #undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_EXTENDED
-#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_EXTENDED 2L
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_EXTENDED 3L
 #undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC_WL
-#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC_WL 3L
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_BASIC_WL 4L
 #undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_MINIMUM
-#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_MINIMUM 4L
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_MINIMUM 5L
 #undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_XRECHNUNG
-#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_XRECHNUNG 5L
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_XRECHNUNG 6L
 #undef com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_UNKNOWN
-#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_UNKNOWN 6L
+#define com_artifex_mupdf_fitz_PDFDocument_ZUGFERD_UNKNOWN 7L
 /*
  * Class:     com_artifex_mupdf_fitz_PDFDocument
  * Method:    newNative

platform/java/src/com/artifex/mupdf/fitz/PDFDocument.java
@@ -284,13 +284,13 @@ public class PDFDocument extends Document
 	public native PDFObject associatedFile(int idx);
 
 	public static final int NOT_ZUGFERD = 0;
-	public static final int ZUGFERD_COMFORT = 0;
-	public static final int ZUGFERD_BASIC = 1;
-	public static final int ZUGFERD_EXTENDED = 2;
-	public static final int ZUGFERD_BASIC_WL = 3;
-	public static final int ZUGFERD_MINIMUM = 4;
-	public static final int ZUGFERD_XRECHNUNG = 5;
-	public static final int ZUGFERD_UNKNOWN = 6;
+	public static final int ZUGFERD_COMFORT = 1;
+	public static final int ZUGFERD_BASIC = 2;
+	public static final int ZUGFERD_EXTENDED = 3;
+	public static final int ZUGFERD_BASIC_WL = 4;
+	public static final int ZUGFERD_MINIMUM = 5;
+	public static final int ZUGFERD_XRECHNUNG = 6;
+	public static final int ZUGFERD_UNKNOWN = 7;
 
 	public native int zugferdProfile();
 	public native float zugferdVersion();