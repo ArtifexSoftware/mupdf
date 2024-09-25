40d3521442717ab5a93e510281a2dbd4df171c3c
Tor Andersson <tor.andersson@artifex.com>
2024-09-25 19:13:30 +0200

    Fix error code usage.

--- source/pdf/pdf-link.c
+++ source/pdf/pdf-link.c
@@ -568,7 +568,7 @@ static void pdf_set_link_rect(fz_context *ctx, fz_link *link_, fz_rect rect)
 		return;
 
 	if (!link->page)
-		fz_throw(ctx, FZ_ERROR_GENERIC, "link not bound to a page");
+		fz_throw(ctx, FZ_ERROR_ARGUMENT, "link not bound to a page");
 
 	pdf_begin_operation(ctx, link->page->doc, "Set link rectangle");
 
@@ -592,7 +592,7 @@ static void pdf_set_link_uri(fz_context *ctx, fz_link *link_, const char *uri)
 		return;
 
 	if (!link->page)
-		fz_throw(ctx, FZ_ERROR_GENERIC, "link not bound to a page");
+		fz_throw(ctx, FZ_ERROR_ARGUMENT, "link not bound to a page");
 
 	pdf_begin_operation(ctx, link->page->doc, "Set link uri");
 
