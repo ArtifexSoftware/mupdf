9662887505d021bb6a1067b88cea1b9c598fe98d
Robin Watts <Robin.Watts@artifex.com>
2025-01-06 18:43:52 +0000

    Tweak vector handling in page segmentation.
    Allow a 1 point margin around vectors when considering usage for
    page segmentation.
    
    Without this we can occasionally see hairline gaps between adjacent
    table columns due to the vagaries of floating point.

--- source/fitz/stext-boxer.c
+++ source/fitz/stext-boxer.c
@@ -884,7 +884,17 @@ int fz_segment_stext_page(fz_context *ctx, fz_stext_page *page)
 						feed_line(ctx, boxer, line);
 				break;
 			case FZ_STEXT_BLOCK_VECTOR:
-				boxer_feed(ctx, boxer, &block->bbox);
+			{
+				/* Allow a 1 point margin around vectors to avoid hairline
+				 * cracks between supposedly abutting things. */
+				int VECTOR_MARGIN = 1;
+				fz_rect r = block->bbox;
+				r.x0 -= VECTOR_MARGIN;
+				r.y0 -= VECTOR_MARGIN;
+				r.x1 += VECTOR_MARGIN;
+				r.y1 += VECTOR_MARGIN;
+				boxer_feed(ctx, boxer, &r);
+			}
 			}
 		}
 
