123f7ccb53939f037bba2bdc93b2e303b08b8934
Sebastian Rasmussen <sebras@gmail.com>
2025-02-28 10:30:32 +0100

    Bug 708304: Handle PDF objects numbered outside xref range.
    The PDF audit tool would trigger an ASAN warning when encountering a
    PDF object outside of the xref range, since the audit tool allocates
    internal state based of the number of objects specified by the xref.
    Later on when encountering an object that resides outside of that
    range, the audit tool did no bounds checking.
    
    The fix is to add bounds checking to avoid accessing outside the
    internal state.

--- source/tools/pdfaudit.c
+++ source/tools/pdfaudit.c
@@ -1691,7 +1691,7 @@ typedef struct
 } walk_stack_t;
 
 static void
-walk(fz_context *ctx, walk_stack_t *ws, obj_info_t *oi, pdf_obj *obj, audit_type_t type)
+walk(fz_context *ctx, walk_stack_t *ws, int n, obj_info_t *oi, pdf_obj *obj, audit_type_t type)
 {
 	int num = 0;
 	do
@@ -1699,6 +1699,8 @@ walk(fz_context *ctx, walk_stack_t *ws, obj_info_t *oi, pdf_obj *obj, audit_type
 		if (pdf_is_indirect(ctx, obj))
 		{
 			num = pdf_to_num(ctx, obj);
+			if (num < 0 || num >= n)
+				fz_throw(ctx, FZ_ERROR_GENERIC, "object outside of xref range");
 			if (oi[num].type != AUDIT_UNKNOWN)
 				goto visited;
 			if (pdf_mark_obj(ctx, obj))
@@ -1850,12 +1852,12 @@ visited:
 }
 
 static void
-classify_by_walking(fz_context *ctx, pdf_document *doc, obj_info_t *oi)
+classify_by_walking(fz_context *ctx, pdf_document *doc, int n, obj_info_t *oi)
 {
 	walk_stack_t ws = { 0 };
 
 	fz_try(ctx)
-		walk(ctx, &ws, oi, pdf_trailer(ctx, doc), AUDIT_TRAILER);
+		walk(ctx, &ws, n, oi, pdf_trailer(ctx, doc), AUDIT_TRAILER);
 	fz_always(ctx)
 		fz_free(ctx, ws.stack);
 	fz_catch(ctx)
@@ -1947,7 +1949,7 @@ filter_file(fz_context *ctx, fz_output *out, const char *filename)
 		}
 
 		/* Walk the doc structure. */
-		classify_by_walking(ctx, pdf, oi);
+		classify_by_walking(ctx, pdf, n, oi);
 
 		/* Filter the content streams to establish operator usage */
 		filter_page_streams(ctx, pdf, &ou);
