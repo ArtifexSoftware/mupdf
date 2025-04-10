ArtifexSoftware/mupdf

commit 5576bba26c0fe38243b25365c6b0faf0638114b5

Don't stack allocate stroke states.

source/html/html-layout.c
@@ -659,11 +659,11 @@ static void layout_flow(fz_context *ctx, layout_data *ld, fz_html_box *box, fz_h
 
 #ifdef DEBUG_LAYOUT_RESTARTING
 		/* Output us some crufty debug */
-		if(restart->start_flow == NULL)
+		if (restart->start_flow == NULL)
 			printf("<restart>");
 		for (node = box->u.flow.head; node; node = node->next)
 		{
-			if(restart->start_flow == node)
+			if (restart->start_flow == node)
 				printf("<restart>");
 			switch (node->type)
 			{
@@ -1846,10 +1846,12 @@ static int draw_flow_box(fz_context *ctx, fz_html_box *box, float page_top, floa
 	int stroking, prev_stroking = 0;
 	int restartable_ended = 0;
 	fz_stroke_state *ss = NULL;
+	fz_stroke_state *line_ss = NULL;
 
 	fz_var(text);
 	fz_var(line);
 	fz_var(ss);
+	fz_var(line_ss);
 
 	/* FIXME: HB_DIRECTION_TTB? */
 
@@ -1911,8 +1913,13 @@ static int draw_flow_box(fz_context *ctx, fz_html_box *box, float page_top, floa
 				stroking = style->text_stroke_color.a != 0;
 				if (stroking)
 				{
-					if(ss == NULL)
+					if (ss == NULL)
 						ss = fz_new_stroke_state(ctx);
+				}
+				if (line)
+				{
+					if (line_ss == NULL)
+						line_ss = fz_new_stroke_state(ctx);
 				}
 
 				if (	/* If we've changed whether we're filling... */
@@ -1952,7 +1959,7 @@ static int draw_flow_box(fz_context *ctx, fz_html_box *box, float page_top, floa
 					if (line)
 					{
 						unpacked_color color = unpack_color(prev_color);
-						fz_stroke_path(ctx, dev, line, &fz_default_stroke_state, ctm, fz_device_rgb(ctx), color.rgb, color.a, fz_default_color_params);
+						fz_stroke_path(ctx, dev, line, line_ss, ctm, fz_device_rgb(ctx), color.rgb, color.a, fz_default_color_params);
 						fz_drop_path(ctx, line);
 						line = NULL;
 					}
@@ -2098,7 +2105,7 @@ static int draw_flow_box(fz_context *ctx, fz_html_box *box, float page_top, floa
 		if (line)
 		{
 			unpacked_color color = unpack_color(prev_color);
-			fz_stroke_path(ctx, dev, line, &fz_default_stroke_state, ctm, fz_device_rgb(ctx), color.rgb, color.a, fz_default_color_params);
+			fz_stroke_path(ctx, dev, line, line_ss, ctm, fz_device_rgb(ctx), color.rgb, color.a, fz_default_color_params);
 			fz_drop_path(ctx, line);
 			line = NULL;
 		}
@@ -2108,6 +2115,7 @@ static int draw_flow_box(fz_context *ctx, fz_html_box *box, float page_top, floa
 		fz_drop_text(ctx, text);
 		fz_drop_path(ctx, line);
 		fz_drop_stroke_state(ctx, ss);
+		fz_drop_stroke_state(ctx, line_ss);
 	}
 	fz_catch(ctx)
 		fz_rethrow(ctx);

source/svg/svg-run.c
@@ -36,7 +36,7 @@
 typedef struct svg_state
 {
 	fz_matrix transform;
-	fz_stroke_state stroke;
+	fz_stroke_state *stroke;
 	int use_depth;
 
 	float viewport_w, viewport_h;
@@ -74,7 +74,7 @@ static void svg_stroke(fz_context *ctx, fz_device *dev, svg_document *doc, fz_pa
 {
 	float opacity = state->opacity * state->stroke_opacity;
 	if (path)
-		fz_stroke_path(ctx, dev, path, &state->stroke, state->transform, fz_device_rgb(ctx), state->stroke_color, opacity, fz_default_color_params);
+		fz_stroke_path(ctx, dev, path, state->stroke, state->transform, fz_device_rgb(ctx), state->stroke_color, opacity, fz_default_color_params);
 }
 
 static void svg_draw_path(fz_context *ctx, fz_device *dev, svg_document *doc, fz_path *path, svg_state *state)
@@ -954,7 +954,7 @@ static const char *linejoin_table[] = { "miter", "round", "bevel" };
 static void
 svg_parse_common(fz_context *ctx, svg_document *doc, fz_xml *node, svg_state *state)
 {
-	fz_stroke_state *stroke = &state->stroke;
+	fz_stroke_state *stroke = state->stroke;
 
 	char *transform_att = fz_xml_att(node, "transform");
 
@@ -1059,7 +1059,7 @@ svg_parse_common(fz_context *ctx, svg_document *doc, fz_xml *node, svg_state *st
 	}
 	else
 	{
-		stroke->linewidth = svg_parse_number_from_style(ctx, doc, style_att, "stroke-width", state->stroke.linewidth);
+		stroke->linewidth = svg_parse_number_from_style(ctx, doc, style_att, "stroke-width", state->stroke->linewidth);
 	}
 
 	if (stroke_linecap_att)
@@ -1104,7 +1104,7 @@ svg_parse_common(fz_context *ctx, svg_document *doc, fz_xml *node, svg_state *st
 	}
 	else
 	{
-		stroke->miterlimit = svg_parse_number_from_style(ctx, doc, style_att, "stroke-miterlimit", state->stroke.miterlimit);
+		stroke->miterlimit = svg_parse_number_from_style(ctx, doc, style_att, "stroke-miterlimit", state->stroke->miterlimit);
 	}
 }
 
@@ -1463,7 +1463,7 @@ svg_run_text_string(fz_context *ctx, fz_device *dev, fz_matrix trm, const char *
 				fz_default_color_params);
 		if (state->stroke_is_set)
 			fz_stroke_text(ctx, dev, text,
-				&state->stroke,
+				state->stroke,
 				state->transform,
 				fz_device_rgb(ctx), state->stroke_color,
 				state->opacity,
@@ -1656,7 +1656,7 @@ svg_run_document(fz_context *ctx, svg_document *doc, fz_xml *root, fz_device *de
 
 	/* Initial graphics state */
 	state.transform = ctm;
-	state.stroke = fz_default_stroke_state;
+	state.stroke = fz_new_stroke_state(ctx);
 	state.use_depth = 0;
 
 	state.viewport_w = DEF_WIDTH;
@@ -1689,5 +1689,16 @@ svg_run_document(fz_context *ctx, svg_document *doc, fz_xml *root, fz_device *de
 	state.is_italic = 0;
 	state.text_anchor = 0;
 
-	svg_run_svg(ctx, dev, doc, root, &state);
+	fz_try(ctx)
+	{
+		svg_run_svg(ctx, dev, doc, root, &state);
+	}
+	fz_always(ctx)
+	{
+		fz_drop_stroke_state(ctx, state.stroke);
+	}
+	fz_catch(ctx)
+	{
+		fz_rethrow(ctx);
+	}
 }

commit 281860659a0981d2b6d03a1383632f1ca9156d67

Bug 708442: Avoid crash due to unallocated stroke state.
Commit 5576bba26c0fe38243b25365c6b0faf0638114b5 accidentally omitted
allocating a new stroke state when a new line is allocated. ASAN
detects the NULL dereference that causes a crash.

The fix is to ensure that the stoke state is allocated when a new
line is allocated. This is reasonable since the original code just
depended on fz_default_stroke_state. After this fix ASAN no longer
complains.

source/html/html-layout.c
@@ -1969,7 +1969,11 @@ static int draw_flow_box(fz_context *ctx, fz_html_box *box, float page_top, floa
 				if (style->text_decoration > 0)
 				{
 					if (!line)
+					{
 						line = fz_new_path(ctx);
+						if (line_ss == NULL)
+							line_ss = fz_new_stroke_state(ctx);
+					}
 					if (style->text_decoration & TD_UNDERLINE)
 					{
 						fz_moveto(ctx, line, node->x, node->y + 1.5f - page_top);