0d31ac63a05bda88930a17e1d996bfceb5eaff31
Sebastian Rasmussen <sebras@gmail.com>
2025-04-29 03:17:55 +0200

    Ensure that annotations are processed whens subsetting fonts.
    The font analysis processor lacks a callback for the cm operator.
    Previously pdf_process_annot() would ignore processing the annotation
    because of this. Now pdf_process_annot() does more fine grained
    callback checks and just ignors the cm calls if the given processor
    does not have a callback for the cm operator.
    
    This fixes:
    tests_private/pdf/PDFIA1.7_SUBSET/CATX2579.pdf

--- source/pdf/pdf-interpret.c
+++ source/pdf/pdf-interpret.c
@@ -1189,6 +1189,8 @@ void
 pdf_process_annot(fz_context *ctx, pdf_processor *proc, pdf_annot *annot, fz_cookie *cookie)
 {
 	int flags = pdf_dict_get_int(ctx, annot->obj, PDF_NAME(F));
+	fz_matrix matrix;
+	pdf_obj *ap;
 
 	if (flags & (PDF_ANNOT_IS_INVISIBLE | PDF_ANNOT_IS_HIDDEN) || annot->hidden_editing)
 		return;
@@ -1216,23 +1218,23 @@ pdf_process_annot(fz_context *ctx, pdf_processor *proc, pdf_annot *annot, fz_coo
 	if (pdf_is_ocg_hidden(ctx, annot->page->doc, NULL, proc->usage, pdf_dict_get(ctx, annot->obj, PDF_NAME(OC))))
 		return;
 
-	if (proc->op_q && proc->op_cm && proc->op_Do_form && proc->op_Q)
-	{
-		pdf_obj *ap = pdf_annot_ap(ctx, annot);
-		fz_matrix matrix;
+	ap = pdf_annot_ap(ctx, annot);
 
-		if (!ap)
-			return;
+	if (!ap)
+		return;
 
-		matrix = pdf_annot_transform(ctx, annot);
+	matrix = pdf_annot_transform(ctx, annot);
+	if (proc->op_q)
 		proc->op_q(ctx, proc);
+	if (proc->op_cm)
 		proc->op_cm(ctx, proc,
 			matrix.a, matrix.b,
 			matrix.c, matrix.d,
 			matrix.e, matrix.f);
+	if (proc->op_Do_form)
 		proc->op_Do_form(ctx, proc, NULL, ap);
+	if (proc->op_Q)
 		proc->op_Q(ctx, proc);
-	}
 }
 
 void
