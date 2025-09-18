60e518456b074fe22f0b7dba3d42e5b82a63295f
Tor Andersson <tor.andersson@artifex.com>
2025-09-18 11:50:39 +0200

    Bug 708174: Optimize caching of ICC based colorspaces.
    Cache colorspaces on indirect references before general objects.
    
    Priority:
    1 - top-level colorspace if indirect object
    2 - first element of array if indirect object
    3 - top-level colorspace
    
    For an ICC-based colorspace we will now cache on the ICC stream
    if the array object is not indirect.
    
    Example 1 (we cache on 22):
    
      << /Type/Image /ColorSpace 22 0 R ...
      22 0 obj
        [ /ICCBased 23 0 R ]
      endobj
    
    Example 2 (we cache on 42):
    
      << /Type/Image /ColorSpace [ /ICCBased 42 0 R ] ...

--- source/pdf/pdf-colorspace.c
+++ source/pdf/pdf-colorspace.c
@@ -368,6 +368,7 @@ pdf_load_colorspace_imp(fz_context *ctx, pdf_obj *obj, pdf_cycle_list *cycle_up)
 {
 	fz_colorspace *cs = NULL;
 	pdf_cycle_list cycle;
+	pdf_obj *sub;
 
 	if (pdf_cycle(ctx, &cycle, cycle_up, obj))
 		fz_throw(ctx, FZ_ERROR_SYNTAX, "recursive colorspace");
@@ -418,16 +419,30 @@ pdf_load_colorspace_imp(fz_context *ctx, pdf_obj *obj, pdf_cycle_list *cycle_up)
 
 			else
 			{
-				if ((cs = pdf_find_item(ctx, fz_drop_colorspace_imp, obj)) != NULL)
-					return cs;
+				sub = pdf_array_get(ctx, obj, 1);
+
+				if (pdf_is_indirect(ctx, obj))
+				{
+					if ((cs = pdf_find_item(ctx, fz_drop_colorspace_imp, obj)) != NULL)
+						return cs;
+				}
+				else if (pdf_is_indirect(ctx, sub))
+				{
+					if ((cs = pdf_find_item(ctx, fz_drop_colorspace_imp, sub)) != NULL)
+						return cs;
+				}
+				else
+				{
+					if ((cs = pdf_find_item(ctx, fz_drop_colorspace_imp, obj)) != NULL)
+						return cs;
+				}
 
 				if (pdf_name_eq(ctx, name, PDF_NAME(ICCBased)))
-					cs = load_icc_based(ctx, pdf_array_get(ctx, obj, 1), 1, &cycle);
-
+					cs = load_icc_based(ctx, sub, 1, &cycle);
 				else if (pdf_name_eq(ctx, name, PDF_NAME(CalGray)))
-					cs = pdf_load_cal_gray(ctx, pdf_array_get(ctx, obj, 1));
+					cs = pdf_load_cal_gray(ctx, sub);
 				else if (pdf_name_eq(ctx, name, PDF_NAME(CalRGB)))
-					cs = pdf_load_cal_rgb(ctx, pdf_array_get(ctx, obj, 1));
+					cs = pdf_load_cal_rgb(ctx, sub);
 
 				else if (pdf_name_eq(ctx, name, PDF_NAME(Indexed)))
 					cs = load_indexed(ctx, obj, &cycle);
@@ -442,15 +457,20 @@ pdf_load_colorspace_imp(fz_context *ctx, pdf_obj *obj, pdf_cycle_list *cycle_up)
 
 				else if (pdf_name_eq(ctx, name, PDF_NAME(Pattern)))
 				{
-					pdf_obj *pobj = pdf_array_get(ctx, obj, 1);
-					if (!pobj)
+					if (!sub)
 						return fz_keep_colorspace(ctx, fz_device_gray(ctx));
-					cs = pdf_load_colorspace_imp(ctx, pobj, &cycle);
+					cs = pdf_load_colorspace_imp(ctx, sub, &cycle);
 				}
 				else
 					fz_throw(ctx, FZ_ERROR_SYNTAX, "unknown colorspace %s", pdf_to_name(ctx, name));
 
-				pdf_store_item(ctx, obj, cs, 1000);
+				if (pdf_is_indirect(ctx, obj))
+					pdf_store_item(ctx, obj, cs, 1000);
+				else if (pdf_is_indirect(ctx, sub))
+					pdf_store_item(ctx, sub, cs, 1000);
+				else
+					pdf_store_item(ctx, obj, cs, 1000);
+
 				return cs;
 			}
 		}
