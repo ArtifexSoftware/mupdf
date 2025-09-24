6339ad2249d2048f9d828e09fbf9a2ea3ff6aab3
Sebastian Rasmussen <sebras@gmail.com>
2025-09-24 18:04:04 +0200

    Fix java build due to typo in PDFPage constant name.
    This was accidentally introdued in commit
    28150795fc36f5b7ab901b36862646b6fee72ece.

--- platform/java/src/com/artifex/mupdf/fitz/PDFPage.java
+++ platform/java/src/com/artifex/mupdf/fitz/PDFPage.java
@@ -46,7 +46,7 @@ public class PDFPage extends Page
 
 	public static final int REDACT_TEXT_REMOVE = 0;
 	public static final int REDACT_TEXT_NONE = 1;
-	public static final int REDACT_TEXT_REMOVE_INVISIBILE = 2;
+	public static final int REDACT_TEXT_REMOVE_INVISIBLE = 2;
 
 	public native boolean applyRedactions(boolean blackBoxes, int imageMethod, int lineArt, int text);
 
