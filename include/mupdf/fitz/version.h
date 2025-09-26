1ab161a2939f6168518d32a6bc5f506f7da8ae82
Sebastian Rasmussen <sebras@gmail.com>
2025-09-26 12:29:11 +0200

    Bump patch release.

--- include/mupdf/fitz/version.h
+++ include/mupdf/fitz/version.h
@@ -23,9 +23,9 @@
 #ifndef MUPDF_FITZ_VERSION_H
 #define MUPDF_FITZ_VERSION_H
 #ifndef FZ_VERSION
-#define FZ_VERSION "1.26.10"
+#define FZ_VERSION "1.26.11"
 #define FZ_VERSION_MAJOR 1
 #define FZ_VERSION_MINOR 26
-#define FZ_VERSION_PATCH 10
+#define FZ_VERSION_PATCH 11
 #endif
 #endif
