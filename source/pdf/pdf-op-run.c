a1409d8fe68a88b4dfc7149f4d4327ccb3299cdd
Tor Andersson <tor.andersson@artifex.com>
2025-10-14 16:58:26 +0200

    Bug 708875: Consider d1 operator to define fill/stroke color.
    An "uncolored" Type3 glyph never sets the color, but it's also not to be
    considered as undefined.
    
    This error caused all uncolored Type3 glyphs to be considered uncacheable,
    and rendered them in direct mode instead of as text.

--- source/pdf/pdf-op-run.c
+++ source/pdf/pdf-op-run.c
@@ -2880,6 +2880,7 @@ static void pdf_run_d1(fz_context *ctx, pdf_processor *proc, float wx, float wy,
 {
 	pdf_run_processor *pr = (pdf_run_processor *)proc;
 	pr->dev->flags |= FZ_DEVFLAG_MASK | FZ_DEVFLAG_BBOX_DEFINED;
+	pr->dev->flags &= ~(FZ_DEVFLAG_FILLCOLOR_UNDEFINED | FZ_DEVFLAG_STROKECOLOR_UNDEFINED);
 	pr->dev->d1_rect.x0 = fz_min(llx, urx);
 	pr->dev->d1_rect.y0 = fz_min(lly, ury);
 	pr->dev->d1_rect.x1 = fz_max(llx, urx);
