ArtifexSoftware/mupdf-julian

scripts/wrap/ setup.py: various minor improvements to C++/Python/C# bindings.
scripts/wrap/__main__.py:
    Use separate compilation when building Python/C# shared libraries.  This
    avoids slow compilation phase if only mupdf.so has changed.

scripts/wrap/swig.py:
    _csharp_unicode_prefix(): use a single typemap, As suggested in
    https://github.com/swig/swig/pull/2364#issuecomment-3258918476.

    Simplified IteratorWrap(). We don't need IteratorWrap.__iter__() and
    IteratorWrap.next() methods. Also improved IteratorWrap docs.

scripts/mupdfwrap_test.py:
    Fix to match change to pdf_sanitize_filter_options:text_filter().

    test_filter(): don't disable on Windows. Use of filters has worked fine on
    Windows for a while now.

scripts/wrap/cpp.py:
    Fixed incorrect asserts in generated code.

setup.py:
    Remove macos special case for libclang version. Current libclang works fine.

scripts/wrap/swig.py
@@ -70,22 +70,7 @@ def _csharp_unicode_prefix():
                          outattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
                          directorinattributes="[global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
                          directoroutattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]"
-                         ) string "string"
-
-
-                %typemap(imtype,
-                         inattributes="[global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         outattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         directorinattributes="[global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         directoroutattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]"
-                         ) const string & "string"
-
-                %typemap(imtype,
-                         inattributes="[global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         outattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         directorinattributes="[global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         directoroutattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]"
-                         ) const char* "string"
+                         ) std::string, std::string, const std::string &, const char* string
             }
             ''')
     return text
@@ -1175,28 +1160,46 @@ def build_swig(
 
                     Iterators must have the following methods:
 
-                        __increment__(): move to next item in the container.
+                        __increment__():
+                            Move to next item in the container. Needs Swig
+                            `%rename(__increment__) *::operator++;`.
                         __ref__(): return reference to item in the container.
+                            Automaticalls generated from C++ operator*().
 
                     Must also be able to compare two iterators for equality.
 
+                    If an class supports C++ iteration with begin() and end()
+                    methods, then one can allow Python iteration by adding
+                    a .__iter__() method that returns a instance of this
+                    class. For example:
+
+                    # A container class that supports C++ iteration with
+                    # .begin() and .end() methods.
+                    class Foo:
+                        def begin(): ...
+                        def end(): ...
+                        ...
+
+                    # Make Foo support Python iteration.
+                    Foo.__iter__ = lambda self: IteratorWrap(self)
+
+                    # Usage.
+                    foo = Foo()
+                    for i in foo:
+                        ...
                     """
                     def __init__( self, container):
-                        self.container = container
-                        self.pos = None
+                        self.begin = container.begin()
                         self.end = container.end()
-                    def __iter__( self):
-                        return self
-                    def __next__( self):    # for python2.
+                        self.pos = None
+                    def __next__( self):
                         if self.pos is None:
-                            self.pos = self.container.begin()
+                            self.pos = self.begin
                         else:
                             self.pos.__increment__()
                         if self.pos == self.end:
                             raise StopIteration()
                         return self.pos.__ref__()
-                    def next( self):    # for python3.
-                        return self.__next__()
 
                 # The auto-generated Python class method
                 # {rename.class_('fz_buffer')}.{rename.method('fz_buffer', 'fz_buffer_extract')}() returns (size, data).

ArtifexSoftware/mupdf

scripts/wrap/swig.py
@@ -70,22 +70,7 @@ def _csharp_unicode_prefix():
                          outattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
                          directorinattributes="[global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
                          directoroutattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]"
-                         ) string "string"
-
-
-                %typemap(imtype,
-                         inattributes="[global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         outattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         directorinattributes="[global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         directoroutattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]"
-                         ) const string & "string"
-
-                %typemap(imtype,
-                         inattributes="[global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         outattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         directorinattributes="[global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]",
-                         directoroutattributes="[return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPUTF8Str)]"
-                         ) const char* "string"
+                         ) std::string, const std::string &, const char* string
             }
             ''')
     return text
@@ -1175,28 +1160,46 @@ def build_swig(
 
                     Iterators must have the following methods:
 
-                        __increment__(): move to next item in the container.
+                        __increment__():
+                            Move to next item in the container. Needs Swig
+                            `%rename(__increment__) *::operator++;`.
                         __ref__(): return reference to item in the container.
+                            Automatically generated from C++ operator*().
 
                     Must also be able to compare two iterators for equality.
 
+                    If a class supports C++ iteration with begin() and end()
+                    methods, then one can allow Python iteration by adding
+                    a .__iter__() method that returns a instance of this
+                    class. For example:
+
+                    # A container class that supports C++ iteration with
+                    # .begin() and .end() methods.
+                    class Foo:
+                        def begin(): ...
+                        def end(): ...
+                        ...
+
+                    # Make Foo support Python iteration.
+                    Foo.__iter__ = lambda self: IteratorWrap(self)
+
+                    # Usage.
+                    foo = Foo()
+                    for i in foo:
+                        ...
                     """
                     def __init__( self, container):
-                        self.container = container
-                        self.pos = None
+                        self.begin = container.begin()
                         self.end = container.end()
-                    def __iter__( self):
-                        return self
-                    def __next__( self):    # for python2.
+                        self.pos = None
+                    def __next__( self):
                         if self.pos is None:
-                            self.pos = self.container.begin()
+                            self.pos = self.begin
                         else:
                             self.pos.__increment__()
                         if self.pos == self.end:
                             raise StopIteration()
                         return self.pos.__ref__()
-                    def next( self):    # for python3.
-                        return self.__next__()
 
                 # The auto-generated Python class method
                 # {rename.class_('fz_buffer')}.{rename.method('fz_buffer', 'fz_buffer_extract')}() returns (size, data).