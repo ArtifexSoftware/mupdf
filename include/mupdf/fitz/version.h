66834fe59e969ad281f9880b2a94a9f0dcfa973d
Robin Watts <Robin.Watts@artifex.com>
2026-02-19 19:19:35 +0000

    Bump version number.

--- include/mupdf/fitz/version.h
+++ include/mupdf/fitz/version.h
@@ -23,9 +23,9 @@
 #ifndef MUPDF_FITZ_VERSION_H
 #define MUPDF_FITZ_VERSION_H
 #ifndef FZ_VERSION
-#define FZ_VERSION "1.27.2"
+#define FZ_VERSION "1.27.3"
 #define FZ_VERSION_MAJOR 1
 #define FZ_VERSION_MINOR 27
-#define FZ_VERSION_PATCH 2
+#define FZ_VERSION_PATCH 3
 #endif
 #endif
