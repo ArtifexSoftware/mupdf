ArtifexSoftware/mupdf

commit b0f6c095ea0760065666ee41377e8553674f2be0

New and improved string option handling.
Add fz_options struct and helpers to unify generic key-value
option checking. This new structure tracks unused options, and
options that are flagged as having invalid values, and can print
warnings automatically about mis-spelled options.

Allow more option string dialects:

- handle quotes like CSV in classic parser: key="hello ""world""!",array="1,2,3,4"

- a subset of JSON syntax in option string: {"key":"Hello \"world\"!","array":[1,2,3,4]}

- URL query string syntax: key=Hello%20%22world%22%21&array=1,2,3,4

Bug 701757: Hook up mudraw options for PCL and PWG.

Use the new options handling to handle PCL and PWG options from
the command line.

Thanks to Robin Watts for the first iteration of this change.

source/pdf/pdf-write.c
@@ -1906,94 +1906,122 @@ const char *fz_pdf_write_options_usage =
 	"\tregenerate-id: (default yes) regenerate document id\n"
 	"\n";
 
-pdf_write_options *
-pdf_parse_write_options(fz_context *ctx, pdf_write_options *opts, const char *args)
+void
+pdf_init_write_options(fz_context *ctx, pdf_write_options *opts)
+{
+	memset(opts, 0, sizeof *opts);
+}
+
+static const fz_option_enums compressions[] =
+{
+	{ "brotli", 2 },
+	{ "flate", 1 },
+	{ NULL, -1 }
+};
+
+static const fz_option_enums encryptions[] =
+{
+	{ "none", PDF_ENCRYPT_NONE },
+	{ "keep", PDF_ENCRYPT_KEEP },
+	{ "rc4-40", PDF_ENCRYPT_RC4_40 },
+	{ "rc4-128", PDF_ENCRYPT_RC4_128 },
+	{ "aes-128", PDF_ENCRYPT_AES_128 },
+	{ "aes-256", PDF_ENCRYPT_AES_256 },
+	{ NULL, -1}
+};
+
+static const fz_option_enums garbage_opts[] =
+{
+	{ "compact", 2 },
+	{ "deduplicate", 3 },
+	{ NULL, -1 }
+};
+
+static const fz_option_enums appearance_opts[] =
+{
+	{ "all", 2 },
+	{ NULL, -1 }
+};
+
+void
+pdf_apply_write_options(fz_context *ctx, pdf_write_options *opts, fz_options *args)
 {
 	const char *val;
+	int bv;
 
-	memset(opts, 0, sizeof *opts);
+	fz_lookup_option_boolean(ctx, args, "decompress", &opts->do_decompress);
 
-	if (fz_has_option(ctx, args, "decompress", &val))
-		opts->do_decompress = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "compress", &val))
+	if (fz_lookup_option_boolean(ctx, args, "compress", &opts->do_compress))
+	{}
+	else if (fz_lookup_option_enum(ctx, args, "compress", &opts->do_compress, compressions) < 0)
+		fz_throw(ctx, FZ_ERROR_ARGUMENT, "unknown compression method in options");
+
+	fz_lookup_option_boolean(ctx, args, "compress-fonts", &opts->do_compress_fonts);
+	fz_lookup_option_boolean(ctx, args, "compress-images", &opts->do_compress_images);
+	fz_lookup_option_boolean(ctx, args, "labels", &opts->do_labels);
+	fz_lookup_option_boolean(ctx, args, "ascii", &opts->do_ascii);
+	fz_lookup_option_boolean(ctx, args, "pretty", &opts->do_pretty);
+	fz_lookup_option_boolean(ctx, args, "linearize", &opts->do_linear);
+	fz_lookup_option_boolean(ctx, args, "clean", &opts->do_clean);
+	fz_lookup_option_boolean(ctx, args, "sanitize", &opts->do_sanitize);
+	fz_lookup_option_boolean(ctx, args, "incremental", &opts->do_incremental);
+	fz_lookup_option_boolean(ctx, args, "objstms", &opts->do_use_objstms);
+
+	fz_lookup_option_integer(ctx, args, "compression-effort", &opts->compression_effort);
+
+	if (fz_lookup_option_boolean(ctx, args, "regenerate-id", &bv))
+		opts->dont_regenerate_id = !bv;
+
+	if (fz_lookup_option_boolean(ctx, args, "regenerate-id", &bv))
+		opts->do_encrypt = bv ? PDF_ENCRYPT_NONE : PDF_ENCRYPT_KEEP;
+
+	if (fz_lookup_option_boolean(ctx, args, "encrypt", &opts->do_encrypt))
 	{
-		if (fz_option_eq(val, "brotli"))
-			opts->do_compress = 2;
-		else if (fz_option_eq(val, "flate"))
-			opts->do_compress = 1;
-		else
-			opts->do_compress = fz_option_eq(val, "yes");
-	}
-	if (fz_has_option(ctx, args, "compress-fonts", &val))
-		opts->do_compress_fonts = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "compress-images", &val))
-		opts->do_compress_images = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "compression-effort", &val))
-		opts->compression_effort = fz_atoi(val);
-	if (fz_has_option(ctx, args, "labels", &val))
-		opts->do_labels = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "ascii", &val))
-		opts->do_ascii = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "pretty", &val))
-		opts->do_pretty = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "linearize", &val))
-		opts->do_linear = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "clean", &val))
-		opts->do_clean = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "sanitize", &val))
-		opts->do_sanitize = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "incremental", &val))
-		opts->do_incremental = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "objstms", &val))
-		opts->do_use_objstms = fz_option_eq(val, "yes");
-	if (fz_has_option(ctx, args, "regenerate-id", &val))
-		opts->dont_regenerate_id = fz_option_eq(val, "no");
-	if (fz_has_option(ctx, args, "decrypt", &val))
-		opts->do_encrypt = fz_option_eq(val, "yes") ? PDF_ENCRYPT_NONE : PDF_ENCRYPT_KEEP;
-	if (fz_has_option(ctx, args, "encrypt", &val))
-	{
-		if (fz_option_eq(val, "none") || fz_option_eq(val, "no"))
+		if (opts->do_encrypt == 0)
 			opts->do_encrypt = PDF_ENCRYPT_NONE;
-		else if (fz_option_eq(val, "keep"))
-			opts->do_encrypt = PDF_ENCRYPT_KEEP;
-		else if (fz_option_eq(val, "rc4-40") || fz_option_eq(val, "yes"))
+		else
 			opts->do_encrypt = PDF_ENCRYPT_RC4_40;
-		else if (fz_option_eq(val, "rc4-128"))
-			opts->do_encrypt = PDF_ENCRYPT_RC4_128;
-		else if (fz_option_eq(val, "aes-128"))
-			opts->do_encrypt = PDF_ENCRYPT_AES_128;
-		else if (fz_option_eq(val, "aes-256"))
-			opts->do_encrypt = PDF_ENCRYPT_AES_256;
-		else
-			fz_throw(ctx, FZ_ERROR_ARGUMENT, "unknown encryption in options");
 	}
-	if (fz_has_option(ctx, args, "owner-password", &val))
-		fz_copy_option(ctx, val, opts->opwd_utf8, nelem(opts->opwd_utf8));
-	if (fz_has_option(ctx, args, "user-password", &val))
-		fz_copy_option(ctx, val, opts->upwd_utf8, nelem(opts->upwd_utf8));
-	if (fz_has_option(ctx, args, "permissions", &val))
-		opts->permissions = fz_atoi(val);
-	else
-		opts->permissions = ~0;
-	if (fz_has_option(ctx, args, "garbage", &val))
-	{
-		if (fz_option_eq(val, "yes"))
-			opts->do_garbage = 1;
-		else if (fz_option_eq(val, "compact"))
-			opts->do_garbage = 2;
-		else if (fz_option_eq(val, "deduplicate"))
-			opts->do_garbage = 3;
-		else
-			opts->do_garbage = fz_atoi(val);
-	}
-	if (fz_has_option(ctx, args, "appearance", &val))
-	{
-		if (fz_option_eq(val, "yes"))
-			opts->do_appearance = 1;
-		else if (fz_option_eq(val, "all"))
-			opts->do_appearance = 2;
-	}
+	else if (fz_lookup_option_enum(ctx, args, "encrypt", &opts->do_encrypt, encryptions) < 0)
+		fz_throw(ctx, FZ_ERROR_ARGUMENT, "unknown encryption in options");
 
+	if (fz_lookup_option(ctx, args, "owner-password", &val))
+		fz_strlcpy(opts->opwd_utf8, val, nelem(opts->opwd_utf8));
+	if (fz_lookup_option(ctx, args, "user-password", &val))
+		fz_strlcpy(opts->upwd_utf8, val, nelem(opts->upwd_utf8));
+
+	opts->permissions = ~0;
+	fz_lookup_option_integer(ctx, args, "permissions", &opts->permissions);
+
+	if (fz_lookup_option_boolean(ctx, args, "garbage", &opts->do_garbage))
+	{}
+	else if (fz_lookup_option_integer(ctx, args, "garbage", &opts->do_garbage))
+	{}
+	else if (fz_lookup_option_enum(ctx, args, "garbage", &opts->do_garbage, garbage_opts) < 0)
+		fz_throw(ctx, FZ_ERROR_ARGUMENT, "unknown garbage option in options");
+
+	if (fz_lookup_option_boolean(ctx, args, "appearance", &opts->do_appearance))
+	{}
+	else if (fz_lookup_option_enum(ctx, args, "appearance", &opts->do_appearance, appearance_opts) < 0)
+		fz_throw(ctx, FZ_ERROR_ARGUMENT, "unknown appearance option in options");
+
+	fz_validate_options(ctx, args, "pdf");
+}
+
+pdf_write_options *
+pdf_parse_write_options(fz_context *ctx, pdf_write_options *opts, const char *args_string)
+{
+	fz_options *args = fz_new_options(ctx, args_string);
+	fz_try(ctx)
+	{
+		pdf_init_write_options(ctx, opts);
+		pdf_apply_write_options(ctx, opts, args);
+		fz_throw_on_unused_options(ctx, args, "pdf");
+	}
+	fz_always(ctx)
+		fz_drop_options(ctx, args);
+	fz_catch(ctx)
+		fz_rethrow(ctx);
 	return opts;
 }
 

include/mupdf/fitz.h
@@ -33,6 +33,7 @@ extern "C" {
 #include "mupdf/fitz/context.h"
 #include "mupdf/fitz/output.h"
 #include "mupdf/fitz/log.h"
+#include "mupdf/fitz/options.h"
 
 #include "mupdf/fitz/crypt.h"
 #include "mupdf/fitz/getopt.h"

include/mupdf/fitz/device.h
@@ -30,6 +30,7 @@
 #include "mupdf/fitz/shade.h"
 #include "mupdf/fitz/path.h"
 #include "mupdf/fitz/text.h"
+#include "mupdf/fitz/options.h"
 
 /**
 	The different format handlers (pdf, xps etc) interpret pages to
@@ -633,9 +634,23 @@ typedef struct
 FZ_DATA extern const char *fz_draw_options_usage;
 
 /**
-	Parse draw device options from a comma separated key-value string.
+	Initialise a draw_options struct to sensible values.
 */
-fz_draw_options *fz_parse_draw_options(fz_context *ctx, fz_draw_options *options, const char *string);
+void fz_init_draw_options(fz_context *ctx, fz_draw_options *draw_options);
+
+/**
+	Parse draw device options from a comma separated key-value string.
+
+	This initialises the draw_options struct first.
+*/
+fz_draw_options *fz_parse_draw_options(fz_context *ctx, fz_draw_options *draw_options, const char *string);
+
+/**
+	Parse draw device options from an fz_options structure.
+
+	This assumes that the draw_options struct has been initialised already.
+*/
+void fz_apply_draw_options(fz_context *ctx, fz_draw_options *draw_options, fz_options *options);
 
 /**
 	Create a new pixmap and draw device, using the specified options.

include/mupdf/fitz/options.h
@@ -1,0 +1,213 @@
+// Copyright (C) 2025 Artifex Software, Inc.
+//
+// This file is part of MuPDF.
+//
+// MuPDF is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// MuPDF is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with MuPDF. If not, see <https://www.gnu.org/licenses/agpl-3.0.en.html>
+//
+// Alternative licensing terms are available from the licensor.
+// For commercial licensing, see <https://www.artifex.com/> or contact
+// Artifex Software, Inc., 39 Mesa Street, Suite 108A, San Francisco,
+// CA 94129, USA, for further information.
+
+#ifndef MUPDF_FITZ_OPTIONS_H
+#define MUPDF_FITZ_OPTIONS_H
+
+#include "mupdf/fitz/context.h"
+#include "mupdf/fitz/pool.h"
+
+/**
+	An fz_options structure encapsulates a list of key, or
+	key=value options, together with details such as whether
+	they have been used or not.
+*/
+typedef struct fz_options fz_options;
+
+/**
+	Create an options object, with the initial contents parsed from the string.
+	See fz_parse_options for details on the string parsing.
+	If the string is NULL, the options object will be initialized but empty.
+*/
+fz_options *fz_new_options(fz_context *ctx, const char *option_string);
+
+/**
+	Parse more options from an options string, and add them to
+	an existing fz_options object.
+
+	The parser supports three distinct syntaxes (identified by the leading character).
+
+	- Classic comma separated list of values
+		rotate=90,bbox="0,0,100,100",title="Hello, world!"
+
+	- URL query string
+		?rotate=90&bbox=0,0,100,100&title=Hello, world!
+
+	- A single JSON object -- no nested objects (except arrays of numbers)
+		{"rotate":90,"bbox":[0,0,100,100],"title":"Hello, world!"}
+*/
+void fz_parse_options(fz_context *ctx, fz_options *options, const char *option_string);
+
+/**
+	Take a new reference to the options struct.
+*/
+fz_options *fz_keep_options(fz_context *ctx, fz_options *opts);
+
+/**
+	Drop an fz_options object.
+*/
+void fz_drop_options(fz_context *ctx, fz_options *opts);
+
+/**
+	Check to see if a key is present in the options object.
+
+	If it is not, then return 0.
+
+	If val is non-NULL, *val will be updated to point to the value.
+
+	The option will be recorded as having been accessed.
+*/
+int fz_lookup_option(fz_context *ctx, fz_options *options, const char *key, const char **val);
+
+/**
+	Check to see if a key is a boolean.
+
+	If it is absent, return 0.
+
+	If it is true (empty, 1, yes, true, or enabled), returns 1,
+	clears any invalid flag, and counts as accessed.
+
+	If it is false (0, no, false, or disabled), returns 0,
+	clears any invalid flag, and counts as accessed.
+
+	If it is absent, or any other value, it is marked as invalid, returns 0.
+*/
+int fz_lookup_option_yes(fz_context *ctx, fz_options *options, const char *key);
+
+/**
+	Check to see if a key is present, and is true or false.
+
+	If it is absent, return 0.
+
+	If it is true (empty, 1, yes, true, or enabled), *x is assigned 1,
+	the invalid flag is cleared, marked as accessed, returns 1.
+
+	If it is false (0, no, false, or disabled), *x is assigned 0,
+	the invalid flag is clear, marked as accessed, returns 0.
+
+	If it is any other value, it is marked as invalid, returns -1.
+*/
+int fz_lookup_option_boolean(fz_context *ctx, fz_options *options, const char *key, int *x);
+
+/**
+	Check to see if an option is present and is of the given type.
+
+	If an option is absent, this returns 0.
+
+	If an option is present, but not of the required type, it will be
+	flagged internally as being invalid, and we will return -1.
+
+	If an option is present, and of the required type, any previously
+	set invalid flag will be cleared.
+
+	This means we can (for example) lookup an option as an enum, and then
+	safely look for it being an integer if that fails.
+*/
+int fz_lookup_option_float(fz_context *ctx, fz_options *options, const char *key, float *x);
+int fz_lookup_option_integer(fz_context *ctx, fz_options *options, const char *key, int *x);
+int fz_lookup_option_unsigned(fz_context *ctx, fz_options *options, const char *key, unsigned int *x);
+
+typedef struct
+{
+	const char *key;
+	int val;
+} fz_option_enums;
+
+/*
+	Check to see if an option is present. Look for it in the given fz_option_enums
+	array, which is expected to end with an entry with a NULL key.
+
+	If the option is not found, return 0.
+
+	If the option is found, and a matching entry in the list exists, then *x will
+	be set to the 'val' from that entry, the invalid flag is cleared, and we will return 1.
+
+	If the option is found, and no matching entry in the list exists, then *x will
+	be set to the 'val' from the terminating NULL entry, the invalid flag will be set,
+	and we will return -1.
+*/
+int fz_lookup_option_enum(fz_context *ctx, fz_options *options, const char *key, int *x,
+	const fz_option_enums *enum_list);
+
+/**
+	This should be called by any consumer of options after it has looked up
+	the options it understands. This will throw if any options were found to
+	be flagged as being invalid.
+*/
+void fz_validate_options(fz_context *ctx, fz_options *options, const char *prefix);
+
+/**
+	Warn for any options being unused. Throw if any options are invalid.
+
+	Either this, or fz_throw_on_unused_options should always be called
+	(in non-error cases at least) before dropping options.
+
+	Returns 0 if OK, non-zero otherwise.
+*/
+void fz_warn_on_unused_options(fz_context *ctx, fz_options *options, const char *prefix);
+
+/**
+	Throw for any options being unused or invalid.
+
+	Either this, or fz_warn_on_unused_options should always be called
+	(in non-error cases at least) before dropping options.
+
+	Returns 0 if OK, non-zero otherwise.
+*/
+void fz_throw_on_unused_options(fz_context *ctx, fz_options *options, const char *prefix);
+
+
+/**
+	Count the number of options in an options structure.
+*/
+int fz_count_options(fz_context *ctx, fz_options *options);
+
+/**
+	Get an option by index.
+*/
+const char *fz_get_option_by_index(fz_context *ctx, fz_options *options, int i, const char **val);
+
+/**
+	Mark a given option index as being accessed.
+*/
+void fz_access_option_by_index(fz_context *ctx, fz_options *options, int i);
+
+/**
+	Implementation details: subject to change. Only public for
+	SWIG built wrappers.
+*/
+
+typedef struct fz_option
+{
+	int flags;
+	char *key;
+	char *val;
+	struct fz_option *next;
+} fz_option;
+
+struct fz_options {
+	int refs;
+	fz_pool *pool;
+	fz_option *head;
+};
+
+#endif

include/mupdf/fitz/output-svg.h
@@ -42,7 +42,9 @@ struct fz_svg_device_options {
 	int *id;
 };
 
+void fz_init_svg_device_options(fz_context *ctx, fz_svg_device_options *opts);
 void fz_parse_svg_device_options(fz_context *ctx, fz_svg_device_options *opts, const char *args);
+void fz_apply_svg_device_options(fz_context *ctx, fz_svg_device_options *opts, fz_options *options);
 
 fz_device *fz_new_svg_device_with_options(fz_context *ctx, fz_output *out, float page_width, float page_height, fz_svg_device_options *opts);
 

include/mupdf/fitz/structured-text.h
@@ -711,11 +711,21 @@ typedef struct
 	fz_rect clip;
 } fz_stext_options;
 
+void fz_init_stext_options(fz_context *ctx, fz_stext_options *opts);
+
 /**
 	Parse stext device options from a comma separated key-value
 	string.
+
+	This initialises the opts structure.
 */
 fz_stext_options *fz_parse_stext_options(fz_context *ctx, fz_stext_options *opts, const char *string);
+
+/**
+	Parse stext device options from an fz_options struct
+	into an already initialised opts structure.
+*/
+void fz_apply_stext_options(fz_context *ctx, fz_stext_options *opts, fz_options *options);
 
 /**
 	Perform segmentation analysis on an (unstructured) page to look for
@@ -894,7 +904,7 @@ fz_device *fz_new_ocr_device(fz_context *ctx, fz_device *target, fz_matrix ctm, 
 			const char *datadir, int (*progress)(fz_context *, void *, int), void *progress_arg);
 
 fz_device *fz_new_ocr_device_with_options(fz_context *ctx, fz_device *target, fz_matrix ctm, fz_rect mediabox, int with_list, const char *language,
-			const char *datadir, int (*progress)(fz_context *, void *, int), void *progress_arg, const char *options);
+			const char *datadir, int (*progress)(fz_context *, void *, int), void *progress_arg, fz_options *options);
 
 fz_document *fz_open_reflowed_document(fz_context *ctx, fz_document *underdoc, const fz_stext_options *opts);
 
@@ -928,7 +938,11 @@ typedef enum
 
 FZ_DATA extern const char *fz_search_options_usage;
 
-fz_search_options fz_parse_search_options(const char *options);
+void fz_init_search_options(fz_context *ctx, fz_search_options *options);
+
+fz_search_options *fz_parse_search_options(fz_context *ctx, fz_search_options *options, const char *args);
+
+void fz_apply_search_options(fz_context *ctx, fz_search_options *options, fz_options *opts);
 
 /**
 	Create a new search.

include/mupdf/fitz/write-pixmap.h
@@ -98,7 +98,11 @@ void fz_pcl_preset(fz_context *ctx, fz_pcl_options *opts, const char *preset);
 		is_ljet4pjl	Disable/Enable HP 4PJL model-specific output
 		is_oce9050	Disable/Enable Oce 9050 model-specific output
 */
+void fz_init_pcl_options(fz_context *ctx, fz_pcl_options *opts);
+
 fz_pcl_options *fz_parse_pcl_options(fz_context *ctx, fz_pcl_options *opts, const char *args);
+
+void fz_apply_pcl_options(fz_context *ctx, fz_pcl_options *opts, fz_options *args);
 
 /**
 	Create a new band writer, outputting monochrome pcl.
@@ -151,7 +155,9 @@ typedef struct
 		compression=flate: Flate compression
 		strip-height=n: Strip height (default 16)
 */
+void fz_init_pclm_options(fz_context *ctx, fz_pclm_options *opts);
 fz_pclm_options *fz_parse_pclm_options(fz_context *ctx, fz_pclm_options *opts, const char *args);
+void fz_apply_pclm_options(fz_context *ctx, fz_pclm_options *opts, fz_options *args);
 
 /**
 	Create a new band writer, outputting pclm
@@ -185,7 +191,7 @@ typedef struct
 	/* Updated as we move through the job */
 	int page_count;
 
-	char *options;
+	fz_options *options;
 } fz_pdfocr_options;
 
 /**
@@ -199,7 +205,9 @@ typedef struct
 		ocr-language=<lang>: OCR Language (default eng)
 		ocr-datadir=<datadir>: OCR data path (default rely on TESSDATA_PREFIX)
 */
+void fz_init_pdfocr_options(fz_context *ctx, fz_pdfocr_options *opts);
 fz_pdfocr_options *fz_parse_pdfocr_options(fz_context *ctx, fz_pdfocr_options *opts, const char *args);
+void fz_apply_pdfocr_options(fz_context *ctx, fz_pdfocr_options *opts, fz_options *options);
 
 /**
 	Create a new band writer, outputting pdfocr.
@@ -454,6 +462,21 @@ typedef struct
 	char rendering_intent[64];
 	char page_size_name[64];
 } fz_pwg_options;
+
+/**
+	Init pwg options to a sensible default.
+*/
+void fz_init_pwg_options(fz_context *ctx, fz_pwg_options *opts);
+
+/**
+	Initialise a pwg options struct, and parse the given options string.
+*/
+fz_pwg_options *fz_parse_pwg_options(fz_context *ctx, fz_pwg_options *opts, const char *args);
+
+/**
+	Apply the given options to an initialised pwg options struct.
+*/
+void fz_apply_pwg_options(fz_context *ctx, fz_pwg_options *opts, fz_options *args);
 
 /**
 	Save a pixmap as a PWG.

include/mupdf/fitz/writer.h
@@ -73,31 +73,6 @@ typedef void (fz_document_writer_drop_writer_fn)(fz_context *ctx, fz_document_wr
 	((TYPE *)Memento_label(fz_new_document_writer_of_size(CTX,sizeof(TYPE),BEGIN_PAGE,END_PAGE,CLOSE,DROP),#TYPE))
 
 /**
-	Look for a given option (key) in the opts string. Return 1 if
-	it has it, and update *val to point to the value within opts.
-*/
-int fz_has_option(fz_context *ctx, const char *opts, const char *key, const char **val);
-
-/**
-	Check to see if an option, a, from a string matches a reference
-	option, b.
-
-	(i.e. a could be 'foo' or 'foo,bar...' etc, but b can only be
-	'foo'.)
-*/
-int fz_option_eq(const char *a, const char *b);
-
-/**
-	Copy an option (val) into a destination buffer (dest), of maxlen
-	bytes.
-
-	Returns the number of bytes (including terminator) that did not
-	fit. If val is maxlen or greater bytes in size, it will be left
-	unterminated.
-*/
-size_t fz_copy_option(fz_context *ctx, const char *val, char *dest, size_t maxlen);
-
-/**
 	Create a new fz_document_writer, for a
 	file of the given type.
 

commit c99c85d59542584ec3e01b4f0b19dd088e249350

Deprecate the decrypt write option, but fix the typo.
The decrypt option is deprecated in preference to the encrypt write option.

Commit b0f6c095ea0760065666ee41377e8553674f2be0 accidentally
confused "decrypt" with "regenerate-id", fix that too.

docs/man/mutool.1
@@ -821,12 +821,12 @@ Continue saving the document even if there is an error.
 .IP
 .B decrypt
 .in +4n
-Write unencrypted document.
+This option is deprecated: instead of decrypt use encrypt=none.
 .in
 .IP
 .B encrypt=\fR(\fBnone\fR|\fBkeep\fR|\fBrc4-40\fR|\fBrc4-128\fR|\fBaes-128\fR|\fBaes-256\fR)
 .in +4n
-Write encrypted document using specified encryption method.
+Write encrypted document using specified encryption method. The default value is keep.
 .in
 .IP
 .B permissions=NUMBER

source/pdf/pdf-write.c
@@ -2073,8 +2073,11 @@ pdf_apply_write_options(fz_context *ctx, pdf_write_options *opts, fz_options *ar
 	if (fz_lookup_option_boolean(ctx, args, "regenerate-id", &bv))
 		opts->dont_regenerate_id = !bv;
 
-	if (fz_lookup_option_boolean(ctx, args, "regenerate-id", &bv))
+	if (fz_lookup_option_boolean(ctx, args, "decrypt", &bv))
+	{
+		fz_warn(ctx, "the decrypt write option is deprecated, use encrypt=none instead");
 		opts->do_encrypt = bv ? PDF_ENCRYPT_NONE : PDF_ENCRYPT_KEEP;
+	}
 
 	if (fz_lookup_option_boolean(ctx, args, "encrypt", &opts->do_encrypt) > 0)
 	{