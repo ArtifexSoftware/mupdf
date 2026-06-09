44ea2135a5ae61c98be51f509bbcefac7829cd43
Robin Watts <Robin.Watts@artifex.com>
2026-06-09 14:31:54 +0100

    Bug 709390: Fix bad parents in page trees.
    If we hit a loop in the parents of page trees, attempt to
    repair.

--- source/pdf/pdf-object.c
+++ source/pdf/pdf-object.c
@@ -22,6 +22,7 @@
 
 #include "mupdf/fitz.h"
 #include "mupdf/pdf.h"
+#include "pdf-imp.h"
 
 #include <stdarg.h>
 #include <stdlib.h>
@@ -3784,11 +3785,17 @@ int pdf_obj_refs(fz_context *ctx, pdf_obj *obj)
 	https://www.geeksforgeeks.org/floyds-cycle-finding-algorithm/
 */
 pdf_obj *
-pdf_dict_get_inheritable(fz_context *ctx, pdf_obj *node, pdf_obj *key)
+pdf_dict_get_inheritable(fz_context *ctx, pdf_obj *start, pdf_obj *key)
 {
-	pdf_obj *slow = node;
-	int halfbeat = 11; /* Don't start moving slow pointer for a while. */
+	pdf_obj *node;
+	pdf_obj *slow;
+	int halfbeat;
+	int repaired = 0;
 
+retry_after_repair:
+	node = start;
+	slow = node;
+	halfbeat = 11; /* Don't start moving slow pointer for a while. */
 	while (node)
 	{
 		pdf_obj *val = pdf_dict_get(ctx, node, key);
@@ -3796,7 +3803,15 @@ pdf_dict_get_inheritable(fz_context *ctx, pdf_obj *node, pdf_obj *key)
 			return val;
 		node = pdf_dict_get(ctx, node, PDF_NAME(Parent));
 		if (node == slow)
+		{
+			if (repaired == 0)
+			{
+				repaired = 1;
+				pdf_repair_page_tree_parents(ctx, pdf_get_bound_document(ctx, start));
+				goto retry_after_repair;
+			}
 			fz_throw(ctx, FZ_ERROR_FORMAT, "cycle in resources");
+		}
 		if (--halfbeat == 0)
 		{
 			slow = pdf_dict_get(ctx, slow, PDF_NAME(Parent));
@@ -3805,14 +3820,21 @@ pdf_dict_get_inheritable(fz_context *ctx, pdf_obj *node, pdf_obj *key)
 	}
 
 	return NULL;
+
 }
 
 pdf_obj *
-pdf_dict_getp_inheritable(fz_context *ctx, pdf_obj *node, const char *path)
+pdf_dict_getp_inheritable(fz_context *ctx, pdf_obj *start, const char *path)
 {
-	pdf_obj *slow = node;
-	int halfbeat = 11; /* Don't start moving slow pointer for a while. */
+	pdf_obj *node;
+	pdf_obj *slow;
+	int halfbeat;
+	int repaired = 0;
 
+retry_after_repair:
+	node = start;
+	slow = node;
+	halfbeat = 11; /* Don't start moving slow pointer for a while. */
 	while (node)
 	{
 		pdf_obj *val = pdf_dict_getp(ctx, node, path);
@@ -3820,7 +3842,15 @@ pdf_dict_getp_inheritable(fz_context *ctx, pdf_obj *node, const char *path)
 			return val;
 		node = pdf_dict_get(ctx, node, PDF_NAME(Parent));
 		if (node == slow)
+		{
+			if (repaired == 0)
+			{
+				repaired = 1;
+				pdf_repair_page_tree_parents(ctx, pdf_get_bound_document(ctx, start));
+				goto retry_after_repair;
+			}
 			fz_throw(ctx, FZ_ERROR_FORMAT, "cycle in resources");
+		}
 		if (--halfbeat == 0)
 		{
 			slow = pdf_dict_get(ctx, slow, PDF_NAME(Parent));
@@ -3832,11 +3862,17 @@ pdf_dict_getp_inheritable(fz_context *ctx, pdf_obj *node, const char *path)
 }
 
 pdf_obj *
-pdf_dict_gets_inheritable(fz_context *ctx, pdf_obj *node, const char *key)
+pdf_dict_gets_inheritable(fz_context *ctx, pdf_obj *start, const char *key)
 {
-	pdf_obj *slow = node;
-	int halfbeat = 11; /* Don't start moving slow pointer for a while. */
+	pdf_obj *node;
+	pdf_obj *slow;
+	int halfbeat;
+	int repaired = 0;
 
+retry_after_repair:
+	node = start;
+	slow = node;
+	halfbeat = 11; /* Don't start moving slow pointer for a while. */
 	while (node)
 	{
 		pdf_obj *val = pdf_dict_gets(ctx, node, key);
@@ -3844,7 +3880,15 @@ pdf_dict_gets_inheritable(fz_context *ctx, pdf_obj *node, const char *key)
 			return val;
 		node = pdf_dict_get(ctx, node, PDF_NAME(Parent));
 		if (node == slow)
+		{
+			if (repaired == 0)
+			{
+				repaired = 1;
+				pdf_repair_page_tree_parents(ctx, pdf_get_bound_document(ctx, start));
+				goto retry_after_repair;
+			}
 			fz_throw(ctx, FZ_ERROR_FORMAT, "cycle in resources");
+		}
 		if (--halfbeat == 0)
 		{
 			slow = pdf_dict_get(ctx, slow, PDF_NAME(Parent));
