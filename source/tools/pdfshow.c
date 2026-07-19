dae22b309e79acd887cea50e429dd89b5de5826a
Sebastian Rasmussen <sebras@gmail.com>
2026-07-19 19:20:46 +0200

    Remember to mark variables fz_var to avoid stale values upon exception.

--- source/tools/pdfshow.c
+++ source/tools/pdfshow.c
@@ -1,4 +1,4 @@
-// Copyright (C) 2004-2025 Artifex Software, Inc.
+// Copyright (C) 2004-2026 Artifex Software, Inc.
 //
 // This file is part of MuPDF.
 //
@@ -674,6 +674,7 @@ int pdfshow_main(int argc, char **argv)
 
 	fz_var(doc);
 	fz_var(labels);
+
 	fz_try(ctx)
 	{
 		doc = pdf_open_document(ctx, filename);
