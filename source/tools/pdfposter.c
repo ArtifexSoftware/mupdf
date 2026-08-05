b745d99e32596c7a687718556ec261e4dc2653db
Tor Andersson <tor.andersson@artifex.com>
2026-08-05 11:24:13 +0200

    mutool poster: Drop kids array upon exception.
    The previous fix for this bug clobbered the Kids array that we need
    to read from to create the new Kids array. Move the assignment Kids
    until _after_ the pages have all been processed.
    
    Re-implements a02d4153326f04885d3990befc090fc2c273f12c.

--- source/tools/pdfposter.c
+++ source/tools/pdfposter.c
@@ -112,126 +112,136 @@ static void decimatepages(fz_context *ctx, pdf_document *doc)
 
 	/* Create a new kids array with our new pages in */
 	kids = pdf_new_array(ctx, doc, 1);
-
-	kidcount = 0;
-	for (page=0; page < num_pages; page++)
+	fz_try(ctx)
 	{
-		pdf_obj *page_obj = pdf_lookup_page_obj(ctx, doc, page);
-		int xf = x_factor, yf = y_factor;
-		float w, h;
-		int x, y;
-		int xd, yd, y0, y1;
-		float xmargin, ymargin;
-
-		rotate = pdf_to_int(ctx, pdf_dict_get_inheritable(ctx, page_obj, PDF_NAME(Rotate)));
-		mediabox = pdf_to_rect(ctx, pdf_dict_get_inheritable(ctx, page_obj, PDF_NAME(MediaBox)));
-		cropbox = pdf_to_rect(ctx, pdf_dict_get_inheritable(ctx, page_obj, PDF_NAME(CropBox)));
-		if (fz_is_empty_rect(mediabox))
-			mediabox = fz_make_rect(0, 0, 612, 792);
-		if (!fz_is_empty_rect(cropbox))
-			mediabox = fz_intersect_rect(mediabox, cropbox);
-
-		w = mediabox.x1 - mediabox.x0;
-		h = mediabox.y1 - mediabox.y0;
-
-		switch (rotate)
+		kidcount = 0;
+		for (page=0; page < num_pages; page++)
 		{
-		default:
-		case 0:
-			xf = x_factor;
-			yf = y_factor;
-			xd = x_dir;
-			yd = -x_dir;
-			break;
-		case 90:
-			xf = y_factor;
-			yf = x_factor;
-			yd = x_dir;
-			xd = x_dir;
-			break;
-		case 180:
-			xf = x_factor;
-			yf = y_factor;
-			xd = -x_dir;
-			yd = x_dir;
-			break;
-		case 270:
-			xf = y_factor;
-			yf = x_factor;
-			yd = -x_dir;
-			xd = -x_dir;
-			break;
-		}
+			pdf_obj *page_obj = pdf_lookup_page_obj(ctx, doc, page);
+			int xf = x_factor, yf = y_factor;
+			float w, h;
+			int x, y;
+			int xd, yd, y0, y1;
+			float xmargin, ymargin;
 
-		if (xf == 0 && yf == 0)
-		{
-			/* Nothing specified, so split along the long edge */
-			if (w > h)
-				xf = 2, yf = 1;
-			else
-				xf = 1, yf = 2;
-		}
-		else if (xf == 0)
-			xf = 1;
-		else if (yf == 0)
-			yf = 1;
+			rotate = pdf_to_int(ctx, pdf_dict_get_inheritable(ctx, page_obj, PDF_NAME(Rotate)));
+			mediabox = pdf_to_rect(ctx, pdf_dict_get_inheritable(ctx, page_obj, PDF_NAME(MediaBox)));
+			cropbox = pdf_to_rect(ctx, pdf_dict_get_inheritable(ctx, page_obj, PDF_NAME(CropBox)));
+			if (fz_is_empty_rect(mediabox))
+				mediabox = fz_make_rect(0, 0, 612, 792);
+			if (!fz_is_empty_rect(cropbox))
+				mediabox = fz_intersect_rect(mediabox, cropbox);
 
-		xmargin = xf == 1 ? 0 : margin * (margin_is_percent ? .001 * w/xf : 1);
-		ymargin = yf == 1 ? 0 : margin * (margin_is_percent ? .001 * h/yf : 1);
+			w = mediabox.x1 - mediabox.x0;
+			h = mediabox.y1 - mediabox.y0;
 
-		y0 = (yd > 0) ? 0 : yf-1;
-		y1 = (yd > 0) ? yf : -1;
-		for (y = y0; y != y1; y += yd)
-		{
-			int x0 = (xd > 0) ? 0 : xf-1;
-			int x1 = (xd > 0) ? xf : -1;
-			for (x = x0; x != x1; x += xd)
+			switch (rotate)
 			{
-				pdf_obj *newpageobj, *newpageref, *newmediabox;
-				fz_rect mb;
+			default:
+			case 0:
+				xf = x_factor;
+				yf = y_factor;
+				xd = x_dir;
+				yd = -x_dir;
+				break;
+			case 90:
+				xf = y_factor;
+				yf = x_factor;
+				yd = x_dir;
+				xd = x_dir;
+				break;
+			case 180:
+				xf = x_factor;
+				yf = y_factor;
+				xd = -x_dir;
+				yd = x_dir;
+				break;
+			case 270:
+				xf = y_factor;
+				yf = x_factor;
+				yd = -x_dir;
+				xd = -x_dir;
+				break;
+			}
 
-				newpageobj = pdf_copy_dict(ctx, pdf_lookup_page_obj(ctx, doc, page));
-				pdf_flatten_inheritable_page_items(ctx, newpageobj);
-				newpageref = pdf_add_object(ctx, doc, newpageobj);
-
-				newmediabox = pdf_new_array(ctx, doc, 4);
-
-				mb.x0 = mediabox.x0 + (w/xf)*x;
-				if (x == xf-1)
-					mb.x1 = mediabox.x1;
+			if (xf == 0 && yf == 0)
+			{
+				/* Nothing specified, so split along the long edge */
+				if (w > h)
+					xf = 2, yf = 1;
 				else
-					mb.x1 = mb.x0 + (w/xf) + xmargin;
-				mb.y0 = mediabox.y0 + (h/yf)*y;
-				if (y == yf-1)
-					mb.y1 = mediabox.y1;
-				else
-					mb.y1 = mb.y0 + (h/yf) + ymargin;
+					xf = 1, yf = 2;
+			}
+			else if (xf == 0)
+				xf = 1;
+			else if (yf == 0)
+				yf = 1;
 
-				pdf_array_push_real(ctx, newmediabox, mb.x0);
-				pdf_array_push_real(ctx, newmediabox, mb.y0);
-				pdf_array_push_real(ctx, newmediabox, mb.x1);
-				pdf_array_push_real(ctx, newmediabox, mb.y1);
+			xmargin = xf == 1 ? 0 : margin * (margin_is_percent ? .001 * w/xf : 1);
+			ymargin = yf == 1 ? 0 : margin * (margin_is_percent ? .001 * h/yf : 1);
 
-				pdf_dict_put(ctx, newpageobj, PDF_NAME(Parent), pages);
-				pdf_dict_put_drop(ctx, newpageobj, PDF_NAME(MediaBox), newmediabox);
+			y0 = (yd > 0) ? 0 : yf-1;
+			y1 = (yd > 0) ? yf : -1;
+			for (y = y0; y != y1; y += yd)
+			{
+				int x0 = (xd > 0) ? 0 : xf-1;
+				int x1 = (xd > 0) ? xf : -1;
+				for (x = x0; x != x1; x += xd)
+				{
+					pdf_obj *newpageobj, *newpageref, *newmediabox;
+					fz_rect mb;
 
-				intersect_box(ctx, doc, newpageobj, PDF_NAME(CropBox), mb);
-				intersect_box(ctx, doc, newpageobj, PDF_NAME(BleedBox), mb);
-				intersect_box(ctx, doc, newpageobj, PDF_NAME(TrimBox), mb);
-				intersect_box(ctx, doc, newpageobj, PDF_NAME(ArtBox), mb);
+					newpageobj = pdf_copy_dict(ctx, pdf_lookup_page_obj(ctx, doc, page));
+					pdf_flatten_inheritable_page_items(ctx, newpageobj);
+					newpageref = pdf_add_object(ctx, doc, newpageobj);
 
-				/* Store page object in new kids array */
-				pdf_drop_obj(ctx, newpageobj);
-				pdf_array_push_drop(ctx, kids, newpageref);
+					newmediabox = pdf_new_array(ctx, doc, 4);
 
-				kidcount++;
+					mb.x0 = mediabox.x0 + (w/xf)*x;
+					if (x == xf-1)
+						mb.x1 = mediabox.x1;
+					else
+						mb.x1 = mb.x0 + (w/xf) + xmargin;
+					mb.y0 = mediabox.y0 + (h/yf)*y;
+					if (y == yf-1)
+						mb.y1 = mediabox.y1;
+					else
+						mb.y1 = mb.y0 + (h/yf) + ymargin;
+
+					pdf_array_push_real(ctx, newmediabox, mb.x0);
+					pdf_array_push_real(ctx, newmediabox, mb.y0);
+					pdf_array_push_real(ctx, newmediabox, mb.x1);
+					pdf_array_push_real(ctx, newmediabox, mb.y1);
+
+					pdf_dict_put(ctx, newpageobj, PDF_NAME(Parent), pages);
+					pdf_dict_put_drop(ctx, newpageobj, PDF_NAME(MediaBox), newmediabox);
+
+					intersect_box(ctx, doc, newpageobj, PDF_NAME(CropBox), mb);
+					intersect_box(ctx, doc, newpageobj, PDF_NAME(BleedBox), mb);
+					intersect_box(ctx, doc, newpageobj, PDF_NAME(TrimBox), mb);
+					intersect_box(ctx, doc, newpageobj, PDF_NAME(ArtBox), mb);
+
+					/* Store page object in new kids array */
+					pdf_drop_obj(ctx, newpageobj);
+					pdf_array_push_drop(ctx, kids, newpageref);
+
+					kidcount++;
+				}
 			}
 		}
-	}
 
-	/* Update page count and kids array */
-	pdf_dict_put_int(ctx, pages, PDF_NAME(Count), kidcount);
-	pdf_dict_put_drop(ctx, pages, PDF_NAME(Kids), kids);
+		/* Update page count and kids array */
+		pdf_dict_put_int(ctx, pages, PDF_NAME(Count), kidcount);
+		pdf_dict_put(ctx, pages, PDF_NAME(Kids), kids);
+	}
+	fz_always(ctx)
+	{
+		pdf_drop_obj(ctx, kids);
+	}
+	fz_catch(ctx)
+	{
+		fz_rethrow(ctx);
+	}
 }
 
 int pdfposter_main(int argc, char **argv)
