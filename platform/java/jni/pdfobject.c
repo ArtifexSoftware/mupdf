72a1298cf8ead21c72b98d66783a40cdc2619ff7
Sebastian Rasmussen <sebras@gmail.com>
2026-08-11 05:21:20 +0200

    jni: Fix typo passing NULL to release UTF string.

--- platform/java/jni/pdfobject.c
+++ platform/java/jni/pdfobject.c
@@ -1,4 +1,4 @@
-// Copyright (C) 2004-2025 Artifex Software, Inc.
+// Copyright (C) 2004-2026 Artifex Software, Inc.
 //
 // This file is part of MuPDF.
 //
@@ -705,7 +705,7 @@ FUN(PDFObject_putDictionaryStringString)(JNIEnv *env, jobject self, jstring jnam
 		str = (*env)->GetStringUTFChars(env, jstr, NULL);
 		if (!str)
 		{
-			(*env)->ReleaseStringUTFChars(env, jname, str);
+			(*env)->ReleaseStringUTFChars(env, jname, name);
 			return;
 		}
 	}
