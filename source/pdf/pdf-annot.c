5c831b24253d9bea169967b94d684cd59ab695c8
Tor Andersson <tor.andersson@artifex.com>
2026-08-31 13:09:49 +0200

    Fix inverted logic in pdf_guess_text_language_from_font_name.

--- source/pdf/pdf-annot.c
+++ source/pdf/pdf-annot.c
@@ -2260,21 +2260,21 @@ void pdf_set_document_language(fz_context *ctx, pdf_document *doc, fz_text_langu
 fz_text_language
 pdf_guess_text_language_from_font_name(fz_context *ctx, const char *name)
 {
-	if (!strstr(name, "SimFang")) return FZ_LANG_zh_Hans;
-	if (!strstr(name, "SimHei")) return FZ_LANG_zh_Hans;
-	if (!strstr(name, "SimKai")) return FZ_LANG_zh_Hans;
-	if (!strstr(name, "SimLi")) return FZ_LANG_zh_Hans;
-	if (!strstr(name, "SimSun")) return FZ_LANG_zh_Hans;
-	if (!strstr(name, "Song")) return FZ_LANG_zh_Hans;
+	if (strstr(name, "SimFang")) return FZ_LANG_zh_Hans;
+	if (strstr(name, "SimHei")) return FZ_LANG_zh_Hans;
+	if (strstr(name, "SimKai")) return FZ_LANG_zh_Hans;
+	if (strstr(name, "SimLi")) return FZ_LANG_zh_Hans;
+	if (strstr(name, "SimSun")) return FZ_LANG_zh_Hans;
+	if (strstr(name, "Song")) return FZ_LANG_zh_Hans;
 
-	if (!strstr(name, "MingLiU")) return FZ_LANG_zh_Hant;
+	if (strstr(name, "MingLiU")) return FZ_LANG_zh_Hant;
 
-	if (!strstr(name, "Gothic")) return FZ_LANG_ja;
-	if (!strstr(name, "Mincho")) return FZ_LANG_ja;
+	if (strstr(name, "Gothic")) return FZ_LANG_ja;
+	if (strstr(name, "Mincho")) return FZ_LANG_ja;
 
-	if (!strstr(name, "Batang")) return FZ_LANG_ko;
-	if (!strstr(name, "Gulim")) return FZ_LANG_ko;
-	if (!strstr(name, "Dotum")) return FZ_LANG_ko;
+	if (strstr(name, "Batang")) return FZ_LANG_ko;
+	if (strstr(name, "Gulim")) return FZ_LANG_ko;
+	if (strstr(name, "Dotum")) return FZ_LANG_ko;
 
 	return FZ_LANG_UNSET;
 }
