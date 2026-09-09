2a0746381087c4d6ac887a7455fe779ef4ccd8e7
Robin Watts <Robin.Watts@artifex.com>
2026-09-09 15:20:38 +0100

    Bug 709652: Avoid out of bounds stack access in CFF subsetting.
    From a bud report and analysis by Giacomo Baldon. Many thanks!
    
    In the handling for 'roll', If N = 0x7FFFFFFF, then N > 0, but 2+N
    overflows. The fix is to check that N won't overflow before using it.
    
    Review of similar code suggests that we should bounds check i
    before ATLEAST(i+1) in the handling for 'index'.

--- source/fitz/subset-cff.c
+++ source/fitz/subset-cff.c
@@ -1134,9 +1134,9 @@ overflow:
 				int i;
 				ATLEAST(1);
 				i = (int)stack[sp-1];
-				ATLEAST(i+1);
 				if (i < 0 || i > sp-1)
 					i = 0;
+				ATLEAST(i+1);
 				stack[sp-1] = stack[sp-2-i];
 				break;
 			}
@@ -1148,7 +1148,7 @@ overflow:
 				N = stack[sp-2];
 				if (N == 0)
 					break;
-				if (N < 0)
+				if (N < 0 || N >= INT_MAX-2)
 					fz_throw(ctx, FZ_ERROR_FORMAT, "Invalid roll");
 				ATLEAST(2+N);
 				if (J < 0)
